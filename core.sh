cc9a804a146a5c3af3df9e992fb76bfe97768dd6
cobalt2727 <60624944+cobalt2727@users.noreply.github.com>
2020-06-07 01:12:39 -0500

    why is color so hard to make work in the terminal

--- core.sh
+++ core.sh
@@ -1,19 +1,10 @@
 clear
-echo "Welcome back, \033[1;31m $USER \033[0m."
+echo "Welcome back, $USER."
 echo "Enter a number from the choices below to configure that program."
 echo "if you're reading this, color should be working"
 
 echo "more stuff goes here"
-
-
-
-
-
-
-
-
-
-
+sleep 10
 
 clear
 echo 'Thank you for using L4T Megascript! Stop by our Discord server https://tinyurl.com/L4TScript for support.'
