8c0cb31dc05e1b66318b2e283319628c7a09cec1
cobalt2727 <60624944+cobalt2727@users.noreply.github.com>
2020-06-07 01:07:10 -0500

    Update core.sh

--- core.sh
+++ core.sh
@@ -1,4 +1,4 @@
 clear
-echo "Welcome back, ${ORANGE}$USER${NC}."
+echo "Welcome back, \033[1;31m $USER \033[0m."
 echo "Enter a number from the choices below to configure that program."
 echo "if you're reading this, color should be working"
