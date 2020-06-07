98f5c320368a8a9b2a26dcb87ae4933b9d975299
cobalt2727 <60624944+cobalt2727@users.noreply.github.com>
2020-06-07 02:15:05 -0500

    Update core.sh

--- core.sh
+++ core.sh
@@ -1,5 +1,5 @@
 clear
-echo "these are here because i'm a lazy coder and i want an easy reference for coloring in the menu - if you're able to read this inside the program, something's wrong"
+echo "These are here because I'm a lazy coder and i want an easy reference for coloring in the menu - if you're able to read this inside the program, something is very wrong."
 echo -e "\e[30mBlack Text\e[0m"
 echo -e "\e[31mRed Text\e[0m"
 echo -e "\e[32mGreen Text\e[0m"
@@ -9,12 +9,23 @@ echo -e "\e[35mPurple Text\e[0m"
 echo -e "\e[36mCyan Text\e[0m"
 echo -e "\e[37mLight Gray Text\e[0m"
 clear
+echo "create a desktop file for the script on this line"
 echo "Welcome back, $USER. This isn't quite finished yet - we'll be ready eventually!"
 sleep 2
-echo "Enter a number from the choices below to configure that program."
+echo "Enter a number from the choices below to configure the specified program."
 
-echo "more stuff goes here"
-sleep 10
+echo "note to self: get lakka firmware files and put in /lib/firmware - see L4T gbatemp release thread about that"
+echo "1..........Initial setup - swapfile, joycon mouse, Bluetooth audio, USB controller support, 2.0 GHz overclock, etc"
+echo "2..........Dolphin - latest development version)"
+echo "3..........RetroArch - Not quite working yet, I don't have a collection of working cores"
+echo "4..........Minecraft - using Luke Chambers's MCSwitchTools (https://gitlab.com/devluke/mcswitchtools)"
+echo "5..........moonlight-qt - stream games from your PC as long as it has an Nvidia GPU!)"
+echo "6..........Discord setup - not the actual program, but the web app)"
+echo "7..........Kodi - media center)"
+
+
+echo "0..........Video Settings - Is your display going past the edges of your TV?)"
+echo "x..........Close L4T Megascript, view credits, and get Discord support server link"
 
 echo -e 'Thank you for using the L4T Megascript! Stop by our \e[36mDiscord\e[0m server at https://tinyurl.com/L4TScript for support.'
 echo 'If that link is down for whatever reason, use https://discord.gg/UYsUFCY.'
