5f9458b4de974b648d1ceb33c5fb0e7f076f7e2d
cobalt2727 <60624944+cobalt2727@users.noreply.github.com>
2020-06-07 12:59:55 -0500

    placeholders and stuff

--- scripts/dolphin/main.sh
+++ scripts/dolphin/main.sh
@@ -1,4 +1,8 @@
 echo "this is the dolphin script! if you're reading this, you're here too early"
+echo "Sending you back to the menu..."
+sleep 2
+curl https://raw.githubusercontent.com/cobalt2727/L4T-Megascript/master/core.sh | bash
+
 
 #check if installed
 #if yes
