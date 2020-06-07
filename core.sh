a4b3cbd6597ae5b9e1e4513364151f0b392c078a
cobalt2727 <60624944+cobalt2727@users.noreply.github.com>
2020-06-07 11:11:38 -0500

    Update core.sh

--- core.sh
+++ core.sh
@@ -18,25 +18,30 @@ echo -e "\e[31mHello World\e[0m"
 echo -e "\x1B[31mHello World\e[0m"
 
 clear
-echo "create a desktop file for the script on this line"
-echo "Welcome back, $USER. This isn't quite finished yet - we'll be ready eventually!"
+#echo "create a desktop file for the script on this line"
+echo "Welcome back to the main menu of the L4T Megascript, $USER. This isn't quite finished yet - we'll be ready eventually!"
 sleep 2
 echo "Enter a number from the choices below to configure the specified program."
 echo "Be mindful of how much storage you have left on your SD card!"
 
 echo "note to self: get lakka firmware files and put in /lib/firmware - see L4T gbatemp release thread about that"
-echo "1..........Update your programs - stuff built from source like Dolphin or RetroArch do not get updated with this"
-#echo "1..........Initial setup - swapfile, joycon mouse, Bluetooth audio, USB controller support, 2.0 GHz overclock, etc"
-echo "2..........Dolphin - latest development version)"
-#echo "3..........RetroArch - Not quite working yet, I don't have a collection of working cores"
-#echo "4..........Minecraft - automatically install Luke Chambers's MCSwitchTools (https://gitlab.com/devluke/mcswitchtools)"
-#echo "5..........moonlight-qt - stream games from your PC as long as it has an Nvidia GPU!"
-#echo "6..........Discord setup - not the actual program, but the web app)"
-#echo "7..........Kodi - media center"
-#echo "8..........Development IDEs - write code on your Switch!"
-#echo "9..........Video Settings - Is your display going past the edges of your TV?)"
-#echo "x..........Close L4T Megascript, view credits, and get Discord support server link"
+echo "1...............Update your programs - download and install updates from the repos"
+#echo "1...............Initial setup - swapfile, joycon mouse, Bluetooth audio, USB controller support, 2.0 GHz overclock, etc"
+echo "2...............Dolphin - Gamecube and Wii emulator, latest development version)"
+#echo "3...............RetroArch - Not quite working yet, I don't have a collection of working cores"
+#echo "4...............Minecraft - automatically install Luke Chambers's MCSwitchTools (https://gitlab.com/devluke/mcswitchtools)"
+#echo "5...............moonlight-qt - stream games from your PC as long as it has an Nvidia GPU!"
+#echo "6...............Discord setup - not the actual program, but the web app)"
+#echo "7...............Kodi - media center"
+#echo "8...............Development IDEs - write code on your Switch!"
+#echo "9...............Video Settings - Is your display going past the edges of your TV?)"
+#echo "10..............Citra - 3DS emulator, this probably is completely broken"
+#echo "any other key...Close L4T Megascript, view credits, and get Discord support server link"
 
+
+echo "Thank you for using the L4T Megascript!
+sleep 2
+clear
 echo "Credits:"
 echo "CTRL + CLICK ON A LINK TO OPEN IT
 echo "Cobalt - Manager/Lead Coder"
