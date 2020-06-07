423df1397600d7e8eda4c54e0bb7def7fce16063
cobalt2727 <60624944+cobalt2727@users.noreply.github.com>
2020-06-07 11:23:27 -0500

    Create main.sh

---
+++ scripts/dolphin/main.sh
@@ -0,0 +1,7 @@
+echo "this is the dolphin script! if you're reading this, you're here too early"
+
+#check if installed
+#if yes
+#git pull cd build whatever
+#else
+#git clone mkdir build cd build whatever
