80d9d40c8942adcff938a7e028d248c2c5504a31
cobalt2727 <60624944+cobalt2727@users.noreply.github.com>
2020-06-07 01:26:16 -0500

    Update core.sh

--- core.sh
+++ core.sh
@@ -1,14 +1,12 @@
-$ echo -e "\e[30mBlack Text\e[0m"
-$ echo -e "\e[31mRed Text\e[0m"
-$ echo -e "\e[32mGreen Text\e[0m"
-$ echo -e "\e[33mBrown Text\e[0m"
-$ echo -e "\e[34mBlue Text\e[0m"
-$ echo -e "\e[35mPurple Text\e[0m"
-$ echo -e "\e[36mCyan Text\e[0m"
-$ echo -e "\e[37mLight Gray Text\e[0m"
-
-
-
+clear
+echo -e "\e[30mBlack Text\e[0m"
+echo -e "\e[31mRed Text\e[0m"
+echo -e "\e[32mGreen Text\e[0m"
+echo -e "\e[33mBrown Text\e[0m"
+echo -e "\e[34mBlue Text\e[0m"
+echo -e "\e[35mPurple Text\e[0m"
+echo -e "\e[36mCyan Text\e[0m"
+echo -e "\e[37mLight Gray Text\e[0m"
 clear
 echo "Welcome back, $USER. This isn't quite finished yet - we'll be ready eventually!"
 sleep 2
@@ -20,4 +18,3 @@ sleep 10
 clear
 echo -e 'Thank you for using L4T Megascript! Stop by our "\e[36mDiscord\e[0m" server at https://tinyurl.com/L4TScript for support.'
 echo 'If that link is down for whatever reason, use https://discord.gg/UYsUFCY.'
-echo -e "\e[31mRed Text\e[0m"
