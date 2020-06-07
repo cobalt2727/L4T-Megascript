c5489874c5615af4226fdf6c6d19b0a1bf4b80c8
cobalt2727 <60624944+cobalt2727@users.noreply.github.com>
2020-06-07 01:11:33 -0500

    Update core.sh

--- core.sh
+++ core.sh
@@ -2,3 +2,19 @@ clear
 echo "Welcome back, \033[1;31m $USER \033[0m."
 echo "Enter a number from the choices below to configure that program."
 echo "if you're reading this, color should be working"
+
+echo "more stuff goes here"
+
+
+
+
+
+
+
+
+
+
+
+clear
+echo 'Thank you for using L4T Megascript! Stop by our Discord server https://tinyurl.com/L4TScript for support.'
+echo 'If that link is down for whatever reason, use https://discord.gg/UYsUFCY.`
