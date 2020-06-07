3d3cd73e291c4390a5489fd325c9c4803d086558
cobalt2727 <60624944+cobalt2727@users.noreply.github.com>
2020-06-07 13:59:06 -0500

    Update core.sh

--- core.sh
+++ core.sh
@@ -29,14 +29,14 @@ echo
 echo
 #echo "0...............Initial setup - swapfile, joycon mouse, Bluetooth audio, USB controller support, 2.0 GHz overclock, etc"
 echo "1...............Update your programs - download and install updates from the repos"
-echo "2...............Dolphin - Gamecube and Wii emulator, latest development version)"
+echo "2...............Dolphin - Gamecube and Wii emulator, latest development version"
 #echo "3...............RetroArch - Not quite working yet, I don't have a collection of working cores"
 #echo "4...............Minecraft - automatically install Luke Chambers's MCSwitchTools (https://gitlab.com/devluke/mcswitchtools)"
 #echo "5...............moonlight-qt - stream games from your PC as long as it has an Nvidia GPU!"
-#echo "6...............Discord setup - not the actual program, but the web app)"
+#echo "6...............Discord setup - not the actual program, but the web app"
 #echo "7...............Kodi - media center"
 #echo "8...............Development IDEs - write code on your Switch!"
-#echo "9...............Video Settings - Is your display going past the edges of your TV?)"
+#echo "9...............Video Settings - Is your display going past the edges of your TV?"
 #echo "10..............Citra - 3DS emulator, this probably is completely broken"
 echo "any other key...Close L4T Megascript, view credits and source code, and get Discord support server link"
 echo
