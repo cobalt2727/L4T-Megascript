5dd3365d57c67c67952ec70df027c93d5c807b43
cobalt2727 <60624944+cobalt2727@users.noreply.github.com>
2020-06-07 12:51:42 -0500

    Update apt.sh

--- scripts/apt.sh
+++ scripts/apt.sh
@@ -5,4 +5,6 @@ sudo apt update
 sudo apt upgrade -y
 sudo apt autoremove -y
 sudo flatpak update
+echo "Done! Sending you back to the main menu..."
+sleep 2
 curl https://raw.githubusercontent.com/cobalt2727/L4T-Megascript/master/core.sh | bash
