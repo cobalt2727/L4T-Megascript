217751d8d949b0f4148a870947e8320960c13ca9
cobalt2727 <60624944+cobalt2727@users.noreply.github.com>
2020-06-07 13:36:48 -0500

    Update apt.sh

--- scripts/apt.sh
+++ scripts/apt.sh
@@ -1,10 +1,10 @@
 clear
-echo "Running updates..."
+echo "Updater script successfully started! Running updates..."
 sleep 1
 sudo apt update
 sudo apt upgrade -y
 sudo apt autoremove -y
 sudo flatpak update
 echo "Done! Sending you back to the main menu..."
-sleep 2
+sleep 5
 bash -c "$(curl -s https://raw.githubusercontent.com/cobalt2727/L4T-Megascript/master/core.sh)"
