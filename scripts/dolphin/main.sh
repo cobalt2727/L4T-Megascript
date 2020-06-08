09b87a2e34931f95d302261d6d39868fd7d8cdf0
cobalt2727 <60624944+cobalt2727@users.noreply.github.com>
2020-06-07 23:41:34 -0500

    formatting and mention other versions of Dolphin

--- scripts/dolphin/main.sh
+++ scripts/dolphin/main.sh
@@ -1,13 +1,10 @@
-echo "this is the dolphin script! if you're reading this, you're here too early"
-echo "Sending you back to the menu..."
-sleep 2
-#bash -c "$(curl -s https://raw.githubusercontent.com/cobalt2727/L4T-Megascript/master/core.sh)"
-
-
 echo "Dolphin script successfully started!"
 echo "What would you like to do?"
+echo
+echo
 echo "1...............Install or update Dolphin (this can and will take a LONG time, so grab some popcorn)"
 echo "2...............Download my latest autoconfigs for Dolphin for the Switch:"
+echo "3...............NOT IMPLEMENTED YET AND WON'T BE FOR A WHILE - Build other variants of Dolphin from source (Primehack, Project+, etc)
 echo "  -graphics settings"
 echo "  -game-specific controller profiles"
 echo "  -custom user interface"
