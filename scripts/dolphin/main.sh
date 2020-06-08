2e548e64652c6d65ec5ef752a23c7b82e72e792e
cobalt2727 <60624944+cobalt2727@users.noreply.github.com>
2020-06-08 00:13:42 -0500

    formatting and phrasing fixes

--- scripts/dolphin/main.sh
+++ scripts/dolphin/main.sh
@@ -2,12 +2,12 @@ echo "Dolphin script successfully started!"
 echo "What would you like to do?"
 echo
 echo
-echo "1...............Install or update Dolphin (this can and will take a LONG time, so grab some popcorn)"
-echo "2...............Download my latest autoconfigs for Dolphin for the Switch:"
-echo "3...............NOT IMPLEMENTED YET AND WON'T BE FOR A WHILE - Build other variants of Dolphin from source (Primehack, Project+, etc)"
+echo "1...............Install Dolphin from the Ubuntu PPA (use the updater on the main menu to update!)"
+echo "2...............Download my latest autoconfigs for Dolphin for the Switch"
 echo "  -graphics settings"
 echo "  -game-specific controller profiles"
 echo "  -custom user interface"
+echo "3...............NOT IMPLEMENTED YET AND WON'T BE FOR A WHILE - Build other variants of Dolphin from source (Primehack, Project+, etc)"
 echo "any other key...Close the Dolphin script and return to the main menu"
 echo
 echo
