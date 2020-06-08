6791eb26c5c994c4a9732d23821b2666b5144f48
cobalt2727 <60624944+cobalt2727@users.noreply.github.com>
2020-06-07 20:46:16 -0500

    Get the main Dolphin script up
    Everything here should be done, hopefully I don't need to touch this again

--- scripts/dolphin/main.sh
+++ scripts/dolphin/main.sh
@@ -4,8 +4,29 @@ sleep 2
 bash -c "$(curl -s https://raw.githubusercontent.com/cobalt2727/L4T-Megascript/master/core.sh)"
 
 
-#check if installed
-#if yes
-#git pull cd build whatever
-#else
-#git clone mkdir build cd build whatever
+echo "Dolphin script successfully started!"
+echo "What would you like to do?"
+echo "1...............Install or update Dolphin (this can and will take a LONG time, so grab some popcorn)"
+echo "2...............Download my latest autoconfigs for Dolphin for the Switch:"
+echo "  -graphics settings"
+echo "  -game-specific controller profiles"
+echo "  -custom user interface"
+echo "any other key...Close the Dolphin script and return to the main menu"
+echo
+echo
+
+read -p "Make a selection: " userInput
+
+echo "you have chosen $userInput"
+if [[ $userInput == 1 ]]; then
+  bash -c "$(curl -s https://raw.githubusercontent.com/cobalt2727/L4T-Megascript/master/scripts/dolphin/install.sh)"
+
+elif [[ $userInput == 2 ]]; then
+  bash -c "$(curl -s https://raw.githubusercontent.com/cobalt2727/L4T-Megascript/master/scripts/dolphin/config.sh)"
+
+else
+  echo ""
+  
+fi
+echo "Sending you back to the main menu..."
+bash -c "$(curl -s https://raw.githubusercontent.com/cobalt2727/L4T-Megascript/master/core.sh)"
