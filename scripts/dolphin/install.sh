fb319d4deb3650e5f3ad79d59e2918e1e4cb1c08
cobalt2727 <60624944+cobalt2727@users.noreply.github.com>
2020-06-08 00:09:52 -0500

    apt-add-repository now always runs
    Removed the checks to see if the PPA was already there since this is really supposed to be a script you only run once. Sure it's messy, but it works just fine, yeah?

--- scripts/dolphin/install.sh
+++ scripts/dolphin/install.sh
@@ -1,18 +1,25 @@
+echo "Making sure software-properties-common is installed..."
 sudo apt install software-properties-common
-echo "looking to see if Dolphin's PPA already exists..."
+#echo "looking to see if Dolphin's PPA already exists..."
 
-if ! grep -q "^deb .*dolphin-emu/ppa" /etc/apt/sources.list /etc/apt/sources.list.d/*; then
-    echo "Dolphin PPA not found, installing now!"
+#if ! grep -q "^deb .*dolphin-emu/ppa" /etc/apt/sources.list /etc/apt/sources.list.d/*; then
+    #echo "Dolphin PPA not found, installing now!"
+echo "Making sure the Dolphin PPA is installed..."
     sudo apt-add-repository ppa:dolphin-emu/ppa
-else
-    echo "You've already installed the PPA, moving on to checking for updates..."
+#else
+    #echo "You've already installed the PPA, moving on to checking for updates..."
 
-fi
+#fi
 sudo apt update
 sudo apt install dolphin-emu-master
-sleep 30
 echo
 echo
 echo "NOTE: If you already have dolphin installed, you don't need to"
 echo "check this script to update - just press 1 on the main menu and"
 echo "Dolphin will get updated alongside everything else!"
+sleep 10
+
+
+echo "Sending you back to the main menu..."
+sleep 1
+bash -c "$(curl -s https://raw.githubusercontent.com/cobalt2727/L4T-Megascript/master/core.sh)"
