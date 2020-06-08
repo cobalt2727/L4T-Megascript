6e38c3031f4769dfb8cc9ec49b1ef8ebbbe3a457
cobalt2727 <60624944+cobalt2727@users.noreply.github.com>
2020-06-07 23:45:57 -0500

    Update install.sh

--- scripts/dolphin/install.sh
+++ scripts/dolphin/install.sh
@@ -1,4 +1,20 @@
 echo "placeholder script here"
 sleep 10
 
-bash -c "$(curl -s https://raw.githubusercontent.com/cobalt2727/L4T-Megascript/master/scripts/dolphin/main.sh)"
+echo "looking to see if Dolphin's PPA already exists...
+
+if ! grep -q "^deb .*dolphin-emu/ppa" /etc/apt/sources.list /etc/apt/sources.list.d/*; then
+    echo "Dolphin PPA not found, installing now!"
+    sudo apt-add-repository ppa:dolphin-emu/ppa
+else
+    echo "You've already installed the PPA, moving on to checking for updates..."
+
+fi
+sudo apt update
+sudo apt install dolphin-emu-master
+
+echo
+echo
+echo "NOTE: If you already have dolphin installed, you don't need to"
+echo "check this script to update - just press 1 on the main menu and"
+echo "Dolphin will get updated alongside everything else!"
