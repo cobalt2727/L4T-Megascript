ba2e3683acd464f1f12954adc276c82d6e8e0b06
N-Games <58544833+N-Games@users.noreply.github.com>
2020-06-08 09:29:35 +0200

    Update main.sh

--- scripts/dolphin_stable/main.sh
+++ scripts/dolphin_stable/main.sh
@@ -1,1 +1,14 @@
-
+echo "installing the stable dolphin build with the other requirements for it! You should only use this build on Chromebooks!"
+sudo apt install git
+sudo apt install cmake
+sudo apt install pkg-config
+sudo apt install gcc
+sudo apt install libwxbase3.0-dev
+sudo apt install libwxgtk3.0-dev
+sudo apt install libgtk2.0-dev
+sudo apt install libxext-dev
+sudo apt install libreadline-dev
+sudo apt install libgl1-mesa-dev
+sudo apt install libevdev-dev
+sudo apt install libudev-dev
+sudo apt install dolphin-emu
