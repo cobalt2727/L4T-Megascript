0425a3b3a9ab5e4e4cd4386659ec9911decef0c6
N-Games <58544833+N-Games@users.noreply.github.com>
2020-06-08 09:32:26 +0200

    Update core.sh

--- core.sh
+++ core.sh
@@ -38,7 +38,7 @@ echo "7...............Kodi - media center"
 echo "8...............Development IDEs - write code on your Switch!"
 echo "9...............Video Settings - Is your display going past the edges of your TV?"
 echo "10..............Citra - 3DS emulator, this probably is completely broken"
-echo "11..............Dolphin Stable - Gamecube and Wii emulator, latest stable version"
+echo "11..............Dolphin Stable - Gamecube and Wii emulator, latest stable version (N-Games Test)"
 echo "any other key...Close L4T Megascript, view credits and source code, and get Discord support server link"
 echo
 echo
