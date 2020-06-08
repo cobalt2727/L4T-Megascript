0a7ad57e5dbb97cd821e6bd2be846ddf7223b46e
cobalt2727 <60624944+cobalt2727@users.noreply.github.com>
2020-06-08 14:42:47 -0500

    Update core.sh

--- core.sh
+++ core.sh
@@ -31,15 +31,16 @@ echo
 echo
 #echo "0...............Initial setup - swapfile, joycon mouse, Bluetooth audio, USB controller support, 2.0 GHz overclock, etc"
 echo "1...............Update your programs - download and install updates from the repos (check this at least once daily!)"
-echo "2...............Dolphin - Gamecube and Wii emulator, latest development version"
-#echo "3...............RetroArch - Not quite working yet, I don't have a collection of working cores"
-#echo "4...............Minecraft - automatically install Luke Chambers's MCSwitchTools (https://gitlab.com/devluke/mcswitchtools)"
-echo "5...............moonlight-qt - stream games from your PC as long as it has an Nvidia GPU!"
-#echo "6...............Discord setup - not the actual program, but the web app"
-#echo "7...............Kodi - media center"
-#echo "8...............Development IDEs - write code on your Switch!"
-#echo "9...............Video Settings - Is your display going past the edges of your TV?"
-#echo "10..............Citra - 3DS emulator, this probably is completely broken"
+echo "2...............Update L4T Megascript autoconfigs for games (very helpful, [FILESIZE] download size though)"
+echo "3...............Dolphin - Gamecube and Wii emulator, latest development version"
+#echo "4...............RetroArch - Not quite working yet, I don't have a collection of working cores"
+#echo "5...............Minecraft - automatically install Luke Chambers's MCSwitchTools (https://gitlab.com/devluke/mcswitchtools)"
+echo "6...............moonlight-qt - stream games from your PC as long as it has an Nvidia GPU!"
+#echo "7...............Discord setup - not the actual program, but the web app"
+#echo "8...............Kodi - media center"
+#echo "9...............Development IDEs - write code on your Switch!"
+#echo "10...............Video Settings - Is your display going past the edges of your TV?"
+#echo "11..............Citra - 3DS emulator, this probably is completely broken"
 echo "any other key...Close L4T Megascript, view credits and source code, and get Discord support server link"
 echo
 echo
@@ -53,11 +54,14 @@ if [[ $userInput == 0 ]]; then
 elif [[ $userInput == 1 ]]; then
   bash -c "$(curl -s https://raw.githubusercontent.com/cobalt2727/L4T-Megascript/master/scripts/apt.sh)"
 
-
 elif [[ $userInput == 2 ]]; then
+  bash -c "$(curl -s https://raw.githubusercontent.com/cobalt2727/L4T-Megascript-Assets/master/asset-core.sh)"
+
+
+elif [[ $userInput == 3 ]]; then
   bash -c "$(curl -s https://raw.githubusercontent.com/cobalt2727/L4T-Megascript/master/scripts/dolphin/main.sh)"
 
-elif [[ $userInput == 5 ]]; then
+elif [[ $userInput == 6 ]]; then
   bash -c "$(curl -s https://raw.githubusercontent.com/cobalt2727/L4T-Megascript/master/scripts/moonlight.sh)"
 
 
