4c398bc1917a6694445026e512fb928eeb2c09ce
N-Games <58544833+N-Games@users.noreply.github.com>
2020-06-08 09:21:28 +0200

    Update core.sh

--- core.sh
+++ core.sh
@@ -30,14 +30,15 @@ echo
 #echo "0...............Initial setup - swapfile, joycon mouse, Bluetooth audio, USB controller support, 2.0 GHz overclock, etc"
 echo "1...............Update your programs - download and install updates from the repos (check this at least once daily!)"
 echo "2...............Dolphin - Gamecube and Wii emulator, latest development version"
-#echo "3...............RetroArch - Not quite working yet, I don't have a collection of working cores"
-#echo "4...............Minecraft - automatically install Luke Chambers's MCSwitchTools (https://gitlab.com/devluke/mcswitchtools)"
-#echo "5...............moonlight-qt - stream games from your PC as long as it has an Nvidia GPU!"
-#echo "6...............Discord setup - not the actual program, but the web app"
-#echo "7...............Kodi - media center"
-#echo "8...............Development IDEs - write code on your Switch!"
-#echo "9...............Video Settings - Is your display going past the edges of your TV?"
-#echo "10..............Citra - 3DS emulator, this probably is completely broken"
+echo "3...............RetroArch - Not quite working yet, I don't have a collection of working cores"
+echo "4...............Minecraft - automatically install Luke Chambers's MCSwitchTools (https://gitlab.com/devluke/mcswitchtools)"
+echo "5...............moonlight-qt - stream games from your PC as long as it has an Nvidia GPU!"
+echo "6...............Discord setup - not the actual program, but the web app"
+echo "7...............Kodi - media center"
+echo "8...............Development IDEs - write code on your Switch!"
+echo "9...............Video Settings - Is your display going past the edges of your TV?"
+echo "10..............Citra - 3DS emulator, this probably is completely broken"
+echo "11..............Dolphin Stable - Gamecube and Wii emulator, latest stable version"
 echo "any other key...Close L4T Megascript, view credits and source code, and get Discord support server link"
 echo
 echo
@@ -55,6 +56,8 @@ elif [[ $userInput == 1 ]]; then
 elif [[ $userInput == 2 ]]; then
   bash -c "$(curl -s https://raw.githubusercontent.com/cobalt2727/L4T-Megascript/master/scripts/dolphin/main.sh)"
 
+elif [[ $userInput == 11 ]]; then
+  bash -c "$(curl -s https://raw.githubusercontent.com/N-Games/L4T-Megascript/master/scripts/dolphin/main.sh)"
 
 else
   echo ""
