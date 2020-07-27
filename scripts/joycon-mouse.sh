83a0f84c430cb029c826ba5d7697ab38ec758347
cobalt2727 <60624944+cobalt2727@users.noreply.github.com>
2020-07-26 22:29:26 -0500

    get configuration working
    note: get the reboot prompt working later when i'm not half asleep

--- scripts/joycon-mouse.sh
+++ scripts/joycon-mouse.sh
@@ -1,1 +1,15 @@
-#echo "hello world"
+clear
+echo "Joycon mouse script started!"
+sleep 1
+cd ~
+sudo apt install xserver-xorg-input-joystick
+wget https://cdn.discordapp.com/attachments/604648722491768883/684926248505704484/50-joystick.conf && sudo mv 50-joystick.conf /usr/share/X11/xorg.conf.d
+
+echo "Done!"
+
+
+echo "placeholder to ask if they want to reboot now"
+
+
+echo "Going back to the menu in 5 seconds..."
+sleep 5
