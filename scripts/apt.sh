7e40a6b8c6ad61e0af6f7deec6fd86c9f858dce0
cobalt2727 <60624944+cobalt2727@users.noreply.github.com>
2020-08-12 22:42:12 -0500

    literally just a single comment
    that's it

--- scripts/apt.sh
+++ scripts/apt.sh
@@ -5,5 +5,6 @@ sudo apt update
 sudo apt upgrade -y
 sudo apt autoremove -y
 sudo flatpak update -y
+##maybe somehow add a way to automatically re-run scripts for things that were built from source?
 echo "Done! Sending you back to the main menu..."
 sleep 4
