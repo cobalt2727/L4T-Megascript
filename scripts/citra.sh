224f8057ef4fca42ca5bd7085481ef7d3766931f
cobalt2727 <60624944+cobalt2727@users.noreply.github.com>
2020-08-12 22:13:00 -0500

    formatting and crediting

--- scripts/citra.sh
+++ scripts/citra.sh
@@ -1,11 +1,18 @@
 clear
 echo "Citra script successfully started!"
-sleep 1
+echo "Credits: i literally just took https://citra-emu.org/wiki/building-for-linux/ and made it run by itself"
+sleep 3
 
+echo "Running updates..."
+sleep 1
 sudo apt update -y
 
+echo "Installing dependencies..."
+sleep 1
 sudo apt-get install libsdl2-2.0-0 libsdl2-dev qtbase5-dev libqt5opengl5-dev qtmultimedia5-dev libfdk-aac-dev build-essential cmake ffmpeg libswscale-dev libavdevice57 libavformat-dev libavcodec-dev libavdevice-dev -y
 
+echo "Building Citra..."
+sleep 1
 cd ~
 git clone --recursive https://github.com/citra-emu/citra
 cd citra
@@ -14,5 +21,12 @@ cd build
 cmake .. -DCMAKE_BUILD_TYPE=Release
 make -j$(nproc)
 sudo make install
+
+echo "Removing build files..."
+sleep 1
 cd ~
 sudo rm -rf citra
+
+echo "Done!"
+echo "Sending you back to the main menu..."
+sleep 5
