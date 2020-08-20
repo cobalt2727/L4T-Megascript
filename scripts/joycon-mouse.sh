0489c42fec2b04f285e98041cc4f60097bef2541
cobalt2727 <60624944+cobalt2727@users.noreply.github.com>
2020-08-20 01:55:45 -0500

    add prompt to reboot

--- scripts/joycon-mouse.sh
+++ scripts/joycon-mouse.sh
@@ -5,6 +5,6 @@ cd ~
 sudo apt install xserver-xorg-input-joystick
 wget https://cdn.discordapp.com/attachments/604648722491768883/684926248505704484/50-joystick.conf && sudo mv 50-joystick.conf /usr/share/X11/xorg.conf.d
 
-echo "Done!"
+echo "Done! Restart your Switch when you're ready to gain access to using your joycons as a mouse."
 
 sleep 2
