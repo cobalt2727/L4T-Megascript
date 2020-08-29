a99d65ba153f02fe6391290e2535d515a1f3e52a
Lang Kasempo <lang_kasempo@outlook.com>
2020-08-29 16:55:47 -0600

    Update SRB2.sh
    Removed a not necessary anymore command

--- assets/SRB2-A/SRB2.sh
+++ assets/SRB2-A/SRB2.sh
@@ -1,1 +1,1 @@
-read RES_X RES_Y <<<$(xdpyinfo | awk -F'[ x]+' '/dimensions:/{print $3, $4}') && export DBUS_FATAL_WARNINGS=0 && /usr/share/SRB2/build/bin/lsdlsrb2 use_joystick 1 -width $RES_X -height $RES_Y
+read RES_X RES_Y <<<$(xdpyinfo | awk -F'[ x]+' '/dimensions:/{print $3, $4}') && /usr/share/SRB2/build/bin/lsdlsrb2 use_joystick 1 -width $RES_X -height $RES_Y
