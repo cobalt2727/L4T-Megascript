ff1d9fd718f4c9c1516193bbb4af0c4a4465abf9
cobalt2727 <60624944+cobalt2727@users.noreply.github.com>
2020-09-07 22:04:13 -0400

    we don't have dolphin configs ready

--- scripts/dolphin/main.sh
+++ scripts/dolphin/main.sh
@@ -1,28 +1,29 @@
 echo "Dolphin script successfully started!"
-echo "What would you like to do?"
-echo
-echo
-echo "1...............Install Dolphin from the Ubuntu PPA (use the updater on the main menu to update!)"
-echo "2...............Download my latest autoconfigs for Dolphin for the Switch"
-echo "  -graphics settings"
-echo "  -game-specific controller profiles"
-echo "  -custom user interface"
-echo "3...............NOT IMPLEMENTED YET AND WON'T BE FOR A WHILE - Build other variants of Dolphin from source (Primehack, Project+, etc)"
-echo "any other key...Close the Dolphin script and return to the main menu"
-echo
-echo
+sleep 1
+##echo "What would you like to do?"
+##echo
+##echo
+##echo "1...............Install Dolphin from the Ubuntu PPA (use the updater on the main menu to update!)"
+##echo "2...............Download my latest autoconfigs for Dolphin for the Switch"
+##echo "  -graphics settings"
+##echo "  -game-specific controller profiles"
+##echo "  -custom user interface"
+##echo "3...............NOT IMPLEMENTED YET AND WON'T BE FOR A WHILE - Build other variants of Dolphin from source (Primehack, Project+, etc)"
+##echo "any other key...Close the Dolphin script and return to the main menu"
+##echo
+##echo
 
-read -p "Make a selection: " userInput
+##read -p "Make a selection: " userInput
 
-echo "you have chosen $userInput"
-if [[ $userInput == 1 ]]; then
+##echo "you have chosen $userInput"
+##if [[ $userInput == 1 ]]; then
   bash -c "$(curl -s https://raw.githubusercontent.com/cobalt2727/L4T-Megascript/master/scripts/dolphin/install.sh)"
 
-elif [[ $userInput == 2 ]]; then
-  bash -c "$(curl -s https://raw.githubusercontent.com/cobalt2727/L4T-Megascript/master/scripts/dolphin/config.sh)"
+##elif [[ $userInput == 2 ]]; then
+##  bash -c "$(curl -s https://raw.githubusercontent.com/cobalt2727/L4T-Megascript/master/scripts/dolphin/config.sh)"
 
-else
-  echo ""
+##else
+##  echo ""
   
-fi
+##fi
 echo "Sending you back to the main menu..."
