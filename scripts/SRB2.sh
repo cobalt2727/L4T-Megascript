165a59a91cb370766a42fba8a42832855f3576b1
Lugsole <Lugsole@users.noreply.github.com>
2020-09-09 16:44:36 -0400

    changes all references to home to ~

--- scripts/SRB2.sh
+++ scripts/SRB2.sh
@@ -8,31 +8,31 @@ sudo apt install libsdl2-dev libsdl2-mixer-dev cmake extra-cmake-modules subvers
 wget $(curl --silent "https://api.github.com/repos/STJr/SRB2/releases/latest" | grep "Data" | grep "SRB2_release_" | cut -c 31- | cut -d '"' -f 2) -O SRB2-Data.zip
 svn export https://github.com/cobalt2727/L4T-Megascript/trunk/assets/SRB2-A
 cd SRB2-A
-mv SRB2_retropie.sh -t /$HOME/RetroPie/roms/ports
+mv SRB2_retropie.sh -t ~/RetroPie/roms/ports
 cd
 unzip master.zip
 mkdir SRB2-DT
-mv SRB2-Data.zip -t /home/$USER/SRB2-DT
-cd /home/$USER/SRB2-DT
+mv SRB2-Data.zip -t ~/SRB2-DT
+cd ~/SRB2-DT
 unzip SRB2-Data.zip
 rm SRB2-Data.zip
-cd /home/$USER/SRB2-master/assets 
+cd ~/SRB2-master/assets
 mkdir installer
 cd ..
 mkdir build
-cd /home/$USER
+cd ~
 mkdir .srb2
-cd /home/$USER/SRB2-A 
+cd ~/SRB2-A
 chmod 777 SRB2.sh
-mv config.cfg -t /home/$USER/.srb2
-mv SRB2.sh SRB2Icon.png -t /home/$USER/SRB2-master
+mv config.cfg -t ~/.srb2
+mv SRB2.sh SRB2Icon.png -t ~/SRB2-master
 sudo mv "Sonic Robo Blast 2.desktop" -t /usr/share/applications 
-cd /home/$USER
+cd ~
 rm master.zip
 rm -r SRB2-A
-cd /home/$USER/SRB2-DT
-mv music.dta patch.pk3 patch_music.pk3 player.dta srb2.pk3 zones.pk3 -t /home/$USER/SRB2-master/assets/installer
-cd /home/$USER/SRB2-master/build
+cd ~/SRB2-DT
+mv music.dta patch.pk3 patch_music.pk3 player.dta srb2.pk3 zones.pk3 -t ~/SRB2-master/assets/installer
+cd ~/SRB2-master/build
 echo
 echo "Compiling the game..."
 sleep 1
@@ -47,14 +47,14 @@ echo
 echo "Erasing temporary build files to save space..."
 sleep 2
 echo
-cd /home/$USER/SRB2-master/assets
+cd ~/SRB2-master/assets
 rm -r debian-template
 rm CMakeLists.txt
 rm LICENSE.txt
 rm LICENSE-3RD-PARTY.txt
 rm README.txt
 rm .gitignore
-cd /home/$USER/SRB2-master
+cd ~/SRB2-master
 rm -r cmake
 rm -r android
 rm -r bin
