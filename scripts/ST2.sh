0d99070429c29483162b180e12dc23adcf22c25d
Garrett <gmesmer@comcast.net>
2020-09-11 19:08:47 -0400

    forgot about the svn, done now

--- scripts/ST2.sh
+++ scripts/ST2.sh
@@ -5,7 +5,7 @@ sleep 3
 cd ~
 git clone --recursive https://github.com/SuperTux/supertux
 sudo apt install build-essential libsdl2-2.0-0 libsdl2-dev libsdl2-image-2.0-0 libsdl2-image-dev curl libcurl4 libcurl4-openssl-dev libvorbis-dev libogg-dev cmake extra-cmake-modules libopenal-dev libglew-dev libboost-dev libboost-all-dev subversion -y
-svn export https://github.com/cobalt2727/L4T-Megascript/trunk/assets/ST2
+svn export https://github.com/$repository_username/L4T-Megascript/trunk/assets/ST2
 cd ST2
 mv supertux2.sh -t ~/RetroPie/roms/ports
 cd
