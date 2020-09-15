7b06c66eb57963c4477226a04c0a2d075169d4e0
cobalt2727 <60624944+cobalt2727@users.noreply.github.com>
2020-09-15 15:35:55 -0400

    Create kde.sh

---
+++ scripts/desktop-environments/kde.sh
@@ -0,0 +1,16 @@
+clear
+echo "You are about to install the KDE Plasma desktop environment."
+echo "You'll be prompted to choose a display manager during the install, uh, can someone tell me which one's supposed to be used with the Switch and/or update this script?"
+echo "Under most cases this shouldn't break anything and install alongside of your existing desktop, but just to be sure..."
+echo "Are you sure you want to continue?"
+
+##prompt yes/no
+sudo apt update
+sudo apt install kde-standard
+
+echo "If your screen goes black, don't panic, that's normal. Give it a minute..."
+sleep 5
+
+sudo systemctl restart gdm
+echo "Going back to the main menu..."
+sleep 1
