b9d7f1aac44a485a5bb2429ea176c829d6b6b7c8
cobalt2727 <60624944+cobalt2727@users.noreply.github.com>
2020-09-15 15:37:49 -0400

    Create lxde.sh

---
+++ scripts/desktop-environments/lxde.sh
@@ -0,0 +1,15 @@
+clear
+echo "You are about to install the LXDE desktop environment."
+echo "Under most cases this shouldn't break anything and install alongside of your existing desktop, but just to be sure..."
+echo "Are you sure you want to continue?"
+
+##prompt yes/no
+sudo apt update
+sudo apt install lxde -y
+
+echo "If your screen goes black, don't panic, that's normal. Give it a minute..."
+sleep 5
+
+sudo systemctl restart gdm
+echo "Going back to the main menu..."
+sleep 1
