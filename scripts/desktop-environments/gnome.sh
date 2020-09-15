5f0f98f61f80494629d9242dec9db3fe73859ce0
cobalt2727 <60624944+cobalt2727@users.noreply.github.com>
2020-09-15 15:34:09 -0400

    Create gnome.sh

---
+++ scripts/desktop-environments/gnome.sh
@@ -0,0 +1,15 @@
+clear
+echo "You are about to install the GNOME desktop environment."
+echo "Under most cases this shouldn't break anything and install alongside of your existing one, but just to be sure..."
+echo "Are you sure you want to continue?"
+
+##prompt yes/no
+sudo apt update
+sudo apt install gnome-session gnome-tweaks gnome-tweak-tool -y
+
+echo "If your screen goes black, don't panic, that's normal. Give it a minute..."
+sleep 5
+
+sudo systemctl restart gdm
+echo "Going back to the main menu..."
+sleep 1
