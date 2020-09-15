943d265704ed7158e181b1b97682e89e1565eee9
cobalt2727 <60624944+cobalt2727@users.noreply.github.com>
2020-09-15 15:33:23 -0400

    Update mate.sh

--- scripts/desktop-environments/mate.sh
+++ scripts/desktop-environments/mate.sh
@@ -1,5 +1,6 @@
 clear
-echo "You are about to install a new desktop environment. Under most cases this shouldn't break anything and install alongside of your existing one, but just to be sure..."
+echo "You are about to install the MATE desktop environment."
+echo "Under most cases this shouldn't break anything and install alongside of your existing one, but just to be sure..."
 echo "Are you sure you want to continue?"
 
 ##prompt yes/no
