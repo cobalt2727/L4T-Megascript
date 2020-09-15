ad4451e4ae8d235749448370c1e154daf5ba9fe9
cobalt2727 <60624944+cobalt2727@users.noreply.github.com>
2020-09-15 15:31:33 -0400

    Update mate.sh

--- scripts/desktop-environments/mate.sh
+++ scripts/desktop-environments/mate.sh
@@ -1,3 +1,4 @@
+clear
 echo "You are about to install a new desktop environment. Under most cases this shouldn't break anything and install alongside of your existing one, but just to be sure..."
 echo "Are you sure you want to continue?"
 
@@ -5,5 +6,9 @@ echo "Are you sure you want to continue?"
 sudo apt update
 sudo apt install mate-desktop-environment mate-desktop-environment-extras ubuntu-mate-themes -y
 
+echo "If your screen goes black, don't panic, that's normal. Give it a minute..."
+sleep 5
 
 sudo systemctl restart gdm
+echo "Going back to the main menu..."
+sleep 1
