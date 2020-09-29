eac5ac3fd65f792db9a63712e934eb4b7a317429
cobalt2727 <60624944+cobalt2727@users.noreply.github.com>
2020-09-28 22:22:13 -0400

    uncommented the fix for QT apps to follow GTK theming
    also added some rudimentary documentation
    rerun the init script @everyone
    (hopefully that ping works in discord)

--- scripts/init.sh
+++ scripts/init.sh
@@ -10,17 +10,30 @@ echo "CREDITS:"
 echo "  https://gbatemp.net/threads/l4t-ubuntu-applcation-install-guides.537579/"
 echo "  Optional tab on https://gbatemp.net/threads/installing-moonlight-qt-on-l4t-ubuntu.537429/"
 sleep 10
+#allow support for 32-bit architecture
 sudo dpkg --add-architecture armhf
-sudo add-apt-repository ppa:alexlarsson/flatpak
-sudo apt update && sudo apt upgrade -y && sudo apt autoremove -y
-sudo apt install wget indicator-cpufreq flatpak gnome-software-plugin-flatpak openssh-sftp-server fonts-migmix fonts-noto-color-emoji -y
-#grep -qxF 'export QT_QPA_PLATFORMTHEME=gtk2' ~/.profile || echo 'export QT_QPA_PLATFORMTHEME=gtk2' | sudo tee --append ~/.profile
 
+#bionic's flatpak app is out of date, i'll probably leave this line in even after the focal upgrade as long as it's not hurting anything
+sudo add-apt-repository ppa:alexlarsson/flatpak
+
+#updates whee
+sudo apt update && sudo apt upgrade -y && sudo apt autoremove -y
+
+#install some recommended dependencies - the fonts packages are there to support a lot of symbols and foreign language characters
+sudo apt install wget indicator-cpufreq flatpak gnome-software-plugin-flatpak openssh-sftp-server fonts-migmix fonts-noto-color-emoji -y
+
+#automatically sets QT applications to follow the system theme
+grep -qxF 'export QT_QPA_PLATFORMTHEME=gtk2' ~/.profile || echo 'export QT_QPA_PLATFORMTHEME=gtk2' | sudo tee --append ~/.profile
+#and now i force it on anyway so that the user doesn't have to reboot to see the effect
+export QT_QPA_PLATFORMTHEME=gtk2
+
+#if you're reading this and i haven't credited Faster Melee for this file in the README yet, please yell at me -Cobalt
 echo "Installing support for Wii U/Switch Nintendo Gamecube controller adapters..."
 cd /etc/udev/rules.d/
 wget https://raw.githubusercontent.com/cobalt2727/L4T-Megascript/master/assets/51-gcadapter.rules
 cd ~
 
+#kinda hard to install flatpaks without flathub
 sudo flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
 sleep 1
 
@@ -61,7 +74,9 @@ clear
 echo "Placeholder for the overclock script (Coming Soon™)"
 sleep 5
 
-echo "All done! Would you like to restart now? (y/n)"
+echo "All done! Would you like to restart now?"
+echo "Required for some of the init script's components to take effect. (y/n)"
+sleep 1
 read -p "Make a selection: " userInput
 
 if [[ $userInput == y || $userInput == Y ]]; then
