9bcc392193ad657dc39255b4197b5328423038f4
cobalt2727 <60624944+cobalt2727@users.noreply.github.com>
2020-09-30 15:38:49 -0400

    tentative OptiFine support
    man
    
    i gotta figure out how to make it automatically get the newest version
    maybe i could get in contact with sp614x somehow

--- scripts/minecraft-java.sh
+++ scripts/minecraft-java.sh
@@ -3,8 +3,13 @@ echo "Minecraft Java script started!"
 sleep 1
 cd ~
 mkdir minecraft-temp && cd minecraft-temp
+echo "Downloading dependencies..."
+##these should do the trick - jarwrapper is included to make it so that .jar files (like OptiFine) can be run with ./[filepath]/[filename] like .deb files
+sudo apt install default-jre default-jdk jarwrapper -i
+
 echo "Downloading Luke Chambers's MCSwitchTools GitLab repo..."
-##i wish gitlab supported svn
+##i wish gitlab supported svn.
+##but noooooo.
 git clone https://gitlab.com/devluke/mcswitchtools
 cd mcswitchtools/
 echo "Moving the Minecraft files to the $HOME/ folder..."
@@ -23,8 +28,40 @@ sleep 10
 ##sudo jetson_clocks
 mc run
 sleep 3
-mc profiles
 
+
+##ask user if they want optifine, sodium, or both
+
+##if they answer optifine
+#############################
+echo "Downloading and installing OptiFine..."
+echo "If the script automatically installs a version that's not up to date,"
+echo "let us know on GitHub or Discord - we haven't figured out how to automate updating yet..."
+cd ~
+wget https://optifine.net/downloadx?f=OptiFine_1.16.3_HD_U_G3.jar&x=ba40d84b0b4333b35fde7329aa59ba31
+sudo chmod +x OptiFine_1.16.3_HD_U_G3.jar
+echo "You will now need to click Install and then exit the program when prompted."
+echo "Please do so when the installer launches in 10 seconds."
+./OptiFine_1.16.3_HD_U_G3.jar
+sudo rm OptiFine_1.16.3_HD_U_G3.jar
+
+sleep 1
+echo "Fixing profiles for use with the Switch..."
+mc profiles
+#############################
+
+##if they answer sodium
+#############################
+##sodium install script
+#############################
+
+##if they answer neither
+#############################
+##echo "Skipping Minecraft game optimizations."
+
+##echo "Please do not message us asking why the game is lagging until you rerun the script and choose one or both optimization programs."
+##echo "Enjoy your low framerates..."
+#############################
 
 ##note: i have a .desktop file already made, but it's back home on an SD card and i'm currently at college.
 ##oops. -cobalt
