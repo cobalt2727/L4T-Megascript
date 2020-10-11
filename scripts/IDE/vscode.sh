7902f01260ac732a13957dfcf12bd9f010c9fb8d
cobalt2727 <60624944+cobalt2727@users.noreply.github.com>
2020-10-10 22:40:24 -0400

    Create vscode.sh
    here's an idea

---
+++ scripts/IDE/vscode.sh
@@ -0,0 +1,15 @@
+clear
+echo "Installing Visual Studio Code directly from Microsoft..."
+echo "(This script is an automation of https://code.visualstudio.com/docs/setup/linux)"
+sleep 5
+
+##i'd be surprised if these weren't already installed, but...
+sudo apt install wget gpg -y
+
+wget -qO- https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > packages.microsoft.gpg
+sudo install -o root -g root -m 644 packages.microsoft.gpg /etc/apt/trusted.gpg.d/
+sudo sh -c 'echo "deb [arch=amd64 signed-by=/etc/apt/trusted.gpg.d/packages.microsoft.gpg] https://packages.microsoft.com/repos/vscode stable main" > /etc/apt/sources.list.d/vscode.list'
+
+sudo apt-get install apt-transport-https
+sudo apt-get update
+sudo apt install code
