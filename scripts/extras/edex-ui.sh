e8f0ecaa453816f0ae1741c568e318d2aab1a7e2
cobalt2727 <60624944+cobalt2727@users.noreply.github.com>
2020-10-21 20:01:17 -0400

    Update edex-ui.sh
    customization info

--- scripts/extras/edex-ui.sh
+++ scripts/extras/edex-ui.sh
@@ -16,6 +16,12 @@ npm run build-linux
 
 npm run install-linux
 
+echo ""
+echo ""
 echo "We'll make a desktop file later, but to launch the program, navigate to /home/$USER/edex-ui/ and type 'npm start'"
+echo "Settings can be modified by changing /home/$USER/.config/eDEX-UI/settings.json"
+echo "Available themes to apply in the settings text file can be listed by typing 'ls /home/$USER/.config/eDEX-UI/themes'"
+echo "We'll eventually hopefully do this by default, but you can turn off the mouse pointer by setting the 'nocursor' variable to true inside the settings.json file"
+
 #note: the icon should already be in an assets folder somewhere
 cd ~
