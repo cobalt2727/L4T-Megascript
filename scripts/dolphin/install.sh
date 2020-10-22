f5c666c2b402cb20dc60cac9b2eb1bf4f6ad421b
cobalt2727 <60624944+cobalt2727@users.noreply.github.com>
2020-10-22 00:25:28 -0400

    manual building integrated
    still uses the PPA for 18.04 though

--- scripts/dolphin/install.sh
+++ scripts/dolphin/install.sh
@@ -1,36 +1,40 @@
-echo "Making sure software-properties-common is installed..."
-sudo apt install software-properties-common
-#echo "looking to see if Dolphin's PPA already exists..."
-
-#if ! grep -q "^deb .*dolphin-emu/ppa" /etc/apt/sources.list /etc/apt/sources.list.d/*; then
-    #echo "Dolphin PPA not found, installing now!"
-echo "Making sure the Dolphin PPA is installed..."
-    sudo apt-add-repository ppa:dolphin-emu/ppa
-#else
-    #echo "You've already installed the PPA, moving on to checking for updates..."
-
-#fi
-sudo apt update
-sudo apt install dolphin-emu-master -y
-echo
-echo
-echo "NOTE: If you already have dolphin installed, you don't need to"
-echo "check this script to update - just press 1 on the main menu and"
-echo "Dolphin will get updated alongside everything else!"
-sleep 10
-
-
-echo "Sending you back to the main menu..."
+clear
+echo "Dolphin script started!"
 sleep 1
 
-##cd ~
-##sudo apt install git cmake ffmpeg libavcodec-dev libevdev-dev libusb-1.0-0-dev libavformat-dev libswscale-dev libsfml-dev libminiupnpc-dev libmbedtls-dev curl libhidapi-dev libpangocairo-1.0-0 libgtk2.0-dev libbluetooth-dev qt5-default qtbase5-private-dev libudev-dev portaudio19-dev -y
-##git clone https://github.com/dolphin-emu/dolphin
-##cd dolphin
-##mkdir build && cd build
-##cmake .. -DCMAKE_BUILD_TYPE=Release -DCMAKE_CXX_FLAGS=-march=armv8-a+simd -DCMAKE_C_FLAGS=-march=armv8-a+simd
-##make -j$(nproc)
-##sudo make install
-##wget default configs, maybe autolaunch Wii sports with real Wii remotes forced on
-##cd ../..
-##sudo rm -rf dolphin
+if grep -q bionic /etc/os-release; then
+	echo "Ubuntu 18.04 detected, using the PPA..."
+    
+    echo "Making sure software-properties-common is installed..."
+    sudo apt install software-properties-common -y
+
+    echo "Making sure the Dolphin PPA is installed..."
+    sudo apt-add-repository ppa:dolphin-emu/ppa -y
+
+    sudo apt update
+    sudo apt install dolphin-emu-master -y
+
+else
+	echo "Building from source with armv8 optimizations..."
+
+    cd ~
+    echo "Installing dependencies..."
+    sleep 1
+    sudo apt update
+    sudo apt install git cmake ffmpeg libavcodec-dev libevdev-dev libusb-1.0-0-dev libavformat-dev libswscale-dev libsfml-dev libminiupnpc-dev libmbedtls-dev curl libhidapi-dev libpangocairo-1.0-0 libgtk2.0-dev libbluetooth-dev qt5-default qtbase5-private-dev libudev-dev portaudio19-dev -y
+    echo "Downloading the source..."
+    git clone https://github.com/dolphin-emu/dolphin
+    cd dolphin
+    mkdir build && cd build
+    echo "Building..."
+    cmake .. -DCMAKE_BUILD_TYPE=Release -DCMAKE_CXX_FLAGS=-march=armv8-a+simd -DCMAKE_C_FLAGS=-march=armv8-a+simd
+    make -j$(nproc)
+    echo "Installing..."
+    sudo make install
+    #wget any game-specific configs, maybe autolaunch stuff like Wii sports with real Wii remotes forced on by default
+    cd ../..
+    sudo rm -rf dolphin
+fi
+echo "Done!"
+echo "Sending you back to the main menu..."
+sleep 2
