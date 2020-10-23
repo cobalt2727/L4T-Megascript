c6a3c204bcdef7e9c08fc05213e22cfc75022f18
cobalt2727 <60624944+cobalt2727@users.noreply.github.com>
2020-10-23 02:13:48 -0400

    Create thonny.sh
    cross another one off the list
    ...I should figure out what to do about Eclipse

---
+++ scripts/IDE/thonny.sh
@@ -0,0 +1,7 @@
+echo "Thonny script started!"
+echo "This one's fairly straightforward."
+echo "Installing dependencies..."
+sudo apt install python2 python3 thonny -y
+echo "Done!"
+echo "Sending you back to the main menu..."
+sleep 1
