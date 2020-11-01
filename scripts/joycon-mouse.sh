2d0eae77ea7360047186f22d8735a3452f6f153f
cobalt2727 <60624944+cobalt2727@users.noreply.github.com>
2020-11-01 15:01:13 -0500

    -y
    oops

--- scripts/joycon-mouse.sh
+++ scripts/joycon-mouse.sh
@@ -2,7 +2,7 @@ clear
 echo "Joycon mouse script started!"
 sleep 1
 cd ~
-sudo apt install xserver-xorg-input-joystick
+sudo apt install xserver-xorg-input-joystick -y
 wget https://cdn.discordapp.com/attachments/604648722491768883/684926248505704484/50-joystick.conf && sudo mv 50-joystick.conf /usr/share/X11/xorg.conf.d
 
 echo "Done! Restart your Switch when you're ready to gain access to using your joycons as a mouse."
