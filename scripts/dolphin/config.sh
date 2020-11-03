44747907196639f4dbfd3d38cb1efb9054dd4c29
cobalt2727 <60624944+cobalt2727@users.noreply.github.com>
2020-11-03 17:03:17 -0500

    Settings import script completed
    ...probably

--- scripts/dolphin/config.sh
+++ scripts/dolphin/config.sh
@@ -1,10 +1,22 @@
+##Import config files
 cd ~
-cd .config
+cd .config/
 sudo mkdir dolphin-emu
 sudo svn export https://github.com/$repository_username/L4T-Megascript/trunk/assets/Dolphin-Config
-cd Dolphin-Config
+cd Dolphin-Config/
 ls
 sudo mv *.ini ../dolphin-emu/
 cd ..
-sudo rm -rf Dolphin-Config
+sudo rm -rf Dolphin-Config/
 cd ~
+
+##Import themes, game-specific settings, etc
+##why does dolphin store these separately from the above files on Linux smh
+cd .local/share/
+sudo mkdir dolphin-emu
+cd dolphin-emu/
+sudo svn export https://github.com/$repository_username/L4T-Megascript/trunk/assets/Dolphin
+cd Dolphin/
+sudo mv * ..
+cd ..
+sudo rm -rf Dolphin/
