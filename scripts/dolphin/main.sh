fcb82dbf208e2f792bf77d12fcaef744a1042217
cobalt2727 <60624944+cobalt2727@users.noreply.github.com>
2020-11-03 16:49:41 -0500

    Update main.sh

--- scripts/dolphin/main.sh
+++ scripts/dolphin/main.sh
@@ -1,29 +1,32 @@
 echo "Dolphin script successfully started!"
 sleep 1
-##echo "What would you like to do?"
-##echo
-##echo
-##echo "1...............Install Dolphin from the Ubuntu PPA (use the updater on the main menu to update!)"
-##echo "2...............Download my latest autoconfigs for Dolphin for the Switch"
-##echo "  -graphics settings"
-##echo "  -game-specific controller profiles"
-##echo "  -custom user interface"
-##echo "3...............NOT IMPLEMENTED YET AND WON'T BE FOR A WHILE - Build other variants of Dolphin from source (Primehack, Project+, etc)"
-##echo "any other key...Close the Dolphin script and return to the main menu"
-##echo
-##echo
+echo "What would you like to do?"
+echo
+echo
+echo "1...............Install Dolphin (use the updater on the main menu to update!)"
+##echo "2...............Build other variants of Dolphin from source (Primehack, Project+, etc)"
+echo "any other key...Close the Dolphin script and return to the main menu"
+echo
+echo
 
-##read -p "Make a selection: " userInput
+read -p "Make a selection: " userInput
 
-##echo "you have chosen $userInput"
-##if [[ $userInput == 1 ]]; then
+echo "you have chosen $userInput"
+if [[ $userInput == 1 ]]; then
+  cd ~
   bash -c "$(curl -s https://raw.githubusercontent.com/$repository_username/L4T-Megascript/$repository_branch/scripts/dolphin/install.sh)"
+  bash -c "$(curl -s https://raw.githubusercontent.com/$repository_username/L4T-Megascript/$repository_branch/scripts/dolphin/config.sh)"
 
-##elif [[ $userInput == 2 ]]; then
-##  bash -c "$(curl -s https://raw.githubusercontent.com/$repository_username/L4T-Megascript/$repository_branch/scripts/dolphin/config.sh)"
 
-##else
-##  echo ""
+elif [[ $userInput == 2 ]]; then
+  echo "not ready yet"
+  sleep 3
+  ##bash -c "$(curl -s https://raw.githubusercontent.com/$repository_username/L4T-Megascript/$repository_branch/scripts/dolphin/config.sh)"
+
+else
+  echo ""
   
-##fi
+fi
+
 echo "Sending you back to the main menu..."
+sleep 3
