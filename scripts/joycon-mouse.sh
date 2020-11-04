1bb02c8af79e2aa65d7e13e0f453a8457abbf68a
theofficialgman <28281419+theofficialgman@users.noreply.github.com>
2020-11-04 16:40:54 -0500

    change joystick.conf to one made by garrett
    This 50-joystick.conf has comments which tell the user what everything does and how to modify it themselves as well as has more features and is compatible with old and new joycond versions

--- scripts/joycon-mouse.sh
+++ scripts/joycon-mouse.sh
@@ -3,7 +3,8 @@ echo "Joycon mouse script started!"
 sleep 1
 cd ~
 sudo apt install xserver-xorg-input-joystick -y
-wget https://cdn.discordapp.com/attachments/604648722491768883/684926248505704484/50-joystick.conf && sudo mv 50-joystick.conf /usr/share/X11/xorg.conf.d
+sudo rm -rf /usr/share/X11/xorg.conf.d/50-joystick.conf
+wget https://cdn.discordapp.com/attachments/527677698672427008/770290009638174750/50-joystick.conf && sudo mv 50-joystick.conf /usr/share/X11/xorg.conf.d
 
 echo "Done! Restart your Switch when you're ready to gain access to using your joycons as a mouse."
 
