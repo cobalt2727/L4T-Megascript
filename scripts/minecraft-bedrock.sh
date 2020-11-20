9a9eeb24fd9b02c6202e6dd8fe0d8fcaf81641b9
cobalt2727 <60624944+cobalt2727@users.noreply.github.com>
2020-11-20 14:06:54 -0500

    bug fixing

--- scripts/minecraft-bedrock.sh
+++ scripts/minecraft-bedrock.sh
@@ -12,8 +12,12 @@ sudo apt install pulseaudio glibc-source flatpak libcanberra-gtk-module -y
 sudo flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
 echo "Installing Minecraft Bedrock..."
 sudo flatpak install flathub io.mrarm.mcpelauncher -y
-#fix up an issue with running flatpaks by enabling non-privileged user namespaces
+
+#fix up an issue with the L4T kernel not being able to run flatpaks by enabling non-privileged user namespaces
 sudo chmod u+s /usr/libexec/flatpak-bwrap
+
+#according to the Minecraft PE Linux Launcher Discord, this line is needed to ensure the flatpak saves server lists and skins after a launcher restart
+sudo flatpak override --persist=../../../data io.mrarm.mcpelauncher
 
 echo "Done!"
 sleep 1
