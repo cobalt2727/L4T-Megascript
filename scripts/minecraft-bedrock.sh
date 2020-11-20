48b580667b66f127f319ef525d865a98127aec81
cobalt2727 <60624944+cobalt2727@users.noreply.github.com>
2020-11-20 00:41:17 -0500

    IT'S ALIIIIIIIIIIIIIIIVE
    shoutouts to @theofficialgman for being better at Google than me

--- scripts/minecraft-bedrock.sh
+++ scripts/minecraft-bedrock.sh
@@ -1,6 +1,5 @@
 clear
 echo "Minecraft Bedrock script started!"
-echo "Currently this installs correctly, but we're having trouble getting it to run properly."
 sleep 1
 echo "Updating sources..."
 sleep 1
@@ -13,6 +12,8 @@ sudo apt install pulseaudio glibc-source flatpak libcanberra-gtk-module -y
 sudo flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
 echo "Installing Minecraft Bedrock..."
 sudo flatpak install flathub io.mrarm.mcpelauncher -y
+#fix up an issue with running flatpaks by enabling non-privileged user namespaces
+sudo chmod u+s /usr/libexec/flatpak-bwrap
 
 echo "Done!"
 sleep 1
