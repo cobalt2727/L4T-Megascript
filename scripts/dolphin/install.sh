60b98999a4469130c97e1b248de55a98e4383958
cobalt2727 <60624944+cobalt2727@users.noreply.github.com>
2020-12-05 22:37:50 -0600

    Update install.sh

--- scripts/dolphin/install.sh
+++ scripts/dolphin/install.sh
@@ -1,46 +1,29 @@
 clear
 echo "Dolphin script started!"
 sleep 1
-
-if grep -q bionic /etc/os-release; then
-	echo "Ubuntu 18.04 detected, using the PPA..."
-    
-    echo "Making sure software-properties-common is installed..."
-    sudo apt install software-properties-common -y
-
-    echo "Making sure the Dolphin PPA is installed..."
-    sudo apt-add-repository ppa:dolphin-emu/ppa -y
-
-    sudo apt update
-    sudo apt install dolphin-emu-master -y
-
-else
-	echo "Building from source with device-specific optimizations..."
-
-    cd ~
-    echo "Installing dependencies..."
-    sleep 1
-    sudo apt update
-    sudo apt install git cmake ffmpeg libavcodec-dev libevdev-dev libusb-1.0-0-dev libavformat-dev libswscale-dev libsfml-dev libminiupnpc-dev libmbedtls-dev curl libhidapi-dev libpangocairo-1.0-0 libgtk2.0-dev libbluetooth-dev qt5-default qtbase5-private-dev libudev-dev portaudio19-dev libavutil-dev libxrandr-dev libxi-dev  -y
-    echo "Downloading the source..."
-    git clone https://github.com/dolphin-emu/dolphin
-    cd dolphin
-    git pull
-    mkdir build
-    cd build
-    echo "Building..."
-    #if you're looking at this script as a reference for building Dolphin on your own hardware,
-    #you can do "cmake .." and nothing else on the next line for a slight performance hit with a much faster build time
-    cmake .. -D ENABLE_LTO=1 -DCMAKE_BUILD_TYPE=Release -DCMAKE_CXX_FLAGS=-march=native -DCMAKE_C_FLAGS=-march=native -DCMAKE_C_FLAGS_INIT="-static"
-    make -j$(nproc)
-    echo "Installing..."
-    sudo make install
-    #wget any game-specific configs, maybe autolaunch stuff like Wii sports with real Wii remotes forced on by default
-    cd ~
-    #commenting out the below line since the first build takes way too long to do on weak hardware like the Switch
-    #leaving the source folder there will make future builds faster
-    ##sudo rm -rf dolphin
-fi
+cd ~
+echo "Installing dependencies..."
+sleep 1
+sudo apt update
+sudo apt install git cmake ffmpeg libavcodec-dev libevdev-dev libusb-1.0-0-dev libavformat-dev libswscale-dev libsfml-dev libminiupnpc-dev libmbedtls-dev curl libhidapi-dev libpangocairo-1.0-0 libgtk2.0-dev libbluetooth-dev qt5-default qtbase5-private-dev libudev-dev portaudio19-dev libavutil-dev libxrandr-dev libxi-dev  -y
+echo "Downloading the source..."
+git clone https://github.com/dolphin-emu/dolphin
+cd dolphin
+git pull
+mkdir build
+cd build
+echo "Building..."
+#if you're looking at this script as a reference for building Dolphin on your own hardware,
+#you can do "cmake .." and nothing else on the next line for a slight performance hit with a much faster build time
+cmake .. -D ENABLE_LTO=1 -DCMAKE_BUILD_TYPE=Release -DCMAKE_CXX_FLAGS=-march=native -DCMAKE_C_FLAGS=-march=native -DCMAKE_C_FLAGS_INIT="-static"
+make -j$(nproc)
+echo "Installing..."
+sudo make install
+#wget any game-specific configs, maybe autolaunch stuff like Wii sports with real Wii remotes forced on by default
+cd ~
+#commenting out the below line since the first build takes way too long to do on weak hardware like the Switch
+#leaving the source folder there will make future builds faster
+##sudo rm -rf dolphin
 echo "Done!"
 echo "Sending you back to the main menu..."
 sleep 2
