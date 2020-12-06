1e3b711a79cecb77c82bb44b9b3dacb2c667cf6b
cobalt2727 <60624944+cobalt2727@users.noreply.github.com>
2020-12-05 22:47:43 -0600

    PPA moved from install.sh to main.sh
    Since install.sh is designed to work regardless of whether you're on a Switch or not, I figured it would make more sense to move the PPA install method to main.sh
    also i added a bunch of placeholders for other dolphin builds

--- scripts/dolphin/main.sh
+++ scripts/dolphin/main.sh
@@ -13,15 +13,47 @@ read -p "Make a selection: " userInput
 
 echo "you have chosen $userInput"
 if [[ $userInput == 1 ]]; then
-  cd ~
-  bash -c "$(curl -s https://raw.githubusercontent.com/$repository_username/L4T-Megascript/$repository_branch/scripts/dolphin/install.sh)"
-  bash -c "$(curl -s https://raw.githubusercontent.com/$repository_username/L4T-Megascript/$repository_branch/scripts/dolphin/config.sh)"
+  
+  
+  if grep -q bionic /etc/os-release; then
+	  echo "Ubuntu 18.04 detected, using the PPA..."
+    
+    echo "Making sure software-properties-common is installed..."
+    sudo apt install software-properties-common -y
 
+    echo "Making sure the Dolphin PPA is installed..."
+    sudo apt-add-repository ppa:dolphin-emu/ppa -y
+
+    sudo apt update
+    sudo apt install dolphin-emu-master -y
+
+  else
+	  echo "Building from source with device-specific optimizations..."
+
+    cd ~
+    bash -c "$(curl -s https://raw.githubusercontent.com/$repository_username/L4T-Megascript/$repository_branch/scripts/dolphin/install.sh)"
+  fi
+  bash -c "$(curl -s https://raw.githubusercontent.com/$repository_username/L4T-Megascript/$repository_branch/scripts/dolphin/config.sh)"
 
 elif [[ $userInput == 2 ]]; then
   echo "not ready yet"
   sleep 3
-  ##bash -c "$(curl -s https://raw.githubusercontent.com/$repository_username/L4T-Megascript/$repository_branch/scripts/dolphin/config.sh)"
+  ##bash -c "$(curl -s https://raw.githubusercontent.com/$repository_username/L4T-Megascript/$repository_branch/scripts/dolphin/primehack.sh)"
+
+elif [[ $userInput == 3 ]]; then
+  echo "not ready yet"
+  sleep 3
+  ##bash -c "$(curl -s https://raw.githubusercontent.com/$repository_username/L4T-Megascript/$repository_branch/scripts/dolphin/slippi.sh)"
+
+elif [[ $userInput == 4 ]]; then
+  echo "not ready yet"
+  sleep 3
+  ##bash -c "$(curl -s https://raw.githubusercontent.com/$repository_username/L4T-Megascript/$repository_branch/scripts/dolphin/primehack.sh)"
+
+elif [[ $userInput == 5 ]]; then
+  echo "not ready yet"
+  sleep 3
+  ##bash -c "$(curl -s https://raw.githubusercontent.com/$repository_username/L4T-Megascript/$repository_branch/scripts/dolphin/projectplus.sh)"
 
 else
   echo ""
