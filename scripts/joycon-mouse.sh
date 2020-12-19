7ca6ec44284cc3727b47e48e8b0a0e3f5e0cdd3b
cobalt2727 <60624944+cobalt2727@users.noreply.github.com>
2020-12-19 16:25:41 -0600

    add wget as a dependency

--- scripts/joycon-mouse.sh
+++ scripts/joycon-mouse.sh
@@ -2,7 +2,7 @@ clear
 echo "Joycon mouse script started!"
 sleep 1
 cd ~
-sudo apt install xserver-xorg-input-joystick -y
+sudo apt install xserver-xorg-input-joystick wget -y
 sudo rm -rf /usr/share/X11/xorg.conf.d/50-joystick.conf
 wget https://cdn.discordapp.com/attachments/527677698672427008/770290009638174750/50-joystick.conf && sudo mv 50-joystick.conf /usr/share/X11/xorg.conf.d
 
