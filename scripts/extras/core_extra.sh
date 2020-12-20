3b915e1189b45975eee6d864ae69899b8681e29d
cobalt2727 <60624944+cobalt2727@users.noreply.github.com>
2020-12-20 02:39:07 -0600

    Update core_extra.sh

--- scripts/extras/core_extra.sh
+++ scripts/extras/core_extra.sh
@@ -1,3 +1,29 @@
 clear
-echo "placeholder"
+
+echo "Ah, looks like someone's read the source code."
+echo "Or maybe someone else just told you this was a thing."
+echo "That's cheating..."
+echo "Anyway, here's a handful of a few fun extra scripts that didn't quite"
+echo "make the cut for the main menu, generally due to their niche use cases."
+echo "Enjoy!"
 sleep 5
+
+echo "Your options are:"
+echo "- eDEX-UI:"
+echo "  fullscreen terminal and system monitor that looks like a sci-fi computer interface."
+echo
+echo "- Prusaslicer:"
+echo "  G-code generator for 3D printers (RepRap, Makerbot, Ultimaker etc)."
+echo
+echo "- Terminal Star Wars:"
+echo "  this script literally just runs 'telnet towel.blinkenlights.nl and that's it."
+echo
+
+sleep 5
+echo "actual choice selection not added yet"
+echo "it's almost 3 AM and i want to sleep, sue me -cobalt"
+echo "this'll get worked out soon don't you worry"
+echo
+sleep 5
+echo "Sending you back to the main menu...
+sleep 1
