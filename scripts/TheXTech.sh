1543cab135ba9a14838aaa4ba9782723b57f824e
pancakers2001 <76455320+pancakers2001@users.noreply.github.com>
2020-12-21 21:42:26 -0600

    Update TheXTech.sh
    fixed color text again

--- scripts/TheXTech.sh
+++ scripts/TheXTech.sh
@@ -1,7 +1,7 @@
 cd
 clear
 echo "TheXTech script started!"
-echo -e "\e[0mMMMMMMMMMMmmmmmmmmmmmmMMMMMMMMMM
+echo -e "\e[31mMMMMMMMMMMmmmmmmmmmmmmMMMMMMMMMM
 MMMMMMmmmm    hhhh    mmmmMMMMMM
 MMMMmmhh      yyyy      hhmmMMMM
 MMmmhhyy    yyyyyyyy    yyhhmmMM
@@ -30,7 +30,7 @@ git pull
 mkdir build
 cd build
 cmake -G Ninja -DCMAKE_BUILD_TYPE=Debug -DUSE_SYSTEM_LIBS=OFF -DUSE_FREEIMAGE_SYSTEM_LIBS=ON ..
-echo -e "\e[0mThis will take a while, please wait, if it seems like it is stuck it is not so please have patience.\e[0m"
+echo -e "\e[31mThis will take a while, please wait, if it seems like it is stuck it is not so please have patience.\e[0m"
 ninja
 cd
 mkdir -p ~/.PGE_Project/thextech/
