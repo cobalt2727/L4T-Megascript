493856d02564eaf2ce7102fefaf1231d21827e52
pancakers2001 <76455320+pancakers2001@users.noreply.github.com>
2020-12-23 15:11:21 -0600

    Update SM64.sh
    changed the ascii art to a star instead of a mario head

--- scripts/SM64.sh
+++ scripts/SM64.sh
@@ -1,21 +1,21 @@
 clear
 echo "SuperMario64 Port script started!"
-echo -e "\e[31m            yyyy                
-          //sssssshh            
-        ++::--ooooyyhh          
-        ::::::ssoossyyhh        
-      hh++++ooyyyyyyyyhhhh      
-    mmmmddyyhhhhhhhhhhhhhhdd    
-      ddhhyyddhhhhhhdddddddd    
-        ----------::ssdddddddd  
-        //``ss//--//hhdd::::ss  
-    --////..ss::--ooddoo//++//  
-  --..::++::sshh:://dd//oo::::  
-  //::++ssmmmmmm////ss//::--++  
-    oohhNNMMmm:://++++ooss      
-      mmhhyy++//++oossyyNNNN    
-        ss++++ssssyyyymmNNNN    
-          oossyyyyyy            \e[0m"
+echo -e "\e[1;33m              ::++              
+              --++              
+            ::..//yy            
+            --..//oo            
+        ::--....::++++++        
+::--::::--------:://:::::::://oo
+oo::........hh::++mm::::::++sshh
+  yy++::----mm++++mm:://ooyydd  
+    yy++//::mm++++NN++oohhdd    
+      ++::::dd////dd++sshh      
+      //----::////////oohh      
+      ::--:://++++////++yy      
+      --::++ssssss++//++ss      
+    ::::++yyhhhhhhyyoo++oohh    
+    //oohhhh        hhyyssyy    
+    sshh                hhhh    \e[0m"
 echo "Downloading the files, removing old files and installing needed dependencies..."
 cd ~/RetroPie/roms/ports 
 rm SM64.sh
