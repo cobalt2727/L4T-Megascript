1fe9ce0a5a50cbd292c88fe2f4b24d1853dc7eea
cobalt2727 <60624944+cobalt2727@users.noreply.github.com>
2021-02-02 09:21:50 -0500

    make sure the user has joycond installed

--- scripts/init.sh
+++ scripts/init.sh
@@ -18,7 +18,7 @@ sudo add-apt-repository ppa:alexlarsson/flatpak -y
 sudo apt update && sudo apt upgrade -y && sudo apt autoremove -y
 
 #install some recommended dependencies - the fonts packages are there to support a lot of symbols and foreign language characters
-sudo apt install subversion wget indicator-cpufreq flatpak gnome-software-plugin-flatpak openssh-sftp-server fonts-migmix fonts-noto-color-emoji qt5-style-plugins gnutls-bin -y
+sudo apt install joycond subversion wget indicator-cpufreq flatpak gnome-software-plugin-flatpak openssh-sftp-server fonts-migmix fonts-noto-color-emoji qt5-style-plugins gnutls-bin -y
 hash -r
 
 #automatically sets QT applications to follow the system theme
