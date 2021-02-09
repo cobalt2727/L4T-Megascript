b9c0bb0d207d84ef68d3d50c5e8851aae54c4f32
cobalt2727 <60624944+cobalt2727@users.noreply.github.com>
2021-02-09 12:24:05 -0500

    Update pycharm.sh

--- scripts/IDE/pycharm.sh
+++ scripts/IDE/pycharm.sh
@@ -1,3 +1,6 @@
+#!/bin/bash
+
+
 echo "https://www.element14.com/community/community/raspberry-pi/blog/2019/09/12/installing-pycharm-on-raspberry-pi"
 echo "i'm lazy"
 echo "don't worry this will happen eventually"
