af93757d07ff79f80546869b9ae372cc47980014
cobalt2727 <60624944+cobalt2727@users.noreply.github.com>
2021-02-25 13:50:20 -0500

    add python 2.7

--- scripts/IDE/thonny.sh
+++ scripts/IDE/thonny.sh
@@ -4,7 +4,7 @@
 echo "Thonny script started!"
 echo "This one's fairly straightforward."
 echo "Installing dependencies..."
-sudo apt install python2 python3 thonny -y
+sudo apt install python2 python2.7 python3 python3-pip thonny -y
 echo "Done!"
 echo "Sending you back to the main menu..."
 sleep 1
