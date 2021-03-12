49e49212a984a2107403a2d8f773e7fbdf8e3cb8
cobalt2727 <60624944+cobalt2727@users.noreply.github.com>
2021-03-11 20:05:18 -0500

    updater now fixes broken packages

--- scripts/update.sh
+++ scripts/update.sh
@@ -1,20 +1,39 @@
 #!/bin/bash
 
 clear
-echo "Updater script successfully started! Running updates..."
+echo "Updater script successfully started!"
+echo "Running APT updates..."
 sleep 1
 sudo apt update
 sudo apt upgrade -y
-sudo apt autoremove -y
 
+echo "Scanning for issues with APT packages..."
+echo "If you receive a yes/no prompt in the following steps,"
+echo "Make sure you carefully read over the"
+echo "packages to be changed before proceeding."
+echo "If not, don't worry about it."
+echo "Purging, cleaning, and autoremoving are NORMALLY"
+echo "fine, but double-check packages to be safe."
+##maintenance (not passing with -y to prevent potentially breaking something for a user)
+sudo dpkg --configure -a
+sudo apt autoremove
+sudo apt --fix-broken install
+sudo apt autoclean
+sudo apt autopurge
+
+echo "Updating Flatpak packages (if you have any)..."
 ##two separate flatpak updaters to catch all programs regardless of whether the user installed them for the system or just the user
 sudo flatpak update -y
 flatpak update -y
 
+echo "Updating NPM (if you have it)..."
 ##this updater will fail if the user doesn't have npm installed so there's no harm in adding it, as it's used by some scripts here
 sudo npm install -g npm
-##maybe somehow add a way to automatically re-run scripts for things that were built from source?
 
+echo "Anything you've installed from source will"
+echo "have to be manually recompiled until/unless we"
+echo "get an automatic updater for those programs running."
+##maybe somehow add a way to automatically re-run scripts for things that were built from source?
 
 
 ##echo "Placeholder for the RetroPie updater (we NEED to y/n prompt this if we decide to add it)"
