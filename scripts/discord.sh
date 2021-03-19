0239ab47b7dc875a74194b0986ff101f98bc6966
cobalt2727 <60624944+cobalt2727@users.noreply.github.com>
2021-03-18 21:36:56 -0400

    Create discord.sh
    hey this is pretty neat

---
+++ scripts/discord.sh
@@ -0,0 +1,28 @@
+echo "Discord aarch64 script started!"
+echo "CREDITS: https://github.com/SpacingBat3/electron-discord-webapp"
+sleep 2
+
+echo "Installing dependencies..."
+sudo apt install git curl -y
+
+cd ~
+mkdir discord-tmp/
+cd discord-tmp/
+
+echo "Downloading the most recent .deb from SpacingBat3's repository..."
+curl -s https://api.github.com/repos/SpacingBat3/electron-discord-webapp/releases/latest \
+  | grep "browser_download_url.*arm64.deb" \
+  | cut -d : -f 2,3 \
+  | tr -d \" \
+  | wget -qi -
+
+echo "Done! Installing the package (and maybe fixing dependencies)..."
+sudo dpkg -i *arm64.deb
+sudo apt --fix-broken install -y
+
+cd ..
+rm -rf discord-tmp/
+
+echo "Done!"
+echo "Sending you back to the menu..."
+sleep 1
