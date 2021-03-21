0e0d39744010d7b2feded62bc2ee05b052d3bcfc
theofficialgman <28281419+theofficialgman@users.noreply.github.com>
2021-03-21 19:50:15 -0400

    add .desktop to multimc and add to megascript gui

--- scripts/minecraft_java_multimc.sh
+++ scripts/minecraft_java_multimc.sh
@@ -15,3 +15,14 @@ cd build
 cmake -DCMAKE_INSTALL_PREFIX=../install -DMultiMC_META_URL:STRING="https://raw.githubusercontent.com/theofficialgman/meta-multimc/master/index.json" ../src
 # build & install (use -j with the number of cores your CPU has)
 make -j$(nproc) install
+cd
+sudo sh -c "cat > /usr/local/share/applications/MultiMC.desktop << _EOF_
+[Desktop Entry]
+Type=Application
+Exec=/home/$USER/MultiMC/install/MultiMC
+Hidden=false
+NoDisplay=false
+Name=MutiMC
+Icon=/home/$USER/MultiMC/src/application/resources/multimc/scalable/multimc.svg
+Categories=Game
+_EOF_"
