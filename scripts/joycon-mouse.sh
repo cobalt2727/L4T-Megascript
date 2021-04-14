f926fef5a91e4723b61b4bbd5d04302674387411
Garrett <gmesmer@comcast.net>
2021-04-14 17:02:27 -0400

    add 50-joystick.conf as an asset
    also separate joycon and pro controller so we can correctly map the pro controller dpad
    these configs will only activate on controllers that advertise as combined joycon and  switch pro controller

--- scripts/joycon-mouse.sh
+++ scripts/joycon-mouse.sh
@@ -7,7 +7,7 @@ sleep 1
 cd ~
 sudo apt install xserver-xorg-input-joystick wget -y
 sudo rm -rf /usr/share/X11/xorg.conf.d/50-joystick.conf
-wget https://cdn.discordapp.com/attachments/527677698672427008/770290009638174750/50-joystick.conf && sudo mv 50-joystick.conf /usr/share/X11/xorg.conf.d
+wget https://raw.githubusercontent.com/cobalt2727/L4T-Megascript/master/assets/50-joystick.conf && sudo mv 50-joystick.conf /usr/share/X11/xorg.conf.d
 
 echo "Done! Restart your Switch when you're ready to gain access to using your joycons as a mouse."
 
