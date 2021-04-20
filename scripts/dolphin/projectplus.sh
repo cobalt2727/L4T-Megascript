980c4f42f64db2131f49fb4eb5f15199f3fa27ef
cobalt2727 <60624944+cobalt2727@users.noreply.github.com>
2021-04-19 22:06:28 -0400

    disclaimer

--- scripts/dolphin/projectplus.sh
+++ scripts/dolphin/projectplus.sh
@@ -2,9 +2,15 @@
 
 clear
 echo "Project+ script started!"
-sleep 1
+echo "This has only been tested for building,"
+echo "not performance or actual netplay sessions."
+echo "In addition, building has only been tested on 20.10,"
+echo "so while I can confirm it compiles on ARM,"
+echo "I don't know if it'll build on 18.04 yet... -Cobalt"
+sleep 10
 
 echo "Installing standard Dolphin dependencies first since the list for P+ is... really small."
+sleep 1
 sudo apt install --no-install-recommends ca-certificates qtbase5-dev qtbase5-private-dev git cmake make gcc g++ pkg-config libavcodec-dev libavformat-dev libavutil-dev libswscale-dev libxi-dev libxrandr-dev libudev-dev libevdev-dev libsfml-dev libminiupnpc-dev libmbedtls-dev libcurl4-openssl-dev libhidapi-dev libsystemd-dev libbluetooth-dev libasound2-dev libpulse-dev libpugixml-dev libbz2-dev libzstd-dev liblzo2-dev libpng-dev libusb-1.0-0-dev gettext -y 
 
 echo "Installing dependencies specific to Project+"
