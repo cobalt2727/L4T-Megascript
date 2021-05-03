7dd22a8c27501fe55af6dd076e4559e33353762a
cobalt2727 <60624944+cobalt2727@users.noreply.github.com>
2021-05-03 19:50:33 -0400

    more udev
    i know the main script already has it but i generally send people install.sh to use on x86 computers by itself too

--- scripts/dolphin/install.sh
+++ scripts/dolphin/install.sh
@@ -7,7 +7,7 @@ cd ~
 echo "Installing dependencies..."
 sleep 1
 sudo apt update
-sudo apt install --no-install-recommends ca-certificates qtbase5-dev qtbase5-private-dev git cmake make gcc g++ pkg-config libavcodec-dev libavformat-dev libavutil-dev libswscale-dev libxi-dev libxrandr-dev libudev-dev libevdev-dev libsfml-dev libminiupnpc-dev libmbedtls-dev libcurl4-openssl-dev libhidapi-dev libsystemd-dev libbluetooth-dev libasound2-dev libpulse-dev libpugixml-dev libbz2-dev libzstd-dev liblzo2-dev libpng-dev libusb-1.0-0-dev gettext -y 
+sudo apt install --no-install-recommends ca-certificates qtbase5-dev qtbase5-private-dev git cmake make gcc g++ pkg-config udev libudev1 libavcodec-dev libavformat-dev libavutil-dev libswscale-dev libxi-dev libxrandr-dev libudev-dev libevdev-dev libsfml-dev libminiupnpc-dev libmbedtls-dev libcurl4-openssl-dev libhidapi-dev libsystemd-dev libbluetooth-dev libasound2-dev libpulse-dev libpugixml-dev libbz2-dev libzstd-dev liblzo2-dev libpng-dev libusb-1.0-0-dev gettext -y 
 
 
 echo "Downloading the source..."
