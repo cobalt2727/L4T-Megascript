c55b08e976f91b2ddd8182596ede5d25d955fd22
cobalt2727 <60624944+cobalt2727@users.noreply.github.com>
2021-05-05 16:34:45 -0400

    Add files via upload

---
+++ scripts/ocs-url.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+clear
+echo "ocs-url script started!"
+echo 'This will allow you to actually USE that fancy "Install"'
+echo "button on the Pling website and any of its derivatives."
+sleep 3
+
+cd ~
+rm -rf ocs-url/
+##using my fork temporarily until the original maintainer merges my changes to fix manual builds
+git clone https://www.opencode.net/cobalt2727/ocs-url
+
+cd ocs-url
+
+##switch over to my commits
+git checkout patch-1
+
+./scripts/prepare
+
+qmake PREFIX=/usr
+
+make -j$(nproc)
+
+sudo make install
+
+cd ~
+rm -rf ocs-url/
