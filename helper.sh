d374ae62da8d797c8c21222fd4190cffa3243026
cobalt2727 <60624944+cobalt2727@users.noreply.github.com>
2021-05-15 00:48:09 -0500

    add user guide

--- helper.sh
+++ helper.sh
@@ -15,3 +15,16 @@ else
 fi
 
 $2 bash -c "$(curl -s https://raw.githubusercontent.com/$repository_username/L4T-Megascript/$repository_branch/$1)"
+
+
+##DOCUMENTATION
+##  This file is here to help people easily run just one component of the megascript in
+##  their own setups (or even other projects! Just make sure to credit us somewhere).
+##  To run, let's say, the joycon-mouse file from this repository, which is in the 'scripts' folder, you
+##  would run the following line (without the hashtags or empty space in front):
+##        bash <( curl https://raw.githubusercontent.com/cobalt2727/L4T-Megascript/master/helper.sh ) "scripts/joycon-mouse.sh"
+
+##  Since barrier.sh is in another subfolder, to run that, you'd do something like:
+##        bash <( curl https://raw.githubusercontent.com/cobalt2727/L4T-Megascript/master/helper.sh ) "scripts/extras/barrier.sh"
+
+##  And obviously you need both bash and curl installed for these to work - the scripts should in most cases install everything else themselves.
