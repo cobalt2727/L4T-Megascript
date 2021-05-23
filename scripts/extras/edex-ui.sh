00bf99b6922b9cdf5c7e52b66727fdf2b0cef381
cobalt2727 <60624944+cobalt2727@users.noreply.github.com>
2021-05-23 04:00:53 -0500

    rework edex-ui *completely* to use their appimage
    i should have done this months ago really
    
    untested as of yet, but i see no reason why it wouldn't work (save for maybe missing dependencies)

--- scripts/extras/edex-ui.sh
+++ scripts/extras/edex-ui.sh
@@ -2,48 +2,27 @@ clear
 cd ~
 echo "eDEX-UI script started!"
 sleep 1
-echo "Removing previous installation files..."
-sudo rm -rf edex-ui/
-cd /usr/share/applications
-sudo rm "eDEX-UI.desktop"
+
 cd ~
-echo "Installing dependencies..."
-if grep -q bionic /etc/os-release; then
-  echo "Ubuntu 18.04 detected - no support for eDex-UI"
-  echo "(Believe me, I tried building it myself. If you've got a cross-compiled binary, make a PR! -Cobalt)"
-  echo "You'll have to wait for 20.04 to launch on L4T or, if it's out by the time you read this,"
-  echo "Reinstall L4T Ubuntu yourself."
-  sleep 5
-  exit 1
-fi
+sudo apt install curl -y
+rm -rf edex-ui-appimage
+mkdir edex-ui-appimage
+cd edex-ui-appimage
+curl https://api.github.com/repos/GitSquared/edex-ui/releases/latest | grep "browser_download_url.*Linux-arm64" | cut -d : -f 2,3 | tr -d \" | wget -qi -
+mv *.AppImage eDEX-UI.AppImage
+chmod +x *.AppImage
+curl https://api.github.com/repos/TheAssassin/AppImageLauncher/releases/latest | grep "browser_download_url.*bionic_arm64" | cut -d : -f 2,3 | tr -d \" | wget -qi -
+sudo dpkg -i *bionic_arm64.deb
+hash -r
+ail-cli integrate eDEX-UI.AppImage
+cd ~
+rm -rf edex-ui-appimage
 
-#everything is on separate lines here because the dependencies are kind of a mess
-sudo apt-get install libssl-dev -y
+echo "Install Dependencies..."
+cd ~
+echo "THIS DEPENDENCY LIST IS PROBABLY MISSING STUFF"
+echo "PLEASE FILE A GITHUB ISSUE LOGGING ANY ERRORS YOU RECEIVE WHEN RUNNING EDEX-UI"
 sudo apt-get install libssl1.* -y
-#libssl 2.anything doesn't even exist yet and probably won't for *years* but hey, i'm planning ahead?
-sudo apt-get install libssl2.* -y
-sudo apt-get install npm* nodejs* -y
 
-echo "Downloading the source..."
-git clone https://github.com/GitSquared/edex-ui
-cd edex-ui
-
-npm install
-##this next step gives me errors, but it works anyway...
-npm run build-linux
-
-npm run install-linux
-
-cd /home/$USER/edex-ui/media/
-sudo mkdir -p /usr/share/edex-ui
-sudo cp logo.svg /usr/share/edex-ui
-cd /usr/share/applications
-sudo wget https://raw.githubusercontent.com/cobalt2727/L4T-Megascript/master/assets/eDEX-UI/eDEX-UI.desktop
-
-echo ""
-echo ""
 echo "Done!"
-echo "Settings can be modified by changing /home/$USER/.config/eDEX-UI/settings.json"
-echo "Available themes to apply in the settings text file can be listed by typing 'ls /home/$USER/.config/eDEX-UI/themes'"
-echo "We'll eventually maybe do this by default, but you can turn off the mouse pointer by setting the 'nocursor' variable to true inside the settings.json file"
-
+echo "Sending you back to the main menu...
