1844d465ca8c42cf56b2c0f62a6226aa92466a2c
cobalt2727 <60624944+cobalt2727@users.noreply.github.com>
2021-05-23 03:49:07 -0500

    Update starwars.sh

--- scripts/extras/starwars.sh
+++ scripts/extras/starwars.sh
@@ -1,13 +1,7 @@
 sudo apt install telnet -y
 clear
-echo "The script will now run the command \e[32mtelnet towel.blinkenlights.nl\e[0m"
-echo "For more info on what this is and how"
-echo "it works, Ctrl+click the following link:"
-##echo "http://www.blinkenlights.nl/thereg/"
+echo "get some popcorn"
+echo "http://www.blinkenlights.nl/thereg/"
 echo "http://www.asciimation.co.nz/"
-echo "This message will close in 30 seconds,"
-echo "and the movie will start. Press Ctrl + C"
-echo "or click the X on the terminal window to cancel"
-echo "at any time. Enjoy!"
-sleep 30
+sleep 5
 telnet towel.blinkenlights.nl
