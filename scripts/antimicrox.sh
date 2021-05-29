cdb5925fad2e7088888b07db24300c8a48921306
Jerry Wester <JerryWester@users.noreply.github.com>
2021-05-29 02:04:05 -0600

    Update antimicrox.sh

--- scripts/antimicrox.sh
+++ scripts/antimicrox.sh
@@ -3,5 +3,14 @@
 # Dependancies
 sudo apt install git gcc cmake extra-cmake-modules \
     qttools5-dev qttools5-dev-tools libsdl2-dev \
-    libxi-dev libxtst-dev libx11-dev itstool gettext
+#   libxi-dev libxtst-dev \
+    libx11-dev itstool gettext -y
 
+# Cloning
+git clone https://github.com/AntiMicroX/antimicrox.git
+cd antimicrox
+mkdir build && cd build
+
+# Building
+cmake ..
+cmake --build .
