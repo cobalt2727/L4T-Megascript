a6d1f7152395992998628e6cbed0cafc4b4a303a
Jerry Wester <JerryWester@users.noreply.github.com>
2021-05-29 01:51:56 -0600

    Create antimicrox.sh

---
+++ scripts/antimicrox.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# Dependancies
+sudo apt install git gcc cmake extra-cmake-modules \
+    qttools5-dev qttools5-dev-tools libsdl2-dev \
+    libxi-dev libxtst-dev libx11-dev itstool gettext
+
