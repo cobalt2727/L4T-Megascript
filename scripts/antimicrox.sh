b793c163a22b2da7c12d687c1e11a61a3deb9de8
Jerry Wester <JerryWester@users.noreply.github.com>
2021-05-29 02:28:54 -0600

    Update antimicrox.sh

--- scripts/antimicrox.sh
+++ scripts/antimicrox.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
 
-echo "This script will install AntiMicroX, an updated fork of AntiMicro and
-    \na graphical program used to map gamepad keys to keyboard."
+echo "This script will install AntiMicroX, an updated fork of AntiMicro and"
+echo "a graphical program used to map gamepad keys to keyboard."
 
 # Dependancies
 sudo apt install -y git gcc cmake extra-cmake-modules \
     qttools5-dev qttools5-dev-tools libsdl2-dev \
-#   libxi-dev libxtst-dev \ # used for X11, XTest, and UInput support. Unsure if necessary.
     libx11-dev itstool gettext
 
 # Cloning
