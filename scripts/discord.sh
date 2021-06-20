5828290e91972881927b6c722815a46e179a68b5
theofficialgman <28281419+theofficialgman@users.noreply.github.com>
2021-06-19 22:06:31 -0400

    update discord/webcord to use apt repo
    this allows you to update webcord with apt upgrade
    will install over previous discord installs without removing user login info

--- scripts/discord.sh
+++ scripts/discord.sh
@@ -5,26 +5,29 @@ sleep 2
 echo "Installing dependencies..."
 sudo apt install git curl -y
 
-cd ~
-mkdir -p discord-tmp/
-cd discord-tmp/
+# cd ~
+# mkdir -p discord-tmp/
+# cd discord-tmp/
 
 echo "Removing previous legacy Discord installs..."
 sudo dpkg -r electron-discord-webapp
 
-echo "Downloading the most recent .deb from SpacingBat3's repository..."
-curl -s https://api.github.com/repos/SpacingBat3/WebCord/releases/latest \
-  | grep "browser_download_url.*arm64.deb" \
-  | cut -d : -f 2,3 \
-  | tr -d \" \
-  | wget -qi -
+# echo "Downloading the most recent .deb from SpacingBat3's repository..."
+# curl -s https://api.github.com/repos/SpacingBat3/WebCord/releases/latest \
+#   | grep "browser_download_url.*arm64.deb" \
+#   | cut -d : -f 2,3 \
+#   | tr -d \" \
+#   | wget -qi -
 
-echo "Done! Installing the package (and maybe fixing dependencies)..."
-sudo dpkg -i *arm64.deb
-sudo apt --fix-broken install -y
+# echo "Done! Installing the package (and maybe fixing dependencies)..."
+# sudo dpkg -i *arm64.deb
+# sudo apt --fix-broken install -y
 
-cd ..
-rm -rf discord-tmp/
+# cd ..
+# rm -rf discord-tmp/
+
+wget -qO- http://download.tuxfamily.org/webcordapt/add-repo.sh | sudo bash
+sudo apt install webcord -y
 
 echo "Done!"
 echo "Sending you back to the menu..."
