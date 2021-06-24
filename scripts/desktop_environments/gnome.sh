4a05ebc8ce6905f7b6c18802b92529ebd1bf2a1b
theofficialgman <28281419+theofficialgman@users.noreply.github.com>
2021-06-23 22:24:32 -0400

    check for gnome version
    this should allow for both focal (and newer) and bionic to install with the same script

--- scripts/desktop_environments/gnome.sh
+++ scripts/desktop_environments/gnome.sh
@@ -2,13 +2,25 @@
 clear
 echo "Installing the GNOME desktop environment."
 
-sudo apt install gnome-session gnome-tweaks gnome-tweak-tool chrome-gnome-shell wget curl jq unzip tput sed egrep gnome-shell-extension-tool sed awk cut basename -y
+# version function
+function version { echo "$@" | awk -F. '{ printf("%d%03d%03d\n", $1,$2,$3); }'; }
 
-echo "Installing Some Common Extensions"
+sudo apt install gnome-session gnome-tweaks gnome-tweak-tool chrome-gnome-shell gnome-shell wget curl jq unzip sed -y
+hash -r
+sdlv=$(dpkg -s gnome-shell | sed -n 's/Version: //p')
 
 cd ~
 rm -f ./install-gnome-extensions.sh
-wget -N -q "https://raw.githubusercontent.com/cyfrost/install-gnome-extensions/7ea5327e36c35e732c6c97887c08fe3596506727/install-gnome-extensions.sh" -O ./install-gnome-extensions.sh
+
+if [ $(version $sdlv) -ge $(version "3.34.0") ]; then
+  echo "Gnome 34 or newer detected"
+  wget -N -q "https://raw.githubusercontent.com/cyfrost/install-gnome-extensions/master/install-gnome-extensions.sh" -O ./install-gnome-extensions.sh
+else
+  echo "Gnome older than 34 detected"
+  wget -N -q "https://raw.githubusercontent.com/cyfrost/install-gnome-extensions/7ea5327e36c35e732c6c97887c08fe3596506727/install-gnome-extensions.sh" -O ./install-gnome-extensions.sh
+fi
+
+echo "Installing Some Common Extensions"
 chmod +x install-gnome-extensions.sh
 # use this helpful script to install some desired extensions from command line
 # 615 is AppIndicator and KStatusNotifyerItem Support
