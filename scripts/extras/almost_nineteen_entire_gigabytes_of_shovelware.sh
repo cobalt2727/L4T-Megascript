eed23ec05dfa6ce2aee92d169dbec16d697bd49f
cobalt2727 <60624944+cobalt2727@users.noreply.github.com>
2021-06-23 22:23:09 -0500

    Update almost_nineteen_entire_gigabytes_of_shovelware.sh

--- scripts/extras/almost_nineteen_entire_gigabytes_of_shovelware.sh
+++ scripts/extras/almost_nineteen_entire_gigabytes_of_shovelware.sh
@@ -1,2 +1,6 @@
 echo "if you find anything in here that's actually good let me know"
+sleep 1
+echo "Don't say we didn't warn you."
+echo "(you can always cancel by choosing N for the next prompt)"
+sleep 3
 sudo apt-get install games-*
