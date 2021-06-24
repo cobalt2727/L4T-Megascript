f972f7e625471c51422927f429a36a20728d4e0c
theofficialgman <theofficialgman@gmail.com>
2021-06-23 20:01:42 -0400

    use binaries for sdl2 install (for both retropie and as part of sdl2 install helper) saves users a lot of time
    If things break.. PLEASE let me know

--- scripts/games_and_emulators/retropie_auto.sh
+++ scripts/games_and_emulators/retropie_auto.sh
@@ -264,7 +264,14 @@ EOF
     echo "Already Installed Newest SDL2 Version"
     sleep 3
   else
-    ./retropie_packages.sh sdl2
+    cd /tmp
+    sudo -u "$SUDO_USER" wget https://github.com/$repository_username/L4T-Megascript/raw/$repository_branch/assets/SDL2/libsdl2-2.0-0_2.0.10%2B5_arm64.deb
+    sudo -u "$SUDO_USER" wget https://github.com/$repository_username/L4T-Megascript/raw/$repository_branch/assets/SDL2/libsdl2-dbg_2.0.10%2B5_arm64.deb
+    sudo -u "$SUDO_USER" wget https://github.com/$repository_username/L4T-Megascript/raw/$repository_branch/assets/SDL2/libsdl2-dev_2.0.10%2B5_arm64.deb
+
+    dpkg -i libsdl2-dev_2.0.10+5_arm64.deb libsdl2-dbg_2.0.10+5_arm64.deb libsdl2-2.0-0_2.0.10+5_arm64.deb
+    apt install -fy
+    rm -rf libsdl2-dev_2.0.10+5_arm64.deb libsdl2-dbg_2.0.10+5_arm64.deb libsdl2-2.0-0_2.0.10+5_arm64.deb
     echo ""
     echo "Successfully Installed Newest SDL2 Version"
     sleep 3
