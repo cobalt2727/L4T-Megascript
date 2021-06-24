c82c51c51523f09e72219a257cf4d266c01394c8
theofficialgman <28281419+theofficialgman@users.noreply.github.com>
2021-06-23 20:01:42 -0400

    use binaries for sdl2 install (for both retropie and as part of sdl2 install helper) saves users a lot of time
    If things break.. PLEASE let me know

--- scripts/sdl2_install_helper.sh
+++ scripts/sdl2_install_helper.sh
@@ -4,36 +4,52 @@ cd
 #make sure this is updated
 #note that this is an apt package in the Switchroot repository
 sudo apt install joycond -y
-if dpkg -s libsdl2-dev | grep -q "2.0.10+5"; then
-echo ""
-echo "Already Installed Newest SDL2 Version"
-sleep 1
+
+function version { echo "$@" | awk -F. '{ printf("%d%03d%03d%03d%03d\n", $1,$2,$3,$4,$5); }'; }
+
+sdlv=$(dpkg -s libsdl2-dev | sed -n 's/Version: //p')
+sdlv=${sdlv/+/.}
+if [ $(version $sdlv) -ge $(version "2.0.10.5") ]; then
+    echo ""
+    echo "Already Installed Newest SDL2 Version"
+    sleep 1
 else
-sudo apt-get --assume-yes install git
-cd /tmp
-mkdir -p temp_install_sdl2
-cd temp_install_sdl2
-curl -s 'https://raw.githubusercontent.com/theofficialgman/RetroPie-Setup/master/retropie_packages.sh' > retropie_packages.sh
-chmod -R 777 retropie_packages.sh
-curl -s 'https://raw.githubusercontent.com/theofficialgman/RetroPie-Setup/master/retropie_setup.sh' > retropie_setup.sh
-mkdir -p scriptmodules
-cd ./scriptmodules
-curl -s 'https://raw.githubusercontent.com/theofficialgman/RetroPie-Setup/master/scriptmodules/system.sh' > system.sh
-curl -s 'https://raw.githubusercontent.com/theofficialgman/RetroPie-Setup/master/scriptmodules/helpers.sh' > helpers.sh
-curl -s 'https://raw.githubusercontent.com/theofficialgman/RetroPie-Setup/master/scriptmodules/packages.sh' > packages.sh
-curl -s 'https://raw.githubusercontent.com/theofficialgman/RetroPie-Setup/master/scriptmodules/inifuncs.sh' > inifuncs.sh
-mkdir -p supplementary
-cd ./supplementary
-curl -s 'https://raw.githubusercontent.com/theofficialgman/RetroPie-Setup/master/scriptmodules/supplementary/sdl2.sh' > sdl2.sh
-cd /tmp/temp_install_sdl2
+    cd /tmp
+    wget https://github.com/$repository_username/L4T-Megascript/raw/$repository_branch/assets/SDL2/libsdl2-2.0-0_2.0.10%2B5_arm64.deb
+    wget https://github.com/$repository_username/L4T-Megascript/raw/$repository_branch/assets/SDL2/libsdl2-dbg_2.0.10%2B5_arm64.deb
+    wget https://github.com/$repository_username/L4T-Megascript/raw/$repository_branch/assets/SDL2/libsdl2-dev_2.0.10%2B5_arm64.deb
 
+    sudo dpkg -i libsdl2-dev_2.0.10+5_arm64.deb libsdl2-dbg_2.0.10+5_arm64.deb libsdl2-2.0-0_2.0.10+5_arm64.deb
+    sudo apt install -fy
+    sudo rm -rf libsdl2-dev_2.0.10+5_arm64.deb libsdl2-dbg_2.0.10+5_arm64.deb libsdl2-2.0-0_2.0.10+5_arm64.deb
 
-#auto install sdl2 and then remove unneeded files
-sudo ./retropie_packages.sh sdl2
-cd /tmp
-sudo rm -rf temp_install_sdl2
-cd
-echo ""
-echo "Successfully Installed Newest SDL2 Version"
-sleep 3
+    # keep this old code for compiling SDL2 if its ever needed
+
+    # sudo apt-get --assume-yes install git
+    # cd /tmp
+    # mkdir -p temp_install_sdl2
+    # cd temp_install_sdl2
+    # curl -s 'https://raw.githubusercontent.com/theofficialgman/RetroPie-Setup/master/retropie_packages.sh' > retropie_packages.sh
+    # chmod -R 777 retropie_packages.sh
+    # curl -s 'https://raw.githubusercontent.com/theofficialgman/RetroPie-Setup/master/retropie_setup.sh' > retropie_setup.sh
+    # mkdir -p scriptmodules
+    # cd ./scriptmodules
+    # curl -s 'https://raw.githubusercontent.com/theofficialgman/RetroPie-Setup/master/scriptmodules/system.sh' > system.sh
+    # curl -s 'https://raw.githubusercontent.com/theofficialgman/RetroPie-Setup/master/scriptmodules/helpers.sh' > helpers.sh
+    # curl -s 'https://raw.githubusercontent.com/theofficialgman/RetroPie-Setup/master/scriptmodules/packages.sh' > packages.sh
+    # curl -s 'https://raw.githubusercontent.com/theofficialgman/RetroPie-Setup/master/scriptmodules/inifuncs.sh' > inifuncs.sh
+    # mkdir -p supplementary
+    # cd ./supplementary
+    # curl -s 'https://raw.githubusercontent.com/theofficialgman/RetroPie-Setup/master/scriptmodules/supplementary/sdl2.sh' > sdl2.sh
+    # cd /tmp/temp_install_sdl2
+
+    # #auto install sdl2 and then remove unneeded files
+    # sudo ./retropie_packages.sh sdl2
+    # cd /tmp
+    # sudo rm -rf temp_install_sdl2
+
+    cd
+    echo ""
+    echo "Successfully Installed Newest SDL2 Version"
+    sleep 3
 fi
