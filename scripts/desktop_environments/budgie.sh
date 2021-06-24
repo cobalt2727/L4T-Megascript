1a5cefae2cc82443116c4c113a3cff291fb6809c
theofficialgman <28281419+theofficialgman@users.noreply.github.com>
2021-06-23 21:02:26 -0400

    add appindicator as option in taskbar
    not enabled by default yet
    still figuring out why my custom topbar doesn't load

--- scripts/desktop_environments/budgie.sh
+++ scripts/desktop_environments/budgie.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 clear
 echo "You are about to install the Budgie desktop environment."
-sudo apt install budgie-core budgie-desktop -y
+sudo apt install budgie-core budgie-desktop budgie-indicator-applet -y
 
 echo "Going back to the main menu..."
