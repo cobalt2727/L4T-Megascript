084c77ed7cf61c3fa850499cf1e404fe269620eb
theofficialgman <28281419+theofficialgman@users.noreply.github.com>
2021-07-02 17:15:04 -0400

    VS-Code script now simply adds the deb repo and installs
    downloading and installing the binary manually has been removed
    (should work on amd64, arm64, and armhf)

--- scripts/IDE/vscode.sh
+++ scripts/IDE/vscode.sh
@@ -2,7 +2,8 @@
 
 
 clear -x
-echo "Installing the aarch64 build of Visual Studio Code directly from Microsoft..."
+arch=$(dpkg --print-architecture)
+echo "Installing the $arch build of Visual Studio Code directly from Microsoft..."
 sleep 1
 
 ##need a newer debugger?
@@ -12,17 +13,18 @@ sudo apt update
 ##i'd be surprised if the first two weren't already installed, but...
 sudo apt install wget gpg gdb -y
 
-##Thankfully Microsoft has a link that always downloads whatever the latest version is.
-##the -O parameter lets me choose the filename myself to make the installation lines easier
-wget -O vscode-latest.deb https://aka.ms/linux-arm64-deb
-sudo dpkg -i vscode-latest.deb
-
-##If you're still somehow missing any dependencies...
-sudo apt --fix-broken install
-echo "Done! You can now launch VS Code from your app list or by typing 'code' into a terminal."
-sleep 1
-
-echo "Removing the .deb file..."
-rm vscode-latest.deb
+case $arch in
+    "arm64"|"armhf"|"amd64")
+    sudo sh -c "cat > /etc/apt/sources.list.d/vscode.list << _EOF_
+deb [arch=$arch] http://packages.microsoft.com/repos/code stable main
+_EOF_"
+    sudo apt update
+    sudo apt install code -y
+    echo "Done! You can now launch VS Code from your app list or by typing 'code' into a terminal."
+    ;;
+    *)
+    echo "Not installing VS-Code, your architecture isn't supported"
+    ;;
+esac
 
 echo "Done! Going back to the main menu..."
