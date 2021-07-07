8e9b0a4e1e42363f4ebd3c19f0d5d65acdcde302
theofficialgman <gmesmer@comcast.net>
2021-07-07 19:38:01 -0400

    add Box64 GUI application .desktop helper
    This zenity based GUI script helps the user add arbitrary applications to their applications list

---
+++ assets/Box64/box64_program_helper.sh
@@ -0,0 +1,37 @@
+# Box64 application .desktop helper
+
+zenity --info --width="500" --height="250" --title "Box64 application adder helper" \
+    --text "This script will assist you in adding your x86_64 applications/programs/games to your applications list \n\All you need to do is follow some simple prompts in the GUI \n\You should verify that box64 can launch your program successfully before using this script" --window-icon=/usr/share/box64/icon.png
+program_path=$(zenity --file-selection --title="Choose your x86_64 binary program/game")
+path=$(zenity --file-selection --directory --title="Choose your path for program/game to run from (usually the directly which the program files reside in)")
+image_path=$(zenity --file-selection --title="Choose the image to represent your application/game (png/ico file)")
+name=$(zenity --entry --text="Enter the name of your program/application/game:" --title="Name")
+declare -n selection=(TRUE "Game" FALSE "System" FALSE "Audio" FALSE "Video" FALSE "Development" FALSE "Education" FALSE "Graphics" FALSE "Network" FALSE "Office" FALSE "Science" FALSE "Settings" FALSE "Utility") 
+category=$(
+zenity \
+    --width="1000" \
+    --height="500" \
+    --text "Chose a category(s) for your application/program/game" \
+    --title "Category" \
+    --window-icon=/usr/share/box64/icon.png \
+    --list \
+    --checklist \
+    --column "Selection" \
+    --column "Category" \
+    --ok-label="Done" \
+    "${selection[@]}" \
+    --separator=';' \
+    --cancel-label "Cancel Helper" \
+)
+if [ $? -eq 0 ]; then
+sudo tee "$HOME/.local/share/applications/$name.desktop" <<EOF
+[Desktop Entry]
+Name=$name
+Exec=box64 "$program_path"
+Path=$path
+Icon=$image_path
+Type=Application
+Categories=$category
+Terminal=hidden
+EOF
+fi
