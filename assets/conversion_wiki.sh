1be6e444cf3652cce8f294c7dc2da455e75eda39
theofficialgman <28281419+theofficialgman@users.noreply.github.com>
2021-07-10 23:09:39 -0400

    for generating the scripts wiki page
    the script is not used by the megascript but it used to help generate the wiki All Scripts page with minimal effort

---
+++ assets/conversion_wiki.sh
@@ -0,0 +1,31 @@
+rm -rf /tmp/megascript_apps.txt
+wget https://raw.githubusercontent.com/$repository_username/L4T-Megascript/$repository_branch/megascript_apps.txt -O /tmp/megascript_apps.txt
+sed -i '/^$/d' /tmp/megascript_apps.txt
+length=$(wc -l "/tmp/megascript_apps.txt" | awk '{ print $1 }')
+prev_f=""
+conversion() {
+  for ((i = 1; i <= ${length}; i++)); do
+    if [[ ! " ${hidden[@]} " =~ " ${i} " ]]; then
+      fn=""
+      d=""
+      e=""
+      f=""
+      sn=""
+      line=$(sed -n $i"p" <"/tmp/megascript_apps.txt")
+      if [[ "$line" != \#* ]]; then
+        eval "$(echo "$line" | tr ";" "\n")"
+        ff="$(echo "$f" | sed -e 's/_/ /g' | sed -e "s/\b\(.\)/\u\1/g")"
+        if [[ "$ff" != "$prev_f" ]];then
+            echo "## $ff"
+        fi
+        if [ "$f" != "scripts" ]; then
+            f="scripts/$f"
+        fi
+        echo "**[$fn](https://github.com/cobalt2727/L4T-Megascript/blob/master/$f/$sn)**: $d "
+        echo ""
+        prev_f="$ff"
+      fi
+    fi
+  done
+}
+conversion
