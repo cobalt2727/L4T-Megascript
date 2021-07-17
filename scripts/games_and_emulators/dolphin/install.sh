a7dbcfd4cf87abb044e90936a7f275c9ae657206
theofficialgman <28281419+theofficialgman@users.noreply.github.com>
2021-07-17 12:56:36 -0400

    remove -D
    caused dolphin script to fail... not sure how long its been failing for
    seems like many months

--- scripts/games_and_emulators/dolphin/install.sh
+++ scripts/games_and_emulators/dolphin/install.sh
@@ -33,7 +33,7 @@ if grep -q bionic /etc/os-release; then
   echo "Alright, NOW we can start the building process."
   echo -e "\e[1;33mIf it freezes, especially around 80% or 100%, even for a few minutes, that's normal.\e[0m"
   sleep 10
-  cmake .. -D -DCMAKE_BUILD_TYPE=Release -DCMAKE_CXX_FLAGS=-mcpu=native -DCMAKE_C_FLAGS=-mcpu=native -DCMAKE_C_FLAGS_INIT="-static" -DCMAKE_C_COMPILER=gcc-11 -DCMAKE_CXX_COMPILER=g++-11
+  cmake .. -DCMAKE_BUILD_TYPE=Release -DCMAKE_CXX_FLAGS=-mcpu=native -DCMAKE_C_FLAGS=-mcpu=native -DCMAKE_C_FLAGS_INIT="-static" -DCMAKE_C_COMPILER=gcc-11 -DCMAKE_CXX_COMPILER=g++-11
 
 else  
   
