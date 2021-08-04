1d950ad8d297219de561b5826a6af243cacf5d50
theofficialgman <28281419+theofficialgman@users.noreply.github.com>
2021-08-04 18:08:56 -0400

    add retropie binaries and install using retropie_auto.sh
    binary build date 08/02/2021
    currently running the install script will install these binaries over anything regardless of the version
    (version checking is not implemented and could come at a later date to avoid overwritting potentially manual newer build from source installs)

--- scripts/games_and_emulators/retropie_auto.sh
+++ scripts/games_and_emulators/retropie_auto.sh
@@ -33,48 +33,50 @@ _EOF_"
     ./retropie_packages.sh runcommand
     #./retropie_packages.sh ports-enable configure
     ./retropie_packages.sh mupen64plus
-    ./retropie_packages.sh lr-mupen64plus-next
-    ./retropie_packages.sh lr-atari800
-    ./retropie_packages.sh lr-beetle-ngp
-    ./retropie_packages.sh lr-beetle-pce-fast
-    ./retropie_packages.sh lr-beetle-supergrafx
-    # ./retropie_packages.sh lr-beetle-saturn
-
-    ./retropie_packages.sh lr-desmume
-    # ./retropie_packages.sh lr-desmume2015
-    ./retropie_packages.sh lr-fbneo
-    ./retropie_packages.sh lr-fceumm
-    ./retropie_packages.sh lr-flycast
-    ./retropie_packages.sh lr-gambatte
-    ./retropie_packages.sh lr-genesis-plus-gx
-    ./retropie_packages.sh lr-handy
-    #./retropie_packages.sh lr-mame
-    # ./retropie_packages.sh lr-mame2003
-    # ./retropie_packages.sh lr-mame2010
-    # ./retropie_packages.sh lr-mame2016
-    # ./retropie_packages.sh lr-mesen
-    ./retropie_packages.sh lr-mgba
-    ./retropie_packages.sh lr-nestopia
-    ./retropie_packages.sh lr-pcsx-rearmed
-    ./retropie_packages.sh lr-ppsspp
-    ./retropie_packages.sh lr-prosystem
-    ./retropie_packages.sh lr-quicknes
-    ./retropie_packages.sh lr-smsplus-gx
-    ./retropie_packages.sh lr-stella2014
-    ./retropie_packages.sh lr-vba-next
-    ./retropie_packages.sh lr-vecx
-    ./retropie_packages.sh lr-tgbdual
-
-    # ./retropie_packages.sh lr-yabasanshiro
-    ./retropie_packages.sh lr-yabause
+    ./retropie_packages.sh lr-duckstation
     ./retropie_packages.sh lzdoom
     # ./retropie_packages.sh scraper
     # ./retropie_packages.sh skyscraper
     # ./retropie_packages.sh usbromservice
 	if [[ $jetson_model != "tegra-x1" ]]; then
-	# install packages which will get installed as binaries on tegra-x1
-	./retropie_packages.sh lr-bsnes
-    ./retropie_packages.sh lr-snes9x	
+        # only build from source on non-tegra-x1 systems
+        ./retropie_packages.sh lr-atari800
+        ./retropie_packages.sh lr-beetle-ngp
+        ./retropie_packages.sh lr-beetle-pce-fast
+        ./retropie_packages.sh lr-beetle-supergrafx
+        ./retropie_packages.sh lr-bsnes
+        ./retropie_packages.sh lr-caprice32
+        ./retropie_packages.sh lr-desmume
+        ./retropie_packages.sh lr-fbneo
+        ./retropie_packages.sh lr-fceumm
+        ./retropie_packages.sh lr-fuse
+        ./retropie_packages.sh lr-flycast
+        ./retropie_packages.sh lr-gambatte
+        ./retropie_packages.sh lr-genesis-plus-gx
+        ./retropie_packages.sh lr-gpsp
+        ./retropie_packages.sh lr-handy
+        ./retropie_packages.sh lr-mame
+        ./retropie_packages.sh lr-mame2003
+        ./retropie_packages.sh lr-mame2010
+        ./retropie_packages.sh lr-mame2016
+        ./retropie_packages.sh lr-mesen
+        ./retropie_packages.sh lr-mgba
+        ./retropie_packages.sh lr-mupen64plus-next
+        ./retropie_packages.sh lr-nestopia
+        ./retropie_packages.sh lr-parallel-n64
+        ./retropie_packages.sh lr-pcsx-rearmed
+        ./retropie_packages.sh lr-ppsspp
+        ./retropie_packages.sh lr-prosystem
+        ./retropie_packages.sh lr-quicknes
+        ./retropie_packages.sh lr-smsplus-gx
+        ./retropie_packages.sh lr-snes9x
+        ./retropie_packages.sh lr-snes9x2005
+        ./retropie_packages.sh lr-snes9x2010
+        ./retropie_packages.sh lr-stella2014
+        ./retropie_packages.sh lr-tgbdual
+        ./retropie_packages.sh lr-vba-next
+        ./retropie_packages.sh lr-vecx
+        ./retropie_packages.sh lr-yabause	
 	fi
 }
 
