2b66ad4c53fdbb4b61d6087c8576c07392b89010
theofficialgman <28281419+theofficialgman@users.noreply.github.com>
2021-08-06 21:56:43 -0400

    remove extra space

--- core_refactor2.sh
+++ core_refactor2.sh
@@ -496,7 +496,6 @@ if [[ $gui == "gui" ]]; then
   zenity  --height="250" --title "Bye" --question \
   "Thank you for using the L4T Megascript!\n\nCredits:\nCobalt - Manager/Lead Dev\nGman - Developer/GUI and CLI Management/RetroPie/Minecraft Handler\nLugsole - Contributor/GUI Manager\nLang Kasempo - Contributor/Beta Tester/did a lot of the standalone game scripts\n\nthe Switchroot L4T Ubuntu team (https://switchroot.org/) - making the actual OS you're running right now\n\nThe Megascript ran for $megascript_elapsed_friendly" \
   --window-icon=/usr/share/icons/L4T-Megascript.png \
-
   --ok-label "Exit the L4T-Megascript" \
   --cancel-label "Open the L4T-Megascript Wiki Page"
   if [[ $? -ne 0 ]]; then
