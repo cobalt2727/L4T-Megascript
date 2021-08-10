c0fedcfadf814824247f8d2126f91280e7439394
theofficialgman <28281419+theofficialgman@users.noreply.github.com>
2021-08-10 17:29:16 -0400

    minecraft bedrock launcher no longer needs hacks
    the bedrock launcher is built on bionic (instead of buster) on arm64 platforms now
    it can be installed normally without any ppas or other shenanigans
    
    also, for previous installs, the hacks are reverted
    (you may need to rebuild stuff compiled from source if it used glibc2.28 stuff, sorry)
    (for example, dolphin will need to be rebuilt if you compiled after installing bedrock previously)

--- scripts/games_and_emulators/minecraft-bedrock-appimage.sh
+++ scripts/games_and_emulators/minecraft-bedrock-appimage.sh
@@ -34,61 +34,14 @@ echo "Install Dependencies..."
 cd ~
 
 if grep -q bionic /etc/os-release; then
+    sudo rm -rf /etc/apt/sources.list.d/zorinos-ubuntu-stable-bionic.list*
+    sudo rm -rf /etc/apt/preferences.d/zorinos*
+    sudo rm -rf /etc/apt/sources.list.d/debian-stable.list*
+    sudo rm -rf /etc/apt/preferences.d/freetype*
 
-sudo add-apt-repository ppa:zorinos/stable -y
-sudo sh -c "cat > /etc/apt/preferences.d/zorinos << _EOF_
-Explanation: Allow installing glibc from zorinos.
-Package: glibc*
-Pin: release o=*zorinos*
-Pin-Priority: 500
-
-Explanation: Allow installing libc6 from zorinos.
-Package: locales*
-Pin: release o=*zorinos*
-Pin-Priority: 500
-
-Explanation: Allow installing libc from zorinos.
-Package: libc*
-Pin: release o=*zorinos*
-Pin-Priority: 500
-
-Explanation: Avoid other packages from the zorinos repo.
-Package: *
-Pin: release o=*zorinos*
-Pin-Priority: 1
-_EOF_"
-
-sudo apt-get install -y debian-archive-keyring
-
-sudo sh -c "cat > /etc/apt/sources.list.d/debian-stable.list << _EOF_
-deb [signed-by=/usr/share/keyrings/debian-archive-keyring.gpg] http://deb.debian.org/debian buster main
-deb-src [signed-by=/usr/share/keyrings/debian-archive-keyring.gpg] http://deb.debian.org/debian buster main
-
-deb [signed-by=/usr/share/keyrings/debian-archive-keyring.gpg] http://deb.debian.org/debian-security/ buster/updates main
-deb-src [signed-by=/usr/share/keyrings/debian-archive-keyring.gpg] http://deb.debian.org/debian-security/ buster/updates main
-
-deb [signed-by=/usr/share/keyrings/debian-archive-keyring.gpg] http://deb.debian.org/debian buster-updates main
-deb-src [signed-by=/usr/share/keyrings/debian-archive-keyring.gpg] http://deb.debian.org/debian buster-updates main
-_EOF_"
-
-sudo sh -c "cat > /etc/apt/preferences.d/freetype << _EOF_
-Explanation: Allow installing freetype from the debian repo.
-Package: *freetype*
-Pin: origin "*.debian.org"
-Pin-Priority: 500
-
-Explanation: Avoid other packages from the debian repo.
-Package: *
-Pin: origin "*.debian.org"
-Pin-Priority: 1
-_EOF_"
-
-
+    sudo apt update
+    sudo apt install libc-bin=2.27* libc-dev-bin=2.27* libc6=2.27* libc6-dbg=2.27* libc6-dev=2.27* libfreetype6=2.8* libfreetype6-dev=2.8* locales=2.27* -y --allow-downgrades
 fi
-
-sudo apt update
-sudo apt install libc6 libc6-dev libc-dev-bin libc-bin locales  -y
-sudo apt upgrade -y
 
 echo "Please Reboot before launching!"
 sleep 5
