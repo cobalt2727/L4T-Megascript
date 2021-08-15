23455d4eb2705c333a97200b3a0c7ab6583bb3ca
cobalt2727 <60624944+cobalt2727@users.noreply.github.com>
2021-08-15 08:43:37 -0500

    add notification manager
    no idea if this is already included, but...

--- scripts/desktop_environments/mate.sh
+++ scripts/desktop_environments/mate.sh
@@ -7,7 +7,7 @@ echo "Under most cases this shouldn't break anything and install alongside of yo
 echo "Are you sure you want to continue?"
 
 ##prompt yes/no
-sudo apt install mate-desktop-environment mate-desktop-environment-extras ubuntu-mate-themes plank -y
+sudo apt install mate-desktop-environment mate-desktop-environment-extras ubuntu-mate-themes plank mate-notification-daemon -y
 ##should we add these?
 ##echo "Installing extras..."
 ##sudo apt-get install mate-session-manager mate-themes mate-screensaver mate-power-manager mate-indicator-applet mate-indicator-applet-common mate-tweak dconf-editor mate-applet-appmenu -y
