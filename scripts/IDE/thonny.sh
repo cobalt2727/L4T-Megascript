e3be18db5ce0c95a74537de06f82a78297b38f91
cobalt2727 <60624944+cobalt2727@users.noreply.github.com>
2021-09-17 15:14:07 -0400

    push python version up to 3.8 on 18.04

--- scripts/IDE/thonny.sh
+++ scripts/IDE/thonny.sh
@@ -2,14 +2,17 @@
 
 
 echo "Thonny script started!"
-echo "Adding Ubuntu Toolchain PPA..."
-ppa_name="ubuntu-toolchain-r/ppa" && ppa_installer
+##not sure why i had these lines, i know i put them in VS Code's installer for a newer GDB, but... -cobalt
+#echo "Adding Ubuntu Toolchain PPA..."
+#ppa_name="ubuntu-toolchain-r/ppa" && ppa_installer
 
 echo "Installing dependencies..."
 sudo apt install python2 python2.7 python3 python3-pip thonny -y
 
 if grep -q bionic /etc/os-release; then
   sudo apt install python3.8 libpython3.8 -y
+  sudo update-alternatives --install /usr/bin/python python /usr/bin/python3.8 10
+  #3.8 is the most modern version 18.04's ever getting unless we use a PPA
 fi
 
 if grep -q focal /etc/os-release; then
