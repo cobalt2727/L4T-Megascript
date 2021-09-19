924ae3de2ebe298ff3f93198abda8c31d22deb7d
cobalt2727 <60624944+cobalt2727@users.noreply.github.com>
2021-09-19 11:32:28 -0400

    add PPAs for MATE (untested)

--- scripts/desktop_environments/mate.sh
+++ scripts/desktop_environments/mate.sh
@@ -6,8 +6,16 @@ echo "You are about to install the MATE desktop environment."
 echo "Under most cases this shouldn't break anything and install alongside of your existing one, but just to be sure..."
 echo "Are you sure you want to continue?"
 
+ppa_name="ubuntu-mate-dev/welcome" && ppa_installer
+
+if grep -q bionic /etc/os-release; then
+  echo ""
+else
+  ppa_name="ubuntu-mate-dev/fresh-mate" && ppa_installer
+fi
+
 ##prompt yes/no
-sudo apt install mate-desktop-environment mate-desktop-environment-extras ubuntu-mate-themes plank mate-notification-daemon -y
+sudo apt install ubuntu-mate-desktop ubuntu-mate-themes plank mate-notification-daemon ubuntu-mate-welcome -y
 ##should we add these?
 ##echo "Installing extras..."
 ##sudo apt-get install mate-session-manager mate-themes mate-screensaver mate-power-manager mate-indicator-applet mate-indicator-applet-common mate-tweak dconf-editor mate-applet-appmenu -y
