2614e49e2d102ca331237085299bc98c50e9282e
theofficialgman <28281419+theofficialgman@users.noreply.github.com>
2021-09-23 20:46:05 -0400

    add functions.sh file
    will be sourced in the future potentially like this to avoid keeping functions up to date in two separate files
    
    source <(curl -s https://raw.githubusercontent.com/$repository_username/L4T-Megascript/$repository_branch/functions.sh)

---
+++ functions.sh
@@ -0,0 +1,236 @@
+#!/bin/bash
+
+#####################################################################################
+
+#functions used by megascript scripts
+
+#####################################################################################
+
+function get_system {
+  # architecture is the native cpu architecture (aarch64, x86_64, etc)
+  architecture="$(uname -m)"
+  # jetson codename is the name reported in the DTS filename
+  jetson_codename=""
+  # jetson model is the friendly name that we assign
+  jetson_model=""
+  # get model name
+  # https://github.com/dylanaraps/neofetch/blob/77f2afc37239d9494ce0f5d0f71f690f8b4d34e3/neofetch#L1232
+  if [[ -d /system/app/ && -d /system/priv-app ]]; then
+      model="$(getprop ro.product.brand) $(getprop ro.product.model)"
+
+  elif [[ -f /sys/devices/virtual/dmi/id/product_name ||
+          -f /sys/devices/virtual/dmi/id/product_version ]]; then
+      model=$(< /sys/devices/virtual/dmi/id/product_name)
+      model+=" $(< /sys/devices/virtual/dmi/id/product_version)"
+
+  elif [[ -f /sys/firmware/devicetree/base/model ]]; then
+      model=$(< /sys/firmware/devicetree/base/model)
+
+  elif [[ -f /tmp/sysinfo/model ]]; then
+      model=$(< /tmp/sysinfo/model)
+  fi
+  # Remove dummy OEM info.
+  model=${model//To be filled by O.E.M.}
+  model=${model//To Be Filled*}
+  model=${model//OEM*}
+  model=${model//Not Applicable}
+  model=${model//System Product Name}
+  model=${model//System Version}
+  model=${model//Undefined}
+  model=${model//Default string}
+  model=${model//Not Specified}
+  model=${model//Type1ProductConfigId}
+  model=${model//INVALID}
+  model=${model//All Series}
+  model=${model//�}
+  local __platform=""
+  if [[ -e "/proc/device-tree/compatible" ]]; then
+      CHIP="$(tr -d '\0' < /proc/device-tree/compatible)"
+      if [[ ${CHIP} =~ "tegra186" ]]; then
+          __platform="tegra-x2"
+      elif [[ ${CHIP} =~ "tegra210" ]]; then
+          __platform="tegra-x1"
+      elif [[ ${CHIP} =~ "tegra194" ]]; then
+          __platform="xavier"
+      fi
+      jetson_model="$__platform"
+  elif [[ -e "/sys/devices/soc0/family" ]]; then
+      CHIP="$(tr -d '\0' < /sys/devices/soc0/family)"
+      if [[ ${CHIP} =~ "tegra20" ]]; then
+          __platform="tegra-2"
+      elif [[ ${CHIP} =~ "tegra30" ]]; then
+          __platform="tegra-3"
+      elif [[ ${CHIP} =~ "tegra114" ]]; then
+          __platform="tegra-4"
+      elif [[ ${CHIP} =~ "tegra124" ]]; then
+          __platform="tegra-k1-32"
+      elif [[ ${CHIP} =~ "tegra132" ]]; then
+          __platform="tegra-k1-64"
+      elif [[ ${CHIP} =~ "tegra210" ]]; then
+          __platform="tegra-x1"
+      fi
+      jetson_model="$__platform"
+  fi
+  unset __platform
+}
+export -f get_system
+
+# run get_system function for the refactor to use
+get_system
+
+function userinput_func {
+  unset uniq_selection
+  height=$(($(echo "$description" | grep -o '\\n' | wc -l) + 8))
+  if [[ $gui == "gui" ]]; then
+    if [[ "${#@}" == "3" ]];then
+      yad --center --width 500 --image "dialog-question" \
+      --text="$1" \
+      --button="$2":0 \
+      --button="$3":1
+      if [[ $? -ne 0 ]]; then
+        output="$3"
+      else
+        output="$2"
+      fi
+    else
+      for string in "${@:2}"; do
+        uniq_selection+=(FALSE "$string")
+      done
+      uniq_selection[0]=TRUE
+      output=$(
+        yad --center \
+          --text "$1" \
+          --list \
+          --radiolist \
+          --column "" \
+          --column "Selection" \
+          --print-column=2 \
+          --separator='' \
+          --button="Ok":0 \
+          "${uniq_selection[@]}"
+      )
+    fi
+  else
+    if [[ "${#@}" == "3" ]];then
+      dialog --clear --yes-label "$2" --no-label "$3" --yesno "$1" "$height" "120"
+      if [[ $? -ne 0 ]]; then
+        output="$3"
+      else
+        output="$2"
+      fi
+    else
+      for string in "${@:2}"; do
+        uniq_selection+=("$string" "")
+      done
+      output=$(
+        dialog --clear \
+          --backtitle "CLI Chooser Helper" \
+          --title "Choices" \
+          --menu "$1" \
+          "$height" "120" "$($# - 1)" \
+          "${uniq_selection[@]}" \
+          2>&1 >/dev/tty
+      )
+    fi
+  fi
+  status=$?
+  if [[ $status = "1" ]]; then
+    clear -x
+    echo "Canceling Install, Back to the Megascript"
+    exit 1
+  fi
+  clear -x
+}
+export -f userinput_func
+
+function ppa_installer {
+  local ppa_grep="$ppa_name"
+  [[ "${ppa_name}" != */ ]] && local ppa_grep="${ppa_name}/"
+  local ppa_added=$(grep ^ /etc/apt/sources.list /etc/apt/sources.list.d/* | grep -v list.save | grep -v deb-src | grep deb | grep -v '#' | grep "$ppa_grep" | wc -l)
+  if [[ $ppa_added -eq "1" ]]; then
+    echo "Skipping $ppa_name PPA, already added"
+  else
+    echo "Adding $ppa_name PPA"
+    sudo add-apt-repository "ppa:$ppa_name" -y
+    sudo apt update
+  fi
+  unset ppa_name
+}
+export -f ppa_installer
+
+function ppa_purger {
+  local ppa_grep="$ppa_name"
+  [[ "${ppa_name}" != */ ]] && local ppa_grep="${ppa_name}/"
+  local ppa_added=$(grep ^ /etc/apt/sources.list /etc/apt/sources.list.d/* | grep -v list.save | grep -v deb-src | grep deb | grep -v '#' | grep "$ppa_grep" | wc -l)
+  echo "$ppa_url"
+  if [[ $ppa_added -eq "1" ]]; then
+    echo "Removing $ppa_name PPA"
+    sudo apt-get install ppa-purge -y
+    sudo ppa-purge "ppa:$ppa_name" -y
+    sudo apt update
+  else
+    echo "$ppa_name PPA does not exist, skipping removal"
+  fi
+  unset ppa_name
+}
+export -f ppa_purger
+
+function online_check {
+    while : ; do
+    clear -x
+    date
+    echo "Checking internet connectivity..."
+    #silently check connection to github AND githubusercontent, we had an edge case where a guy was getting githubusercontent blocked by his ISP
+    wget -q --spider https://github.com && wget -q --spider https://raw.githubusercontent.com/
+
+    #read whether or not it was successful
+    #the $? reads the exit code of the previous command, 0 meaning everything works
+    if [ $? == 0 ]
+    then
+	echo -e "\e[32mInternet OK\e[0m"
+        break
+    fi
+    #tell people to fix their internet
+    echo "You're offline and/or can't reach GitHub."
+    echo "We can't run the script without this..."
+    echo "You'll need to connect to WiFi or allow GitHub in your firewall."
+    echo "(you can close this window at any time.)"
+    echo "Retrying the connection in..."
+    ########## bootleg progress bar time ##########
+    echo -e "\e[31m5\e[0m"
+    printf '\a'
+    sleep 1
+    echo -e "\e[33m4\e[0m"
+    printf '\a'
+    sleep 1
+    echo -e "\e[32m3\e[0m"
+    printf '\a'
+    sleep 1
+    echo -e "\e[34m2\e[0m"
+    printf '\a'
+    sleep 1
+    echo -e "\e[35m1\e[0m"
+    printf '\a'
+    echo "Trying again..."
+    sleep 1
+    #and now we let it loop
+    done
+}
+export -f online_check
+
+function runCmd() {
+    local ret
+    "$@"
+    ret=$?
+    if [[ "$ret" -ne 0 ]]; then
+        echo "${scripts[$word]} reported an error running '$*' - returned $ret" >> /tmp/megascript_errors.txt
+    fi
+    return $ret
+}
+export -f runCmd
+
+#####################################################################################
+
+#end of functions used by megascript scripts
+
+#####################################################################################
