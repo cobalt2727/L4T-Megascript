c923b28969a9c893161106fb177cce7701d1b0f9
cobalt2727 <60624944+cobalt2727@users.noreply.github.com>
2021-10-18 13:32:25 -0400

    Update snapless-chromium.sh
    fix apt priority writing

--- scripts/snapless-chromium.sh
+++ scripts/snapless-chromium.sh
@@ -1,6 +1,6 @@
-sudo echo "Package: *" > /etc/apt/preferences.d/chromium
-sudo echo "Pin: release o=LP-PPA-saiarcot895-chromium-beta" >> /etc/apt/preferences.d/chromium
-sudo echo "Pin-Priority: 700" >> /etc/apt/preferences.d/chromium
+sudo su -c "echo 'Package: *' > /etc/apt/preferences.d/chromium"
+sudo su -c "echo 'Pin: release o=LP-PPA-saiarcot895-chromium-beta' >> /etc/apt/preferences.d/chromium"
+sudo su -c "echo 'Pin-Priority: 700' >> /etc/apt/preferences.d/chromium"
 
 if test -f /usr/local/bin/snap; then
   sudo snap remove chromium
