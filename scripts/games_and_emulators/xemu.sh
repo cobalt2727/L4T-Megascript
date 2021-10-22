c3db0f83023bc03b07d69e51820dc233593a00a9
cobalt2727 <60624944+cobalt2727@users.noreply.github.com>
2021-10-21 21:42:25 -0400

    Update xemu.sh
    -y

--- scripts/games_and_emulators/xemu.sh
+++ scripts/games_and_emulators/xemu.sh
@@ -4,7 +4,7 @@ clear -x
 echo "Xemu script started!"
 
 # Install dependencies
-sudo apt install build-essential libsdl2-dev libepoxy-dev libpixman-1-dev libgtk-3-dev libssl-dev libsamplerate0-dev libpcap-dev ninja-build python3 gcc g++
+sudo apt install -y build-essential libsdl2-dev libepoxy-dev libpixman-1-dev libgtk-3-dev libssl-dev libsamplerate0-dev libpcap-dev ninja-build python3 gcc g++
 
 # Clone and build
 git clone https://github.com/mborgerson/xemu.git -j$(nproc) #not including all submodules on this, the folder is over 2 GB if I do -cobalt
