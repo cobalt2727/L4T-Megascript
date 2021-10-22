5fc3b3faad64a8c8700e93e3738573b028a10b5f
cobalt2727 <60624944+cobalt2727@users.noreply.github.com>
2021-10-21 21:37:47 -0400

    Update xemu.sh

--- scripts/games_and_emulators/xemu.sh
+++ scripts/games_and_emulators/xemu.sh
@@ -16,9 +16,10 @@ if grep -q bionic /etc/os-release; then
   ppa_name="ubuntu-toolchain-r/ppa" && ppa_installer
   sudo apt install python3.8 gcc-9 g++-9 -y #GCC 9 (the 20.04 default) also works, I'm just using 11 to future-proof -cobalt
   sed -i -e 's/python3 /python3.8 /g' build.sh #this is hacky, yes, but hey, it works
-  CC=gcc-9 CXX=g++-9 ./build.sh
+  CFLAGS=-mcpu=native CXXFLAGS=-mcpu=native CC=gcc-9 CXX=g++-9 ./build.sh
 else
-  ./build.sh
+  #./build.sh
+  CFLAGS=-mcpu=native CXXFLAGS=-mcpu=native ./build.sh #I don't think CXXFLAGS actually gets used, but I'm leaving it there in case the build script ever takes it into account
 fi
 
 
