d091af003883c10beb82eee71c4f3c503b76489e
theofficialgman <28281419+theofficialgman@users.noreply.github.com>
2021-11-12 17:37:33 -0500

    use new functions check in helper as well

--- helper.sh
+++ helper.sh
@@ -20,8 +20,9 @@ function error_fatal {
   exit 1
 }
 
+unset functions_downloaded
 source <(curl -s https://raw.githubusercontent.com/$repository_username/L4T-Megascript/$repository_branch/functions.sh)
-type get_system &>/dev/null && echo "Functions Loaded" || error_fatal "Oh no! Something happened to your internet! Exiting the Megascript, pleast fix your internet and try again!"
+[[ ! -z ${functions_downloaded+z} ]] && status "Functions Loaded" || error_fatal "Oh no! Something happened to your internet! Exiting the Megascript, pleast fix your internet and try again!"
 
 mkdir -p "$HOME/L4T-Megascript/logs"
 logfile="$HOME/L4T-Megascript/logs/install-incomplete-${1////-}.log"
