b945aaf572688f7558fce9d8945b83a61e9245ab
cobalt2727 <60624944+cobalt2727@users.noreply.github.com>
2021-11-15 00:55:31 -0500

    fix lazy pin copying

--- scripts/snapless-firefox.sh
+++ scripts/snapless-firefox.sh
@@ -1,6 +1,6 @@
-sudo su -c "echo 'Package: *' > /etc/apt/preferences.d/chromium"
-sudo su -c "echo 'Pin: release o=LP-PPA-saiarcot895-chromium-beta' >> /etc/apt/preferences.d/chromium"
-sudo su -c "echo 'Pin-Priority: 700' >> /etc/apt/preferences.d/chromium"
+sudo su -c "echo 'Package: *' > /etc/apt/preferences.d/firefox"
+sudo su -c "echo 'Pin: release o=LP-PPA-mozillateam-firefox-next' >> /etc/apt/preferences.d/firefox"
+sudo su -c "echo 'Pin-Priority: 700' >> /etc/apt/preferences.d/firefox"
 
 if test -f /usr/local/bin/snap; then
   sudo snap remove firefox
