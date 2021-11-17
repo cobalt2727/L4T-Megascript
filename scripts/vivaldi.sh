17e3759f02c7797c9740730b5ec6ff16e8a0e992
cobalt2727 <60624944+cobalt2727@users.noreply.github.com>
2021-11-17 18:15:23 -0500

    Create vivaldi.sh
    exactly one component of the script now supports Fedora
    
    I haven't tested hwaccel, that's probably broken, but it installs and that's good enough for me to leave it on the repo even if it's not currently usable

---
+++ scripts/vivaldi.sh
@@ -0,0 +1,11 @@
+echo "Vivaldi script started!"
+
+if grep -q debian /etc/os-release; then
+        wget -qO- https://repo.vivaldi.com/archive/linux_signing_key.pub | gpg --dearmor | sudo dd of=/usr/share/keyrings/vivaldi-browser.gpg
+        echo "deb [signed-by=/usr/share/keyrings/vivaldi-browser.gpg arch=$(dpkg --print-architecture)] https://repo.vivaldi.com/archive/deb/ stable main" | sudo dd of=/etc/apt/sources.list.d/vivaldi-archive.list
+        sudo apt update && sudo apt install vivaldi-stable -y
+
+elif grep -q fedora /etc/os-release; then
+        sudo dnf config-manager --add-repo https://repo.vivaldi.com/archive/vivaldi-fedora.repo
+        sudo dnf install vivaldi-stable -y
+fi
