4a3c2c5e80b9ea8229ec2f4b5d304539bb8715de
theofficialgman <28281419+theofficialgman@users.noreply.github.com>
2021-11-17 17:53:48 -0500

    allow user to skip mod install/update
    updating/install can take up to 2 seconds per mod, making minecraft take longer to launch
    This also allows us to inform the user of the current installed modlist, and directions on how to enable the mods

--- scripts/games_and_emulators/minecraft_java_multimc.sh
+++ scripts/games_and_emulators/minecraft_java_multimc.sh
@@ -178,21 +178,32 @@ wget -q --spider https://github.com && wget -q --spider https://raw.githubuserco
 # only run update/install script if the user has an active internet connection
 if [ $? == 0 ]
 then
-    cd "$INST_DIR"
-    mc_version=$(jq -M -r '.components[] | "\(.uid)/\(.version)"' mmc-pack.json | sed -n -e 's/^.*net.minecraft\///p')
-    mkdir -p .minecraft
-    cd .minecraft
-    mkdir -p mods
-    mkdir -p config
-    echo "Downloading latest gamecontrollerdb.txt"
-    wget "https://raw.githubusercontent.com/gabomdq/SDL_GameControllerDB/master/gamecontrollerdb.txt" -O ./config/gamecontrollerdb_temp.txt && rm -rf ./config/gamecontrollerdb.txt && mv ./config/gamecontrollerdb_temp.txt ./config/gamecontrollerdb.txt
-    echo "Patching gamecontrollerdb.txt for combined joycons to work around bug in lwjgl3"
-    echo "060000004e696e74656e646f20537700,Nintendo Combined Joy-Cons 2 (joycond),a:b0,b:b1,back:b9,dpdown:b15,dpleft:b16,dpright:b17,dpup:b14,leftshoulder:b5,leftstick:b12,lefttrigger:b7,leftx:a0,lefty:a1,rightshoulder:b6,rightstick:b13,righttrigger:b8,rightx:a2,righty:a3,start:b10,x:b3,y:b2,platform:Linux," >> ./config/gamecontrollerdb.txt
-    cd mods
-    megascript_mods=$(sed -n "p" <"/home/$USER/MultiMC/scripts/megascript-mods.txt")
-    user_mods=$(sed -n "p" <"/home/$USER/MultiMC/scripts/user-mods.txt")
-    minecraft-mod-manager -v "$mc_version" --mod-loader fabric --beta --alpha install $megascript_mods $user_mods
-    minecraft-mod-manager -v "$mc_version" --mod-loader fabric --beta --alpha update
+	megascript_mods=$(sed -n "p" <"/home/$USER/MultiMC/scripts/megascript-mods.txt")
+	user_mods=$(sed -n "p" <"/home/$USER/MultiMC/scripts/user-mods.txt")
+	cd "$INST_DIR"
+	mc_version=$(jq -M -r '.components[] | "\(.uid)/\(.version)"' mmc-pack.json | sed -n -e 's/^.*net.minecraft\///p')
+	mkdir -p .minecraft
+	cd .minecraft
+	mkdir -p mods
+	mkdir -p config
+	cd mods
+	minecraft_mods_list=$(minecraft-mod-manager list | tail -n +2)
+	echo -e "Do you want to update/install the following Fabric Mods: \n\n$minecraft_mods_list\n\n\nIf this list above is emtpty, you haven't clicked Yes before, you should do that to install suggested performance mods.\n\nMake sure you have already clicked the Install Fabric button otherwise these mods won't activate!" | yad --image "dialog-question" \
+	--borders="20" --height="200" --center --fixed\
+	--window-icon=/usr/share/icons/L4T-Megascript.png \
+	--text-info --fontname="@font@ 11" --wrap --width=800 --height=500 \
+	--show-uri \
+	--button="Yes, Update/Install My Mods":0 \
+	--button="No, skip this and save time":1
+	
+	if [[ $? == 0 ]]; then
+		echo "Downloading latest gamecontrollerdb.txt"
+		wget "https://raw.githubusercontent.com/gabomdq/SDL_GameControllerDB/master/gamecontrollerdb.txt" -O ../config/gamecontrollerdb_temp.txt && rm -rf ../config/gamecontrollerdb.txt && mv ../config/gamecontrollerdb_temp.txt ../config/gamecontrollerdb.txt
+		echo "Patching gamecontrollerdb.txt for combined joycons to work around bug in lwjgl3"
+		echo "060000004e696e74656e646f20537700,Nintendo Combined Joy-Cons 2 (joycond),a:b0,b:b1,back:b9,dpdown:b15,dpleft:b16,dpright:b17,dpup:b14,leftshoulder:b5,leftstick:b12,lefttrigger:b7,leftx:a0,lefty:a1,rightshoulder:b6,rightstick:b13,righttrigger:b8,rightx:a2,righty:a3,start:b10,x:b3,y:b2,platform:Linux," >> ../config/gamecontrollerdb.txt
+		minecraft-mod-manager -v "$mc_version" --mod-loader fabric --beta --alpha install $megascript_mods $user_mods
+		minecraft-mod-manager -v "$mc_version" --mod-loader fabric --beta --alpha update
+	fi
 fi
 echo "Mod script finished or skipped"
 EOF
