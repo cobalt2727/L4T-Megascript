2ad68fc64191f0b92f3ba9eda456c630270c2b03
cobalt2727 <60624944+cobalt2727@users.noreply.github.com>
2021-11-21 01:03:47 -0500

    Create balena-etcher.sh

---
+++ scripts/balena-etcher.sh
@@ -0,0 +1,10 @@
+#https://github.com/Itai-Nelken/Etcher-arm-32-64
+#https://github.com/ryanfortner/balenaetcher-debs
+
+echo "Balena Etcher script started!"
+
+sudo wget http://ryanfortner.github.io/balenaetcher-debs/etcher.list -O /etc/apt/sources.list.d/etcher.list || error "Downloading etcher.list failed"
+wget -qO- http://ryanfortner.github.io/balenaetcher-debs/KEY.gpg | sudo apt-key add -
+sudo apt update && sudo apt install balena-etcher-electron -y || error "Apt Update/Install Failed"
+
+echo "Done! Sending you back to the main menu..."
