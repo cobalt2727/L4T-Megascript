0b791e7f9630ef7b87070e8f89a4260d302bdb6a
theofficialgman <28281419+theofficialgman@users.noreply.github.com>
2021-11-22 13:03:49 -0500

    add simple error output to dobiestation
    (also test if my git signing through github desktop works)

--- scripts/games_and_emulators/dobiestation.sh
+++ scripts/games_and_emulators/dobiestation.sh
@@ -4,14 +4,14 @@ echo "DobieStation script started!"
 
 #no dependencies listed on repo, this is guesswork, guaranteed to be missing things -Cobalt
 #all I know is the setup I already had built it correctly
-sudo apt install -y git build-essential cmake qt5-qmake gcc g++
+sudo apt install -y git build-essential cmake qt5-qmake gcc g++ || error "Could not install dependencies"
 
 if grep -q bionic /etc/os-release; then
   #honestly, default QT probably works fine, but...
   ppa_name="theofficialgman/opt-qt-5.15.2-bionic-arm" && ppa_installer
-  sudo apt install -y qt515base #figure out the rest
+  sudo apt install -y qt515base || error "Could not install dependencies" #figure out the rest
 else
-  sudo apt install -y qtbase5-dev
+  sudo apt install -y qtbase5-dev || error "Could not install dependencies"
 fi
 
 cd ~
@@ -33,7 +33,7 @@ fi
 
 make -j$(nproc)
 
-sudo make install
+sudo make install || error "Make install failed"
 
 cd ~
 
