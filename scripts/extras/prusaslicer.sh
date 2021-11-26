5dc186549cfba0c597edd20067c7a2085a54a53f
theofficialgman <28281419+theofficialgman@users.noreply.github.com>
2021-11-26 01:50:08 -0500

    set cgal version info
    (currently missing from ppa, if ppa gets updated, this will get overwritten safely)
    Still waiting on upstream to implement a fix for CGAL 5.X

--- scripts/extras/prusaslicer.sh
+++ scripts/extras/prusaslicer.sh
@@ -9,7 +9,36 @@ source /etc/upstream-release/lsb-release
 case "$DISTRIB_CODENAME" in
     bionic)
         echo "Adding Boost 1.67 PPA..."
-        ppa_name="theofficialgman/boost1.67-bionic" && ppa_installer 
+        ppa_name="theofficialgman/boost1.67-bionic" && ppa_installer
+        sudo apt install libcgal-dev -y || error "Dependencies failed to install"
+        sudo tee /usr/lib/aarch64-linux-gnu/cmake/CGAL/CGALConfigVersion.cmake <<'EOF' >>/dev/null
+set(CGAL_MAJOR_VERSION 5)
+set(CGAL_MINOR_VERSION 0)
+set(CGAL_BUGFIX_VERSION 0)
+set(CGAL_VERSION_PUBLIC_RELEASE_VERSION "5.0.0")
+set(CGAL_VERSION_PUBLIC_RELEASE_NAME "CGAL-${CGAL_VERSION_PUBLIC_RELEASE_VERSION}")
+
+if (CGAL_BUGFIX_VERSION AND CGAL_BUGFIX_VERSION GREATER 0)
+  set(CGAL_CREATED_VERSION_NUM "${CGAL_MAJOR_VERSION}.${CGAL_MINOR_VERSION}.${CGAL_BUGFIX_VERSION}")
+else()
+  set(CGAL_CREATED_VERSION_NUM "${CGAL_MAJOR_VERSION}.${CGAL_MINOR_VERSION}")
+endif()
+
+set(PACKAGE_VERSION ${CGAL_CREATED_VERSION_NUM})
+
+if(PACKAGE_VERSION VERSION_LESS PACKAGE_FIND_VERSION)
+  set(PACKAGE_VERSION_COMPATIBLE FALSE)
+else()
+  if(PACKAGE_FIND_VERSION_MAJOR STREQUAL CGAL_MAJOR_VERSION)
+    set(PACKAGE_VERSION_COMPATIBLE TRUE)
+  else()
+    set(PACKAGE_VERSION_COMPATIBLE FALSE)
+  endif()
+  if(PACKAGE_FIND_VERSION STREQUAL PACKAGE_VERSION)
+    set(PACKAGE_VERSION_EXACT TRUE)
+  endif()
+endif()
+EOF
         ;;
 esac
 
