38d2821183667db69405825af56c06206350d53a
cobalt2727 <60624944+cobalt2727@users.noreply.github.com>
2021-12-12 20:05:15 -0500

    modify Discord script to (probably) work on Fedora
    long term this will have to be added to `update.sh` and trigger if the user is on Fedora

--- scripts/discord.sh
+++ scripts/discord.sh
@@ -2,32 +2,45 @@ echo "Discord $(uname -m) script started!"
 echo "CREDITS: https://github.com/SpacingBat3/electron-discord-webapp"
 sleep 2
 
-echo "Installing dependencies..."
-sudo apt install git curl -y
 
-# cd ~
-# mkdir -p discord-tmp/
-# cd discord-tmp/
+if grep -q fedora /etc/os-release; then
+  echo "Installing dependencies..."
+  sudo dnf install git curl -y
 
-echo "Removing previous legacy Discord installs..."
-sudo dpkg -r electron-discord-webapp
+  cd ~
+  mkdir -p discord-tmp/
+  cd discord-tmp/
+  rm *
 
-# echo "Downloading the most recent .deb from SpacingBat3's repository..."
-# curl -s https://api.github.com/repos/SpacingBat3/WebCord/releases/latest \
-#   | grep "browser_download_url.*arm64.deb" \
-#   | cut -d : -f 2,3 \
-#   | tr -d \" \
-#   | wget -qi -
+  echo "Downloading the most recent .rpm from SpacingBat3's repository..."
+  #note to self: fix this to be cross-architecture later
+  curl -s https://api.github.com/repos/SpacingBat3/WebCord/releases/latest \
+    | grep "browser_download_url.*arm64.rpm" \
+    | cut -d : -f 2,3 \
+    | tr -d \" \
+    | wget -qi -
 
-# echo "Done! Installing the package (and maybe fixing dependencies)..."
-# sudo dpkg -i *arm64.deb
-# sudo apt --fix-broken install -y
+  echo "Done! Installing the package (and maybe fixing dependencies)..."
+  sudo rpm -i *arm64.rpm || error "Webcord install failed"
+  ## can we find a Fedora equivalent to this? the only one I saw seemed like it needs to be done manually
+  #sudo apt --fix-broken install -y
+  
+  cd ..
+  rm -rf discord-tmp/
+fi
 
-# cd ..
-# rm -rf discord-tmp/
+if grep -q debian /etc/os-release; then
+  echo "Installing dependencies..."
+  sudo apt install git curl -y
 
-wget -qO- http://download.tuxfamily.org/webcordapt/add-repo.sh | sudo bash
-sudo apt install webcord -y || error "Webcord install failed"
+
+  echo "Removing previous legacy Discord installs..."
+  sudo dpkg -r electron-discord-webapp
+
+
+  wget -qO- http://download.tuxfamily.org/webcordapt/add-repo.sh | sudo bash
+  sudo apt install webcord -y || error "Webcord install failed"
+fi
 
 echo "Done!"
 echo "Sending you back to the menu..."
