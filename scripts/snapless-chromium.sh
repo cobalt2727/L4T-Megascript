a79a90fb24b12a7f0e23d3ea06210488f26fa87f
theofficialgman <28281419+theofficialgman@users.noreply.github.com>
2021-12-16 14:33:12 -0500

    add --no-sandbox to install for snapless-chromium

--- scripts/snapless-chromium.sh
+++ scripts/snapless-chromium.sh
@@ -10,3 +10,9 @@ ppa_name="saiarcot895/chromium-beta" && ppa_installer
 
 sudo apt install chromium-browser -y
 sudo apt --fix-broken install -y
+
+# browser crashes without --no-sanbox on newer ubuntu on jetson
+# remove all occurances of no-sanbox (to prevent double replacement)
+sudo sed -i 's/Exec=chromium-browser --no-sandbox/Exec=chromium-browser/g' /usr/share/applications/chromium-browser.desktop
+# add no-sandbox
+sudo sed -i 's/Exec=chromium-browser/Exec=chromium-browser --no-sandbox/g' /usr/share/applications/chromium-browser.desktop
