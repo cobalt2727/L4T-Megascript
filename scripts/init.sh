45fd2bcb20cd83b6cd94364fb5ddff4d9424c301
cobalt2727 <60624944+cobalt2727@users.noreply.github.com>
2021-12-18 15:44:55 -0600

    fix flatpak screwing with displayed categories

--- scripts/init.sh
+++ scripts/init.sh
@@ -97,6 +97,9 @@ fi
 sudo apt install apt-utils subversion wget flatpak qt5-style-plugins gnutls-bin cmake-data libjsoncpp1 libuv1 cmake -y
 # fonts-noto-cjk fonts-noto-cjk-extra fonts-migmix fonts-noto-color-emoji
 
+#fix error at https://forum.xfce.org/viewtopic.php?id=12752
+sudo chown $USER:$USER $HOME/.local/share/flatpak
+
 if [[ $jetson_model ]]; then
   # fix up an issue with running flatpaks by enabling non-privileged user namespaces
   # this is enabled in the kernel defconfig... no clue why it doesn't work
