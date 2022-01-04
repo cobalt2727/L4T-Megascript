0a75c4cd85a37d50fcb70497d22383a3b12686f5
theofficialgman <28281419+theofficialgman@users.noreply.github.com>
2022-01-03 19:21:46 -0500

    add click link to categories

--- assets/conversion_wiki.sh
+++ assets/conversion_wiki.sh
@@ -14,7 +14,31 @@ You're in luck - we've got a Discord server: <a href="https://discord.gg/abgW2AG
     Join the <img src="https://img.shields.io/discord/719014537277210704.svg?color=7289da&label=Discord%20server&logo=discord" alt="Join the Discord server"></a>
 
 ### Below is a list of supported Megascript install scripts with a short description and link to the source.
+[Games and Emulators](https://github.com/cobalt2727/L4T-Megascript/wiki#games-and-emulators)
 EOF
+
+  for ((i = 1; i <= ${length}; i++)); do
+    if [[ ! " ${hidden[@]} " =~ " ${i} " ]]; then
+      fn=""
+      d=""
+      e=""
+      f=""
+      sn=""
+      line=$(sed -n $i"p" <"/tmp/megascript_apps.txt")
+      if [[ "$line" != \#* ]]; then
+        eval "$(echo "$line" | tr ";" "\n")"
+        ff="$(echo "$f" | sed -e 's/_/ /g' | sed -e "s/\b\(.\)/\u\1/g")"
+        fl="$(echo "$f" | sed -e 's/_/-/g')"
+        if [[ "$ff" != "$prev_f" ]];then
+            echo '['"$ff"']((https://github.com/cobalt2727/L4T-Megascript/wiki#'"$fl"')'
+        fi
+        if [ "$f" != "scripts" ]; then
+            f="scripts/$f"
+        fi
+        prev_f="$ff"
+      fi
+    fi
+  done
 
   for ((i = 1; i <= ${length}; i++)); do
     if [[ ! " ${hidden[@]} " =~ " ${i} " ]]; then
