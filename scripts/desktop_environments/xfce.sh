7f90971537b5d35f910ccb91d1b0ee5ea15fe1ea
cobalt2727 <60624944+cobalt2727@users.noreply.github.com>
2022-01-16 22:35:36 -0500

    fix XFCE audio bug reported in discord
    https://discord.com/channels/521977609182117891/588138053735022632/932476147789332511

--- scripts/desktop_environments/xfce.sh
+++ scripts/desktop_environments/xfce.sh
@@ -4,7 +4,7 @@ clear -x
 echo "Installing the XFCE desktop environment."
 
 ##prompt yes/no
-sudo apt install xfce4 xfce4-goodies -y || error "Could not install dependencies"
+sudo apt install xfce4 xfce4-goodies xfce4-volumed -y || error "Could not install dependencies"
 
 bash -c "$(curl -s https://raw.githubusercontent.com/$repository_username/L4T-Megascript/$repository_branch/scripts/desktop_environments/common.sh)"
 
