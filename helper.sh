1546f02478e6bb0d648574514eed1f57d6485797
cobalt2727 <60624944+cobalt2727@users.noreply.github.com>
2022-01-27 20:02:48 -0500

    make sure helper.sh pulls system info

--- helper.sh
+++ helper.sh
@@ -24,6 +24,9 @@ unset functions_downloaded
 source <(curl -s https://raw.githubusercontent.com/$repository_username/L4T-Megascript/$repository_branch/functions.sh)
 [[ ! -z ${functions_downloaded+z} ]] && status "Functions Loaded" || error_fatal "Oh no! Something happened to your internet! Exiting the Megascript, pleast fix your internet and try again!"
 
+#acquire system info for use in scripts
+get_system
+
 mkdir -p "$HOME/L4T-Megascript/logs"
 logfile="$HOME/L4T-Megascript/logs/install-incomplete-${1////-}.log"
 if [ -f "$logfile" ] || [ -f "$(echo "$logfile" | sed 's+-incomplete-+-success-+g')" ] || [ -f "$(echo "$logfile" | sed 's+-incomplete-+-fail-+g')" ];then
