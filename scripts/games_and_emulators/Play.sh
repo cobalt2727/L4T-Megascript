c089c6e2241b2db69e1e7e5c16688babc28f5869
cobalt2727 <60624944+cobalt2727@users.noreply.github.com>
2022-02-04 13:51:31 -0500

    add dependency installs
    untested on 18.04 - my Nano apparently changed its local IP or something so I can't SSH in, will probably fix later today

--- scripts/games_and_emulators/Play.sh
+++ scripts/games_and_emulators/Play.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 
-# TODO: apt install deps
-# example from gh README:
-### sudo apt install cmake libalut-dev qt5-default libevdev-dev libqt5x11extras5-dev libsqlite3-dev
+echo "Play script started!"
+
+sudo apt install -y cmake libalut-dev libevdev-dev libqt5x11extras5-dev libsqlite3-dev qtbase5-dev qtchooser qt5-qmake qtbase5-dev-tools || error "Failed to install dependencies!"
+package_available qt5-default
+if [[ $? == "0" ]]; then
+  sudo apt install -y qt5-default || error "Failed to install dependencies!"
+fi
 
 git clone https://github.com/jpd002/Play-
-
 cd Play-/
 
 git submodule update --init --recursive
@@ -14,7 +17,11 @@ git pull --recurse-submodules -j4
 mkdir -p build 
 cd build
 
-cmake .. -G "Unix Makefiles" -DCMAKE_BUILD_TYPE=Release -DCMAKE_CXX_FLAGS=-mcpu=native -DCMAKE_C_FLAGS=-mcpu=native
-cmake --build . -j$(nproc)
+echo "Building..."
+cmake .. -G "Unix Makefiles" -DCMAKE_BUILD_TYPE=Release -DCMAKE_CXX_FLAGS=-mcpu=native -DCMAKE_C_FLAGS=-mcpu=native || error "Cmake failed!"
+cmake --build . -j$(nproc) || error "Build failed!"
 
-sudo make install
+sudo make install || "Make install failed!"
+
+echo "Done!"
+sleep 2
