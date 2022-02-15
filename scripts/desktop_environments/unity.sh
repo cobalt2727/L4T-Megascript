701125e2a26064ccf84c847cf5aaad34c129591b
cobalt2727 <60624944+cobalt2727@users.noreply.github.com>
2022-02-14 20:59:28 -0500

    modify package and parameter used for Unity script

--- scripts/desktop_environments/unity.sh
+++ scripts/desktop_environments/unity.sh
@@ -5,7 +5,7 @@ echo "You are about to install the Unity desktop environment."
 echo "Under most cases this shouldn't break anything and install alongside of your existing desktop, but just to be sure..."
 echo "Are you sure you want to continue?"
 
-##prompt yes/no
-sudo apt install ubuntu-unity-desktop --install-suggests -y || error "Could not install dependencies"
+##the ubuntu-unity-desktop package tries to pull snap, plus it's not what Nvidia ships by default
+sudo apt install unity-session --no-install-recommends -y || error "Could not install dependencies"
 
 echo "Going back to the main menu..."
