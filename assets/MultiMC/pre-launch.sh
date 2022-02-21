090a1453c272211497b8f01b8b69bac313e64c6c
theofficialgman <28281419+theofficialgman@users.noreply.github.com>
2022-02-20 22:43:01 -0500

    add YAD dialog notice about mod-manager being disabled

--- assets/MultiMC/pre-launch.sh
+++ assets/MultiMC/pre-launch.sh
@@ -26,6 +26,15 @@ then
     fi
     echo "Skipping automatic mod installation script as minecraft-mod-manager is currently broken due to Modrinth API changes"
     echo "See L4T-Megascript Discord for more info"
+    echo "Issue tracked here: https://github.com/Senth/minecraft-mod-manager/issues/150"
+    echo -e "Skipping automatic mod installation script as minecraft-mod-manager is currently broken due to Modrinth API changes.\
+ See L4T-Megascript Discord for more info. When this is fixed, this notice will be removed and the script will automatically update and function again.\
+\n\nIssue tracked here: https://github.com/Senth/minecraft-mod-manager/issues/150" | yad --image "dialog-info" \
+    --borders="20" --center --fixed\
+    --window-icon=/usr/share/icons/L4T-Megascript.png \
+    --text-info --fontname="@font@ 11" --wrap --width=800 --height=250 --timeout=20 --timeout-indicator=bottom\
+    --show-uri \
+    --button="OK, Skipping Mod Installtion Script":0
 #     megascript_mods=$(sed -n "p" <"$MMC_ROOT/scripts/megascript-mods.txt")
 #     user_mods=$(sed -n "p" <"$MMC_ROOT/scripts/user-mods.txt")
 #     cd "$INST_DIR"
