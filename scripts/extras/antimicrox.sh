13980050846276d2a2d366d9ef9b8175442efab9
theofficialgman <28281419+theofficialgman@users.noreply.github.com>
2022-03-22 19:06:30 -0400

    add error check to dependencies installation for antimicrox

--- scripts/extras/antimicrox.sh
+++ scripts/extras/antimicrox.sh
@@ -7,7 +7,7 @@ echo "a graphical program used to map gamepad keys to keyboard."
 # Dependancies
 sudo apt install -y git gcc cmake extra-cmake-modules \
     qttools5-dev qttools5-dev-tools libsdl2-dev \
-    libxi-dev libxtst-dev libx11-dev itstool gettext python3-libxml2
+    libxi-dev libxtst-dev libx11-dev itstool gettext python3-libxml2 || error "Could not install dependencies"
 
 case "$DISTRIB_CODENAME" in
   bionic)
