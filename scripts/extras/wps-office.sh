0ffc9207658f6500b89d831bd81bc0182d5d6224
cobalt2727 <60624944+cobalt2727@users.noreply.github.com>
2022-05-23 17:19:52 -0400

    Update wps-office.sh

--- scripts/extras/wps-office.sh
+++ scripts/extras/wps-office.sh
@@ -5,9 +5,9 @@ echo ttf-mscorefonts-installer msttcorefonts/accepted-mscorefonts-eula select tr
 
 # download apt package
 cd /tmp
-rm -f wps-office_11.1.0.10702_arm64.deb
-wget https://wdl1.cache.wps.cn/wps/download/ep/Linux2019/10702/wps-office_11.1.0.10702_arm64.deb --progress=bar:force:noscroll || error "Failed to download wps office suite"
+rm -f wps-office_*.deb
+wget https://wps-linux-personal.wpscdn.cn/wps/download/ep/Linux2019/10976/wps-office_11.1.0.10976_arm64.deb --progress=bar:force:noscroll || error "Failed to download wps office suite"
 
 # install wps office apt package
-sudo apt install ./wps-office_11.1.0.10702_arm64.deb -y || error "Failed to install wps office suite"
+sudo apt install ./wps-office_*.deb -y || error "Failed to install wps office suite"
 status "Successfully install WPS Office Suite"
