cb79932f4a7ef8ebd5efd854128b4f485022723f
theofficialgman <28281419+theofficialgman@users.noreply.github.com>
2022-06-13 22:15:50 -0400

    add pi-apps to scripts

---
+++ scripts/pi-apps.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# use official pi-apps installation command
+wget -qO- https://raw.githubusercontent.com/Botspot/pi-apps/master/install | bash
+
+status_green "Pi-Apps is installed!"
+status_green "Sending you back to the menu..."
