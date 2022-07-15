8db64826b5e5fe659f0ce6cebb9d44f3396a394f
theofficialgman <28281419+theofficialgman@users.noreply.github.com>
2022-07-15 00:44:13 -0400

    rework sdl2 install helper to install armhf if enabled

--- scripts/sdl2_install_helper.sh
+++ scripts/sdl2_install_helper.sh
@@ -4,49 +4,31 @@
 #note that this is an apt package in the Switchroot repository, its ok for it to fail on other devices
 sudo apt install joycond -y
 
+get_system
 if ! package_installed "libsdl2-dev" || $(dpkg --compare-versions $(dpkg-query -f='${Version}' --show libsdl2-dev) lt 2.0.14); then
-    get_system
-    if [[ "$dpkg_architecture" == "arm64" ]]; then
-        mkdir -p /tmp/sdl2
-        cd /tmp/sdl2 || error "Could not change directory"
-        rm -rf ./*
-        wget https://github.com/$repository_username/L4T-Megascript/raw/$repository_branch/assets/SDL2/libsdl2-2.0-0_2.0.14%2B5_arm64.deb --progress=bar:force:noscroll && wget https://github.com/$repository_username/L4T-Megascript/raw/$repository_branch/assets/SDL2/libsdl2-dbg_2.0.14%2B5_arm64.deb --progress=bar:force:noscroll && wget https://github.com/$repository_username/L4T-Megascript/raw/$repository_branch/assets/SDL2/libsdl2-dev_2.0.14%2B5_arm64.deb --progress=bar:force:noscroll || error "Could not download files, make sure your internet is working"
-
-        sudo apt install ./*.deb -y -f --allow-change-held-packages || error "SDL2 Packages Failed to install"
-        rm -rf ./*
-
-        # keep this old code for compiling SDL2 if its ever needed
-
-        # sudo apt-get --assume-yes install git
-        # cd /tmp
-        # mkdir -p temp_install_sdl2
-        # cd temp_install_sdl2
-        # curl -s 'https://raw.githubusercontent.com/theofficialgman/RetroPie-Setup/master/retropie_packages.sh' > retropie_packages.sh
-        # chmod -R 777 retropie_packages.sh
-        # curl -s 'https://raw.githubusercontent.com/theofficialgman/RetroPie-Setup/master/retropie_setup.sh' > retropie_setup.sh
-        # mkdir -p scriptmodules
-        # cd ./scriptmodules
-        # curl -s 'https://raw.githubusercontent.com/theofficialgman/RetroPie-Setup/master/scriptmodules/system.sh' > system.sh
-        # curl -s 'https://raw.githubusercontent.com/theofficialgman/RetroPie-Setup/master/scriptmodules/helpers.sh' > helpers.sh
-        # curl -s 'https://raw.githubusercontent.com/theofficialgman/RetroPie-Setup/master/scriptmodules/packages.sh' > packages.sh
-        # curl -s 'https://raw.githubusercontent.com/theofficialgman/RetroPie-Setup/master/scriptmodules/inifuncs.sh' > inifuncs.sh
-        # mkdir -p supplementary
-        # cd ./supplementary
-        # curl -s 'https://raw.githubusercontent.com/theofficialgman/RetroPie-Setup/master/scriptmodules/supplementary/sdl2.sh' > sdl2.sh
-        # cd /tmp/temp_install_sdl2
-
-        # #auto install sdl2 and then remove unneeded files
-        # sudo ./retropie_packages.sh sdl2
-        # cd /tmp
-        # sudo rm -rf temp_install_sdl2
-
-        cd
-        echo "Successfully Installed Newest SDL2 Version"
-    else
-        echo "Sorry we don't host binaries on the megascript for non-arm64 architectures"
-        echo "Skipping updated SDL2 install and installing normal sdl2 packages"
-        sudo apt install libsdl2-2.0-0 libsdl2-dev -y
-    fi
+  if [[ "$dpkg_architecture" == "arm64" ]]; then
+    mkdir -p /tmp/sdl2
+    cd /tmp/sdl2 || error "Could not change directory"
+    rm -rf ./*
+    wget https://github.com/$repository_username/L4T-Megascript/raw/$repository_branch/assets/SDL2/libsdl2-2.0-0_2.0.14%2B5_arm64.deb --progress=bar:force:noscroll && wget https://github.com/$repository_username/L4T-Megascript/raw/$repository_branch/assets/SDL2/libsdl2-dev_2.0.14%2B5_arm64.deb --progress=bar:force:noscroll || error "Could not download files, make sure your internet is working"
+    sudo apt install ./*.deb -y -f --allow-change-held-packages || error "SDL2 ARM64 Packages Failed to install"
+    rm -rf ./*
+    cd
+    echo "Successfully Installed Newest ARM64 SDL2 Version"
+  else
+    echo "Sorry we don't host binaries on the megascript for non-arm64 architectures"
+    echo "Skipping updated SDL2 install and installing normal sdl2 packages"
+    sudo apt install libsdl2-2.0-0 libsdl2-dev -y
+  fi
 else
-    echo "Already Installed Newest SDL2 Version"
+  echo "Already Installed Newest SDL2 Version"
+fi
+
+if [[ "$dpkg_architecture" == "arm64" ]] && [[ $(dpkg --print-foreign-architectures) == *"armhf"* ]] && ( apt-cache policy libsdl2-2.0-0:armhf | grep -q 'Installed: (none)' || $(dpkg --compare-versions $(dpkg-query -f='${Version}' --show libsdl2-2.0-0:armhf) lt 2.0.14) ); then
+  mkdir -p /tmp/sdl2
+  cd /tmp/sdl2 || error "Could not change directory"
+  rm -rf ./*
+  wget https://github.com/$repository_username/L4T-Megascript/raw/$repository_branch/assets/SDL2/libsdl2-2.0-0_2.0.14%2B5_armhf.deb --progress=bar:force:noscroll || error "Could not download files, make sure your internet is working"
+  sudo apt install ./*.deb -y -f --allow-change-held-packages || error "SDL2 ARMhf Packages Failed to install"
+  rm -rf ./*
 fi
