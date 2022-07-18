5dd0d3b872ed0c887b2ca2bbfb9f1fbac03b3618
theofficialgman <28281419+theofficialgman@users.noreply.github.com>
2022-07-17 22:54:53 -0400

    add more common box86 wrapped depends

--- scripts/box86.sh
+++ scripts/box86.sh
@@ -42,7 +42,7 @@ fi
 cd
 # gcc 7 produces errors when compiling on arm/arm64 on both box86 and box64
 # there is no available gcc-11 armhf cross compiler so the gcc-8 armhf cross compiler is used and it works fine
-sudo apt install -y cmake git build-essential gcc-8-arm-linux-gnueabihf libsdl2-mixer-2.0-0:armhf libc6:armhf libx11-6:armhf libgdk-pixbuf2.0-0:armhf libgtk2.0-0:armhf libstdc++6:armhf libpng16-16:armhf libcal3d12v5:armhf libopenal1:armhf libcurl4:armhf osspd:armhf libjpeg62:armhf libudev1:armhf || error "Could install box86 dependencies"
+sudo apt install -y cmake git build-essential gcc-8-arm-linux-gnueabihf libsdl2-mixer-2.0-0:armhf libsdl2-image-2.0-0:armhf libsdl2-net-2.0-0:armhf libsdl2-2.0-0:armhf libc6:armhf libx11-6:armhf libgdk-pixbuf2.0-0:armhf libgtk2.0-0:armhf libstdc++6:armhf libpng16-16:armhf libcal3d12v5:armhf libopenal1:armhf libcurl4:armhf osspd:armhf libjpeg62:armhf libudev1:armhf || error "Could install box86 dependencies"
 
 git clone https://github.com/ptitSeb/box86.git
 cd box86
