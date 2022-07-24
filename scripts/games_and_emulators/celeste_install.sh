cb10ded0547009fe9b05f6b258141ce398b1141a
theofficialgman <28281419+theofficialgman@users.noreply.github.com>
2022-07-24 16:47:55 -0400

    install celeste classic to /usr/local

--- scripts/games_and_emulators/celeste_install.sh
+++ scripts/games_and_emulators/celeste_install.sh
@@ -1,30 +1,17 @@
 #!/bin/bash
 
-function error {
-  echo -e "\\e[91m$1\\e[39m"
-  sleep 3
-  exit 1
-}
-
-clear -x
-cd /tmp
-echo "Celeste Pico 8 script started!"
-echo "Installing Dependencies"
-echo
-sudo apt install --assume-yes subversion libsdl1.2-dev libsdl2-mixer-dev libsdl2-dev libsdl-mixer1.2-dev libsdl2-image-dev
-sudo apt install --assume-yes libsndio*
-bash -c "$(curl -s https://raw.githubusercontent.com/$repository_username/L4T-Megascript/$repository_branch/scripts/sdl2_install_helper.sh)"
-echo
-echo "Downloading the game"
+cd /tmp || error "Could not move to /tmp folder"
+status "Installing Dependencies"
+sudo apt install -y libsdl2-mixer-dev libsdl2-dev libsdl2-image-dev libsndio-dev || exit 1
+status "Downloading the game"
 sudo rm -rf /tmp/ccleste
-git clone https://github.com/lemon32767/ccleste.git --depth=1 || error "Could Not Pull Latest Source Code"
-cd ccleste
+git clone https://github.com/lemon32767/ccleste.git --depth=1 && cd ccleste || error "Could Not Pull Latest Source Code"
 rm -rf gamecontrollerdb.txt
-echo "Compiling the game"
+status "Compiling the game"
 make -j$(nproc) || error "Compilation failed"
 wget https://raw.githubusercontent.com/gabomdq/SDL_GameControllerDB/master/gamecontrollerdb.txt
 
-path="/usr/share/celeste"
+path="/usr/local/share/celeste"
 
 sudo rm -rf $path
 sudo mkdir $path
@@ -38,23 +25,27 @@ sudo tee $path/ccleste-start-fullscreen.txt <<'EOF'
 True
 EOF
 
+# set file ownership
+sudo chown root:root $path/* || error "Could not set file ownership"
+sudo chown root:root $path/data/* || error "Could not set file ownership"
+
 sudo rm -rf /tmp/ccleste
 
-echo "Adding to applications menu"
+# remove old folder location if it exists
+if [ -f /usr/share/celeste/ccleste ]; then
+  sudo rm -rf /usr/share/celeste /usr/share/applications/celeste.desktop
+fi
 
-sudo tee /usr/share/applications/celeste.desktop <<'EOF'
+status "Adding to applications menu"
+
+sudo tee /usr/local/share/applications/celeste.desktop <<'EOF'
 [Desktop Entry]
 Type=Application
-Exec=/usr/share/celeste/ccleste
-Path=/usr/share/celeste
-Name=Celeste
-Comment=celeste
-Icon=/usr/share/celeste/icon.png
+Exec=/usr/local/share/celeste/ccleste
+Path=/usr/local/share/celeste
+Name=Celeste Classic
+Icon=/usr/local/share/celeste/icon.png
 Categories=Game
 EOF
 
-echo
-echo "Game installed!"
-sleep 2
-echo
-echo "Sending you back to the main menu..."
+status_green "Game installed!"
