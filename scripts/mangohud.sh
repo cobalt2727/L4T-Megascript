8b5f919766c788ce1b8fa9a0c7bba829d7e3e5b0
theofficialgman <28281419+theofficialgman@users.noreply.github.com>
2022-08-15 19:00:40 -0400

    install glslang-tools for mangohud
    download from debian buster if not available in the repos

--- scripts/mangohud.sh
+++ scripts/mangohud.sh
@@ -2,6 +2,18 @@ clear -x
 
 echo "MangoHud script started!"
 
+package_available glslang-tools
+if [[ $? == "0" ]]; then
+  sudo apt install glslang-tools -y || error "Could not install apt dependencies"
+else
+  rm -rf /tmp/mangohud-debs
+  mkdir /tmp/mangohud-debs
+  cd /tmp/mangohud-debs
+  wget http://ftp.us.debian.org/debian/pool/main/s/spirv-tools/spirv-tools_2020.6-1~bpo10+1_arm64.deb http://ftp.us.debian.org/debian/pool/main/g/glslang/glslang-tools_8.13.3743-1~bpo10+1_arm64.deb
+  sudo apt install ./*.deb -y || error "Could not install apt dependencies"
+  rm -rf /tmp/mangohud-debs
+fi
+
 sudo apt install ninja-build git build-essential python3-pip -y  || error "Could not install apt dependencies"
 python3 -m pip install --upgrade pip meson || error "Could not install python dependencies"
 cd /tmp
