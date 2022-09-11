d7d8b319a79df562bc7deb902dfdf0e477dfcdf9
cobalt2727 <60624944+cobalt2727@users.noreply.github.com>
2022-09-11 14:07:42 -0400

    Metaforce: fix missing QT6 dep

--- scripts/games_and_emulators/metaforce.sh
+++ scripts/games_and_emulators/metaforce.sh
@@ -61,9 +61,9 @@ else
   sudo apt install -y --no-install-recommends libvulkan1 libvulkan-dev || error "Failed to install dependencies!"
 fi
 
-package_available libqt6svg6-dev # note to self: set up a duplicate of this install check to match the PPAs provided by https://launchpad.net/~rncbc at some point
+package_available qt6-tools-dev # note to self: set up a duplicate of this install check to match the PPAs provided by https://launchpad.net/~rncbc at some point
 if [[ $? == "0" ]]; then         # this 22.04+ dep is really only needed for the submodule https://github.com/AxioDL/amuse/ - it can be safely ignored
-  sudo apt install -y libqt6svg6-dev libqt6core5compat6-dev qt6-tools-dev-tools qt6-declarative-dev || error "Failed to install QT6 development libraries!"
+  sudo apt install -y qt6-tools-dev libqt6svg6-dev libqt6core5compat6-dev qt6-tools-dev-tools qt6-declarative-dev || error "Failed to install QT6 development libraries!"
 fi
 
 git clone --recursive https://github.com/AxioDL/metaforce.git -j$(nproc)
