d12be7422cc289e2511f15c3a8cd4ef36f802c01
cobalt2727 <60624944+cobalt2727@users.noreply.github.com>
2022-09-14 14:07:24 -0400

    Libreoffice: fix comment

--- scripts/libreoffice.sh
+++ scripts/libreoffice.sh
@@ -12,7 +12,7 @@ case "$__os_id" in
             sudo apt install -y --no-install-recommends libreoffice-gnome || error "Failed to install dependencies"
         fi
         elif echo $XDG_CURRENT_DESKTOP | grep -q 'Plasma'; then
-            # sudo apt install -y --no-install-recommends libreoffice-kde5 || error "Failed to install dependencies"
+            sudo apt install -y --no-install-recommends libreoffice-kde5 || error "Failed to install dependencies"
         fi
     ;;
     Fedora)
