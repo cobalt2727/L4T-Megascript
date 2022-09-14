7a2c441e9e262a115d3e9b1dab3496ca1b3ee9fd
cobalt2727 <60624944+cobalt2727@users.noreply.github.com>
2022-09-14 13:52:46 -0400

    LibreOffice: temp disable KDE integration

--- scripts/libreoffice.sh
+++ scripts/libreoffice.sh
@@ -9,9 +9,9 @@ case "$__os_id" in
         if echo $XDG_CURRENT_DESKTOP | grep -q 'GNOME'; then
             sudo apt install -y --no-install-recommends libreoffice-gnome || error "Failed to install dependencies"
         fi
-        elif echo $XDG_CURRENT_DESKTOP | grep -q 'Plasma'; then
-            sudo apt install -y --no-install-recommends libreoffice-kde5 || error "Failed to install dependencies"
-        fi
+        # elif echo $XDG_CURRENT_DESKTOP | grep -q 'Plasma'; then
+            # sudo apt install -y --no-install-recommends libreoffice-kde5 || error "Failed to install dependencies"
+        # fi
     ;;
     Fedora)
         sudo dnf install -y libreoffice
