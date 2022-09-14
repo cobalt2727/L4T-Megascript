80623f25be54802af44d40d4fa80d1fa3672ac65
cobalt2727 <60624944+cobalt2727@users.noreply.github.com>
2022-09-14 13:49:48 -0400

    LibreOffice: add script

---
+++ scripts/libreoffice.sh
@@ -0,0 +1,24 @@
+echo "LibreOffice script started!"
+sleep 2
+
+case "$__os_id" in
+    Raspbian | Debian | LinuxMint | Linuxmint | Ubuntu | [Nn]eon | Pop | Zorin | [eE]lementary | [jJ]ing[Oo][sS])
+        ppa_name="libreoffice/ppa" && ppa_installer
+        sudo apt install -y --no-install-recommends libreoffice libxrender1 libreoffice-gtk || error "Failed to install dependencies"
+
+        if echo $XDG_CURRENT_DESKTOP | grep -q 'GNOME'; then
+            sudo apt install -y --no-install-recommends libreoffice-gnome || error "Failed to install dependencies"
+        fi
+        elif echo $XDG_CURRENT_DESKTOP | grep -q 'Plasma'; then
+            sudo apt install -y --no-install-recommends libreoffice-kde5 || error "Failed to install dependencies"
+        fi
+    ;;
+    Fedora)
+        sudo dnf install -y libreoffice
+    ;;
+    *)
+        error "Unknown distro detected"
+    ;;
+esac
+
+echo "Done!"
