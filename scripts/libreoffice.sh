eabbfb29a13b666aefcb1e015cc911033b849f37
cobalt2727 <60624944+cobalt2727@users.noreply.github.com>
2022-09-14 14:13:04 -0400

    LibreOffice: line ending fix?

--- scripts/libreoffice.sh
+++ scripts/libreoffice.sh
@@ -4,22 +4,22 @@ sleep 2
 get_system
 
 case "$__os_id" in
-    Raspbian | Debian | LinuxMint | Linuxmint | Ubuntu | [Nn]eon | Pop | Zorin | [eE]lementary | [jJ]ing[Oo][sS])
-        ppa_name="libreoffice/ppa" && ppa_installer
-        sudo apt install -y --no-install-recommends libreoffice libxrender1 libreoffice-gtk || error "Failed to install dependencies"
+Raspbian | Debian | LinuxMint | Linuxmint | Ubuntu | [Nn]eon | Pop | Zorin | [eE]lementary | [jJ]ing[Oo][sS])
+    ppa_name="libreoffice/ppa" && ppa_installer
+    sudo apt install -y --no-install-recommends libreoffice libxrender1 libreoffice-gtk || error "Failed to install dependencies"
 
-        if echo $XDG_CURRENT_DESKTOP | grep -q 'GNOME'; then
-            sudo apt install -y --no-install-recommends libreoffice-gnome || error "Failed to install dependencies"
-        fi
-        elif echo $XDG_CURRENT_DESKTOP | grep -q 'Plasma'; then
-            sudo apt install -y --no-install-recommends libreoffice-kde5 || error "Failed to install dependencies"
-        fi
+    if echo $XDG_CURRENT_DESKTOP | grep -q 'GNOME'; then
+        sudo apt install -y --no-install-recommends libreoffice-gnome || error "Failed to install dependencies"
+    fi
+    elif echo $XDG_CURRENT_DESKTOP | grep -q 'Plasma'; then
+        sudo apt install -y --no-install-recommends libreoffice-kde5 || error "Failed to install dependencies"
+    fi
     ;;
-    Fedora)
-        sudo dnf install -y libreoffice
+Fedora)
+    sudo dnf install -y libreoffice
     ;;
-    *)
-        error "Unknown distro detected"
+*)
+    error "Unknown distro detected"
     ;;
 esac
 
