31c9de0a8de916e4973895a47bd8799499887e89
cobalt2727 <60624944+cobalt2727@users.noreply.github.com>
2022-09-14 13:59:00 -0400

    LibreOffice: get_system
    this really should just be in the core or functions script at this point

--- scripts/libreoffice.sh
+++ scripts/libreoffice.sh
@@ -1,6 +1,8 @@
 echo "LibreOffice script started!"
 sleep 2
 
+get_system
+
 case "$__os_id" in
     Raspbian | Debian | LinuxMint | Linuxmint | Ubuntu | [Nn]eon | Pop | Zorin | [eE]lementary | [jJ]ing[Oo][sS])
         ppa_name="libreoffice/ppa" && ppa_installer
@@ -9,9 +11,9 @@ case "$__os_id" in
         if echo $XDG_CURRENT_DESKTOP | grep -q 'GNOME'; then
             sudo apt install -y --no-install-recommends libreoffice-gnome || error "Failed to install dependencies"
         fi
-        # elif echo $XDG_CURRENT_DESKTOP | grep -q 'Plasma'; then
+        elif echo $XDG_CURRENT_DESKTOP | grep -q 'Plasma'; then
             # sudo apt install -y --no-install-recommends libreoffice-kde5 || error "Failed to install dependencies"
-        # fi
+        fi
     ;;
     Fedora)
         sudo dnf install -y libreoffice
