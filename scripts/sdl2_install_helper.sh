a67fdbe4590d92a8a65d845fe04015aa64a7dbc6
theofficialgman <28281419+theofficialgman@users.noreply.github.com>
2022-12-20 15:56:12 -0500

    SDL2: updated to 2.26.1

--- scripts/sdl2_install_helper.sh
+++ scripts/sdl2_install_helper.sh
@@ -4,14 +4,14 @@
 #note that this is an apt package in the Switchroot repository, its ok for it to fail on other devices
 sudo apt install joycond -y
 
-if ! package_installed "libsdl2-dev" || $(dpkg --compare-versions $(dpkg-query -f='${Version}' --show libsdl2-dev) lt 2.23.1); then
+if ! package_installed "libsdl2-dev" || $(dpkg --compare-versions $(dpkg-query -f='${Version}' --show libsdl2-dev) lt 2.26.1); then
   if [[ "$dpkg_architecture" == "arm64" ]]; then
     mkdir -p /tmp/sdl2
     cd /tmp/sdl2 || error "Could not change directory"
     rm -rf ./*
-    wget https://github.com/$repository_username/L4T-Megascript/raw/$repository_branch/assets/SDL2/libsdl2-2.0-0_2.23.1%2Bdfsg-1_arm64.deb --progress=bar:force:noscroll && wget https://github.com/$repository_username/L4T-Megascript/raw/$repository_branch/assets/SDL2/libsdl2-dev_2.23.1%2Bdfsg-1_arm64.deb --progress=bar:force:noscroll || error "Could not download files, make sure your internet is working"
+    wget https://github.com/$repository_username/L4T-Megascript/raw/$repository_branch/assets/SDL2/libsdl2-2.0-0_2.26.1%2Bdfsg-1_arm64.deb --progress=bar:force:noscroll && wget https://github.com/$repository_username/L4T-Megascript/raw/$repository_branch/assets/SDL2/libsdl2-dev_2.26.1%2Bdfsg-1_arm64.deb --progress=bar:force:noscroll || error "Could not download files, make sure your internet is working"
     if [[ $(dpkg --print-foreign-architectures) == *"armhf"* ]]; then
-      wget https://github.com/$repository_username/L4T-Megascript/raw/$repository_branch/assets/SDL2/libsdl2-2.0-0_2.23.1%2Bdfsg-1_armhf.deb --progress=bar:force:noscroll || error "Could not download files, make sure your internet is working"
+      wget https://github.com/$repository_username/L4T-Megascript/raw/$repository_branch/assets/SDL2/libsdl2-2.0-0_2.26.1%2Bdfsg-1_armhf.deb --progress=bar:force:noscroll || error "Could not download files, make sure your internet is working"
     fi
     sudo apt install ./*.deb -y -f --allow-change-held-packages || error "SDL2 Packages Failed to install"
     rm -rf ./*
@@ -26,11 +26,11 @@ else
   echo "Already Installed Newest SDL2 Version"
 fi
 
-if [[ "$dpkg_architecture" == "arm64" ]] && [[ $(dpkg --print-foreign-architectures) == *"armhf"* ]] && (apt-cache policy libsdl2-2.0-0:armhf | grep -q 'Installed: (none)' || $(dpkg --compare-versions $(dpkg-query -f='${Version}' --show libsdl2-2.0-0:armhf) lt 2.23.1)); then
+if [[ "$dpkg_architecture" == "arm64" ]] && [[ $(dpkg --print-foreign-architectures) == *"armhf"* ]] && (apt-cache policy libsdl2-2.0-0:armhf | grep -q 'Installed: (none)' || $(dpkg --compare-versions $(dpkg-query -f='${Version}' --show libsdl2-2.0-0:armhf) lt 2.26.1)); then
   mkdir -p /tmp/sdl2
   cd /tmp/sdl2 || error "Could not change directory"
   rm -rf ./*
-  wget https://github.com/$repository_username/L4T-Megascript/raw/$repository_branch/assets/SDL2/libsdl2-2.0-0_2.23.1%2Bdfsg-1_armhf.deb --progress=bar:force:noscroll || error "Could not download files, make sure your internet is working"
+  wget https://github.com/$repository_username/L4T-Megascript/raw/$repository_branch/assets/SDL2/libsdl2-2.0-0_2.26.1%2Bdfsg-1_armhf.deb --progress=bar:force:noscroll || error "Could not download files, make sure your internet is working"
   sudo apt install ./*.deb -y -f --allow-change-held-packages || error "SDL2 ARMhf Package Failed to install"
   rm -rf ./*
 fi
