65365298ee5670bd878e3dbca67520189d92adea
theofficialgman <28281419+theofficialgman@users.noreply.github.com>
2023-01-03 09:41:10 -0500

    Try mesa ppa stupidity another way

--- scripts/games_and_emulators/steam.sh
+++ scripts/games_and_emulators/steam.sh
@@ -30,7 +30,7 @@ ppa_name="kisak/kisak-mesa" && ppa_installer
 sudo sed -i "s/\"library_path\" : .*/\"library_path\" : \"libEGL_mesa.so.0\"/g" "/usr/share/glvnd/egl_vendor.d/50_mesa.json"
 sudo sed -i 's:^DISABLE_MESA_EGL="1":DISABLE_MESA_EGL="0":' /etc/systemd/nv.sh
 
-sudo apt install mesa-common-dev -y
+sudo apt-get dist-upgrade -y
 # if old mesa-common-dev is already installed, it causes conflicts on intial upgrade and an apt --fix-broken install is necessary to finish installation
 sudo apt --fix-broken install -y
 sudo apt-get dist-upgrade -y || error "Could not upgrade MESA (needed for Steam VirtualGL hardware acceleration)"
