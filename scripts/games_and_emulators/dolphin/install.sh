0477b7efe5c14648f2759cdc04cb81ac2ddd0997
cobalt2727 <60624944+cobalt2727@users.noreply.github.com>
2023-01-27 16:17:56 -0500

    Dolphin: duplicate lines for GC adapter support
    I've been having a lot of people run this file directly with the helper script to install Dolphin

--- scripts/games_and_emulators/dolphin/install.sh
+++ scripts/games_and_emulators/dolphin/install.sh
@@ -7,6 +7,13 @@ cd ~
 echo "Installing dependencies..."
 sleep 1
 
+sudo rm -f /etc/udev/rules.d/51-gcadapter.rules
+sudo touch /etc/udev/rules.d/51-gcadapter.rules
+echo 'SUBSYSTEM=="usb", ENV{DEVTYPE}=="usb_device", ATTRS{idVendor}=="057e", ATTRS{idProduct}=="0337", MODE="0666"' | sudo tee /etc/udev/rules.d/51-gcadapter.rules >/dev/null
+sudo udevadm control --reload-rules
+sudo systemctl restart udev.service
+cd ~
+
 case "$__os_id" in
 Raspbian | Debian | LinuxMint | Linuxmint | Ubuntu | [Nn]eon | Pop | Zorin | [eE]lementary | [jJ]ing[Oo][sS])
   case "$__os_codename" in
