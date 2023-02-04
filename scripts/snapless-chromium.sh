7eb90b01f989e5465c5dd1d43c751bd427571679
cobalt2727 <60624944+cobalt2727@users.noreply.github.com>
2023-02-03 20:06:58 -0500

    Snapless-*: properly handle non-L4T installs

--- scripts/snapless-chromium.sh
+++ scripts/snapless-chromium.sh
@@ -1,22 +1,32 @@
 #!/bin/bash
 
 case "$__os_codename" in
-bionic) error_user "You shouldn't be running this on Ubuntu Bionic, skipping installation" ;;
+bionic)
+  error_user "This script is irrelevant on 18.04 - skipping installation..."
+  ;;
+*)
+  if [ $__formal_distro_name == Pop\!_OS ] | [ $__formal_distro_name == "Linux Mint" ]; then
+    status_green "$__formal_distro_name already has Chromium available as a .deb - there is nothing for this script to do..."
+    sleep 5
+  else
+    sudo su -c "echo 'Package: *' > /etc/apt/preferences.d/chromium"
+    sudo su -c "echo 'Pin: release o=LP-PPA-saiarcot895-chromium-beta' >> /etc/apt/preferences.d/chromium"
+    sudo su -c "echo 'Pin-Priority: 700' >> /etc/apt/preferences.d/chromium"
+
+    if command -v snap; then
+      sudo snap remove chromium
+    fi
+
+    ppa_name="saiarcot895/chromium-beta" && ppa_installer
+
+    sudo apt install --reinstall chromium-browser -y
+    sudo apt --fix-broken install -y
+
+    if [[ $jetson_model ]]; then
+      # browser crashes without --no-sandbox on newer ubuntu on jetson
+      # add a customization file with necessary flags
+      echo 'CHROMIUM_FLAGS="--no-sandbox --use-vulkan --enable-features=Vulkan"' | sudo tee /etc/chromium-browser/customizations/l4t-customizations
+    fi
+  fi
+  ;;
 esac
-
-sudo su -c "echo 'Package: *' > /etc/apt/preferences.d/chromium"
-sudo su -c "echo 'Pin: release o=LP-PPA-saiarcot895-chromium-beta' >> /etc/apt/preferences.d/chromium"
-sudo su -c "echo 'Pin-Priority: 700' >> /etc/apt/preferences.d/chromium"
-
-if test -f /usr/local/bin/snap; then
-  sudo snap remove chromium
-fi
-
-ppa_name="saiarcot895/chromium-beta" && ppa_installer
-
-sudo apt install --reinstall chromium-browser -y
-sudo apt --fix-broken install -y
-
-# browser crashes without --no-sanbox on newer ubuntu on jetson
-# add a customization file with necessary flags
-echo 'CHROMIUM_FLAGS="--no-sandbox --use-vulkan --enable-features=Vulkan"' | sudo tee /etc/chromium-browser/customizations/l4t-customizations
--- scripts/snapless-firefox.sh
+++ scripts/snapless-firefox.sh
@@ -1,16 +1,26 @@
 #!/bin/bash
 
-if ! grep -q jammy /etc/os-release; then error_user "You shouldn't be running this unless you are on Ubuntu Jammy, skipping installation"; fi
+case "$__os_codename" in
+bionic | focal)
+  error_user "This script is irrelevant on 20.04 and below - skipping installation..."
+  ;;
+*)
+  if [ $__formal_distro_name == Pop\!_OS ] | [ $__formal_distro_name == "Linux Mint" ]; then
+    status_green "$__formal_distro_name already has Firefox available as a .deb - there is nothing for this script to do..."
+    sleep 5
+  else
+    sudo su -c "echo 'Package: *' > /etc/apt/preferences.d/firefox"
+    sudo su -c "echo 'Pin: release o=LP-PPA-mozillateam' >> /etc/apt/preferences.d/firefox"
+    sudo su -c "echo 'Pin-Priority: 700' >> /etc/apt/preferences.d/firefox"
 
-sudo su -c "echo 'Package: *' > /etc/apt/preferences.d/firefox"
-sudo su -c "echo 'Pin: release o=LP-PPA-mozillateam' >> /etc/apt/preferences.d/firefox"
-sudo su -c "echo 'Pin-Priority: 700' >> /etc/apt/preferences.d/firefox"
+    if command -v snap; then
+      sudo snap remove firefox
+    fi
 
-if test -f /usr/local/bin/snap; then
-  sudo snap remove firefox
-fi
+    ppa_name="mozillateam/ppa" && ppa_installer
 
-ppa_name="mozillateam/ppa" && ppa_installer
-
-sudo apt install firefox -y
-sudo apt --fix-broken install -y
+    sudo apt install firefox -y
+    sudo apt --fix-broken install -y
+  fi
+  ;;
+esac
