01b3a32f87dbb6d5ce32120a56c4f22babd96dec
cobalt2727 <60624944+cobalt2727@users.noreply.github.com>
2023-02-09 16:51:18 -0500

    Vivaldi: fix Nobara support

--- scripts/vivaldi.sh
+++ scripts/vivaldi.sh
@@ -5,7 +5,7 @@ if grep -q debian /etc/os-release; then
   echo "deb [signed-by=/usr/share/keyrings/vivaldi-browser.gpg arch=$(dpkg --print-architecture)] https://repo.vivaldi.com/archive/deb/ stable main" | sudo dd of=/etc/apt/sources.list.d/vivaldi-archive.list
   sudo apt update && sudo apt install vivaldi-stable -y
 
-elif grep -q fedora /etc/os-release; then
+elif [ $__os_id == "Fedora" ]; then
   sudo dnf config-manager --add-repo https://repo.vivaldi.com/archive/vivaldi-fedora.repo
   sudo dnf install vivaldi-stable -y
 fi
