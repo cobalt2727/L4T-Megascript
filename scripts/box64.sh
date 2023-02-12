230c6ac32a81d49bf7cf1460db673af04378ff32
theofficialgman <28281419+theofficialgman@users.noreply.github.com>
2023-02-12 11:21:16 -0500

    use box64 prebuilt debian repo for arm64

--- scripts/box64.sh
+++ scripts/box64.sh
@@ -6,61 +6,64 @@ echo "Box64 script started!"
 
 case "$dpkg_architecture" in
 "arm64")
+  sudo wget https://ryanfortner.github.io/box64-debs/box64.list -O /etc/apt/sources.list.d/box64.list
+  if [ $? != 0 ];then
+    sudo rm -f /etc/apt/sources.list.d/box64.list
+    error "Failed to add box64.list file!"
+  fi
+
+  sudo rm -f /usr/share/keyrings/box64-debs-archive-keyring.gpg
+  curl -fsSL https://ryanfortner.github.io/box64-debs/KEY.gpg | sudo gpg --dearmor -o /usr/share/keyrings/box64-debs-archive-keyring.gpg
+
+  if [ $? != 0 ];then
+    sudo rm -f /etc/apt/sources.list.d/box64.list
+    error "Failed to add KEY.gpg to APT keyring!"
+  fi
+
+  if [[ "$SOC_ID" == "tegra-x1" ]] || [[ "$SOC_ID" == "tegra-x2" ]]; then
+    sudo apt install -y box64-tegrax1 || exit 1
+  elif [[ "$SOC_ID" == "rk3399" ]]; then
+    sudo apt install -y box64-rk3399 || exit 1
+  elif [[ "$SOC_ID" == "bcm2711" ]]; then
+    sudo apt install -y box64 || exit 1
+  else
+    warning "There is no box64 pre-build for your device $SOC_ID $model"
+    warning "Installing the RPI4 tuned box64 build as a fallback (no crypto extensions enabled)"
+    sudo apt install -y box64 || exit 1
+  fi
+  ;;
+"amd64")
+  # add toolchain ppa for gcc 11 on bionic and focal
+  # newer releases of ubuntu have gcc-11 in the normal repos
+  # older releases of ubuntu are not supported
   case "$__os_codename" in
   bionic) ppa_name="theofficialgman/cmake-bionic" && ppa_installer ;;
   esac
-  ;;
-"amd64")
-  echo "Installing Dependencies"
+  case "$__os_codename" in
+  bionic | focal) ppa_name="ubuntu-toolchain-r/test" && ppa_installer ;;
+  esac
+
+  sudo apt install zenity cmake git build-essential gcc-11 g++-11 -y || error "Could not install dependencies"
+  cd /tmp
+  rm -rf box64
+  git clone --depth=1 https://github.com/ptitSeb/box64
+  cd box64
+  mkdir build
+  cd build
+  cmake .. -DLD80BITS=1 -DNOALIGN=1 -DCMAKE_BUILD_TYPE=RelWithDebInfo -DCMAKE_C_COMPILER=gcc-11
+  echo "Building Box64"
+
+  make -j$(nproc) || error "Compilation failed"
+  sudo make install || error "Make install failed"
+  sudo systemctl restart systemd-binfmt
   ;;
 *)
   error_user "Error: your cpu architecture ($dpkg_architecture) is not supported by box64 and will fail to compile"
   ;;
 esac
 
-# add toolchain ppa for gcc 11 on bionic and focal
-# newer releases of ubuntu have gcc-11 in the normal repos
-# older releases of ubuntu are not supported
-case "$__os_codename" in
-bionic | focal) ppa_name="ubuntu-toolchain-r/test" && ppa_installer ;;
-esac
-
-sudo apt install zenity cmake git build-essential gcc-11 g++-11 -y || error "Could not install dependencies"
-cd ~
-rm -rf box64
-git clone --depth=1 https://github.com/ptitSeb/box64
-cd box64
-mkdir build
-cd build
-
-case "$dpkg_architecture" in
-"arm64")
-  case "$jetson_model" in
-  "tegra-x1")
-    cmake .. -DTEGRAX1=1 -DCMAKE_BUILD_TYPE=RelWithDebInfo -DCMAKE_C_COMPILER=gcc-11
-    echo "Tegra X1 based system"
-    ;;
-  *)
-    cmake .. -DARM_DYNAREC=ON -DCMAKE_BUILD_TYPE=RelWithDebInfo -DCMAKE_C_COMPILER=gcc-11
-    echo "Universal aarch64 system"
-    ;;
-  esac
-  ;;
-"amd64")
-  cmake .. -DLD80BITS=1 -DNOALIGN=1 -DCMAKE_BUILD_TYPE=RelWithDebInfo -DCMAKE_C_COMPILER=gcc-11
-  echo "x86_64 based system"
-  ;;
-*) error "Something is very wrong... how did you get past the first check?" ;;
-esac
-
-echo "Building Box64"
-
-make -j$(nproc) || error "Compilation failed"
-sudo make install || error "Make install failed"
-sudo systemctl restart systemd-binfmt
 sudo mkdir /usr/share/box64
-cd ..
-sudo cp docs/img/Box64Icon.png /usr/share/box64/icon.png
+sudo wget https://github.com/ptitSeb/box64/raw/main/docs/img/Box64Icon.png -O /usr/share/box64/icon.png
 
 echo "Adding box64 to applications list"
 sudo tee /usr/share/applications/box64.desktop <<'EOF'
@@ -84,7 +87,7 @@ Terminal=hidden
 Categories=System
 EOF
 
-rm -rf ~/box64
+rm -rf /tmp/box64
 
 echo "Box64 successfully installed"
 echo ""
