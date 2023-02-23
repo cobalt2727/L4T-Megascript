42c9ae17cf40ed6a6a24a8aa2fe37a7f011c9430
cobalt2727 <60624944+cobalt2727@users.noreply.github.com>
2023-02-23 12:45:07 -0500

    Update xlink-kai.sh

--- scripts/xlink-kai.sh
+++ scripts/xlink-kai.sh
@@ -2,8 +2,11 @@
 
 echo "XLink Kai script started!"
 
-sudo curl https://repo.teamxlink.co.uk/debian/KEY.asc --create-dirs -o /usr/share/keyrings/teamxlink.asc
-echo 'deb [signed-by=/usr/share/keyrings/teamxlink.asc] https://repo.teamxlink.co.uk/debian/ /' | sudo tee /etc/apt/sources.list.d/teamxlink.list
+sudo apt install ca-certificates curl gnupg -y || errpr "Couldn't install dependencies!"
+sudo mkdir -m 0755 -p /etc/apt/keyrings
+curl -fsSL https://dist.teamxlink.co.uk/linux/debian/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/teamxlink.gpg
+sudo chmod a+r /etc/apt/keyrings/teamxlink.gpg
+echo  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/teamxlink.gpg] https://dist.teamxlink.co.uk/linux/debian/static/deb/release/ /" | sudo tee /etc/apt/sources.list.d/teamxlink.list > /dev/null
 sudo apt update && sudo apt install xlinkkai -y || error "Apt Update/Install Failed"
 
 echo "Done! Sending you back to the main menu..."
