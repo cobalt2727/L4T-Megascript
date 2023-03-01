34de72e9a4f79336995812e495f2630fad45a962
theofficialgman <28281419+theofficialgman@users.noreply.github.com>
2023-03-01 15:52:09 -0500

    only install libreoffice-gtk2 if available (removed on debian 11+)
    also only match case to Ubuntu as all ubuntu derivatives are matched (__os_id is the upstream id)

--- scripts/libreoffice.sh
+++ scripts/libreoffice.sh
@@ -1,7 +1,7 @@
 echo "LibreOffice script started!"
 
 case "$__os_id" in
-Raspbian | LinuxMint | Linuxmint | Ubuntu | [Nn]eon | Pop | Zorin | [eE]lementary | [jJ]ing[Oo][sS])
+Ubuntu)
   ppa_name="libreoffice/ppa" && ppa_installer
   sudo apt install -y --no-install-recommends libreoffice libxrender1 libreoffice-gtk2 libreoffice-gtk3 || error "Failed to install dependencies"
 
@@ -12,11 +12,16 @@ Raspbian | LinuxMint | Linuxmint | Ubuntu | [Nn]eon | Pop | Zorin | [eE]lementar
   fi
   ;;
 Fedora)
-  sudo dnf install -y libreoffice
+  sudo dnf install -y libreoffice || error "Failed to install dependencies"
   ;;
-Debian)
-  sudo apt install -y --no-install-recommends libreoffice libxrender1 libreoffice-gtk2 libreoffice-gtk3 || error "Failed to install dependencies"
-;;
+Raspbian | Debian)
+  package_available libreoffice-gtk2
+  if [[ $? == "0" ]]; then
+    sudo apt install -y --no-install-recommends libreoffice libxrender1 libreoffice-gtk3 || error "Failed to install dependencies"
+  else
+    sudo apt install -y --no-install-recommends libreoffice libxrender1 libreoffice-gtk2 libreoffice-gtk3 || error "Failed to install dependencies"
+  fi
+  ;;
 *)
   error "Unknown distro detected"
   ;;
