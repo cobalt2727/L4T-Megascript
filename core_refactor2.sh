832b3d0a5788575a108bfae42cef27c1daf795ab
cobalt2727 <60624944+cobalt2727@users.noreply.github.com>
2023-03-26 14:04:28 -0400

    Core: add libxkbcommon-devel for Fedora

--- core_refactor2.sh
+++ core_refactor2.sh
@@ -148,7 +148,7 @@ if grep -q debian /etc/os-release; then
     sudo sh -c "apt update; apt-get dist-upgrade -y; apt-get install $(echo "${dependencies[@]}") -y; hash -r; $FUNC; repository_branch=$repository_branch; repository_username=$repository_username; add_desktop; apt update; apt upgrade -y; hash -r"
   fi
 elif grep -q fedora /etc/os-release || grep -q nobara /etc/os-release; then
-  dependencies=("bash" "dialog" "gnutls" "curl" "yad" "zenity" "redhat-lsb")
+  dependencies=("bash" "dialog" "gnutls" "curl" "yad" "zenity" "redhat-lsb" "libxkbcommon-devel")
   if [[ $gui == "gui" ]]; then
     pkexec sh -c "dnf upgrade -y; dnf install $(echo "${dependencies[@]}") -y; hash -r; $FUNC; repository_branch=$repository_branch; repository_username=$repository_username; add_desktop; dnf upgrade -y; hash -r"
   else
