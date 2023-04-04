3776990f47d122b8f3477c5c7333ae5296fc7dbb
cobalt2727 <60624944+cobalt2727@users.noreply.github.com>
2023-04-04 09:04:25 -0400

    Dolphin: add rcheevos submodule

--- scripts/games_and_emulators/dolphin/install.sh
+++ scripts/games_and_emulators/dolphin/install.sh
@@ -93,6 +93,7 @@ git submodule update --init Externals/VulkanMemoryAllocator || error_user "Faile
 git submodule update --init --recursive Externals/cubeb/ || error_user "Failed to download submodules from GitHub!"
 git submodule update --init Externals/implot || error_user "Failed to download submodules from GitHub!"
 git submodule update --init Externals/gtest || error_user "Failed to download submodules from GitHub!"
+git submodule update --init Externals/rcheevos || error_user "Failed to download submodules from GitHub!"
 
 mkdir -p build
 cd build
