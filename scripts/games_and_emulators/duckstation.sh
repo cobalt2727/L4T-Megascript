0a960a89cf5c1085744b68845e65a8314939d5b9
ZugCodez <118562575+ZugCodez@users.noreply.github.com>
2023-05-23 16:36:03 -0600

    Update duckstation.sh

--- scripts/games_and_emulators/duckstation.sh
+++ scripts/games_and_emulators/duckstation.sh
@@ -38,7 +38,7 @@ esac
 # make -j$(nproc)
 cmake --build . --parallel || error "Compilation failed"
 
-cd ~
+cd ..
 #install duckstation itself
 sudo cp -r duckstation/build/bin/ /usr/local/
 
@@ -49,16 +49,16 @@ wget https://archive.org/download/icons_202305/icons.zip
 unzip icons.zip
 
 #install icons for .desktop files
-sudo install -m 644 -D duckstationicons/SVG.svg /usr/local/share/icons/hicolor/scalable/apps/duckstation.svg
+sudo install -m 644 -D duckstationicons/svg.svg /usr/local/share/icons/hicolor/scalable/apps/duckstation-qt.svg
 
-sudo install -m 644 -D duckstationicons/128.svg /usr/local/share/icons/hicolor/128x128/apps/duckstation.png #128
-sudo install -m 644 -D duckstationicons/16.png /usr/local/share/icons/hicolor/16x16/apps/duckstation.png    #16
-sudo install -m 644 -D duckstationicons/24.png /usr/local/share/icons/hicolor/24x24/apps/duckstation.png    #24
-sudo install -m 644 -D duckstationicons/256.png /usr/local/share/icons/hicolor/256x256/apps/duckstation.png #256
-sudo install -m 644 -D duckstationicons/32.png /usr/local/share/icons/hicolor/32x32/apps/duckstation.png    #32
-sudo install -m 644 -D duckstationicons/48.png /usr/local/share/icons/hicolor/48x48/apps/duckstation.png    #48
-sudo install -m 644 -D duckstationicons/512.png /usr/local/share/icons/hicolor/512x512/apps/duckstation.png #512
-sudo install -m 644 -D duckstationicons/64.png /usr/local/share/icons/hicolor/64x64/apps/duckstation.png    #64
+sudo install -m 644 -D duckstationicons/128.svg /usr/local/share/icons/hicolor/128x128/apps/duckstation-qt.png #128
+sudo install -m 644 -D duckstationicons/16.png /usr/local/share/icons/hicolor/16x16/apps/duckstation-qt.png    #16
+sudo install -m 644 -D duckstationicons/24.png /usr/local/share/icons/hicolor/24x24/apps/duckstation-qt.png    #24
+sudo install -m 644 -D duckstationicons/256.png /usr/local/share/icons/hicolor/256x256/apps/duckstation-qt.png #256
+sudo install -m 644 -D duckstationicons/32.png /usr/local/share/icons/hicolor/32x32/apps/duckstation-qt.png    #32
+sudo install -m 644 -D duckstationicons/48.png /usr/local/share/icons/hicolor/48x48/apps/duckstation-qt.png    #48
+sudo install -m 644 -D duckstationicons/512.png /usr/local/share/icons/hicolor/512x512/apps/duckstation-qt.png #512
+sudo install -m 644 -D duckstationicons/64.png /usr/local/share/icons/hicolor/64x64/apps/duckstation-qt.png    #64
 
 #install .desktop file itself
 sudo install -m 644 -D duckstationicons/DuckStation.desktop /usr/local/share/applications/DuckStation.desktop
