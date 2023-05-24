f973619a33e5f3ece427c0a80fd45318e46c7bab
ZugCodez <118562575+ZugCodez@users.noreply.github.com>
2023-05-23 19:54:24 -0600

    Make supermodel Installable

--- scripts/games_and_emulators/supermodel_installer.sh
+++ scripts/games_and_emulators/supermodel_installer.sh
@@ -7,6 +7,7 @@ echo "Supermodel 3 (Sega Model 3 emulator) script started!"
 ###https://mechafatnick.co.uk/2021/06/13/becoming-a-super-model-adding-model-3-games-to-your-pi4/
 ###https://gbatemp.net/threads/supermodel-nx-segas-ardade-model-3-emulator-on-nintendo-switch-l4t-ubuntu.582130/
 ###get rid of that last echo after the above is completed
+cd ~
 
 echo "Installing dependencies..."
 sudo apt install -y gcc g++ build-essential libsdl2-2.0-0 libsdl2-dev libsdl2-net-dev subversion
@@ -22,4 +23,7 @@ make -f Makefiles/Makefile.UNIX clean
 #actually build
 make -f Makefiles/Makefile.UNIX
 
+sudo install -D bin/supermodel /usr/local/bin/supermodel
+cd /usr/local/share/applications
+sudo wget https://download1649.mediafire.com/wkme04q3h4fgt3LmI2vRk6zLr-2-eYccJwMWzbOYnywqZAPOVWELoERIHqsl8SY-Dt59O_gjuhXFIvcTE6NtIUdGQMie-xZZ0hpnbHB3FLnS5R0BwZUCLHiT0nHoBOqLwqIMTefu50X-FVe-bkTlaY9HVXxhT1PMsmxbeN6PLb5bFoqU/2if46mq5msv3019/supermodel.desktop
 echo "The emulator can be found at ~/Supermodel3/bin/supermodel - enjoy!"
