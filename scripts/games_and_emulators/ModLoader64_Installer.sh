2ddc8a971785677021dfcd273f53227b8ecf7407
ZugCodez <118562575+ZugCodez@users.noreply.github.com>
2023-05-24 11:32:20 -0600

    Update ModLoader64_Installer.sh

--- scripts/games_and_emulators/ModLoader64_Installer.sh
+++ scripts/games_and_emulators/ModLoader64_Installer.sh
@@ -9,4 +9,14 @@ sudo wget https://repo.modloader64.com/launcher/update/app.zip
 sudo unzip app.zip
 sudo rm app.zip
 cd /usr/local/share/applications
-sudo wget https://download1337.mediafire.com/xc23tvmbfkjg9p8zvwUbph52N85Slb1LcQXRSLePdq-qFNOOU18AQwPceGNnvAu4bTSj58XfRUK5T-MbbBiyMZ6UFle1c-P3AM6EDKpvTDi0tleureaa3gnIZrD2GGxKmbmOLj4ViX_vJcPwyBESiSdFKjQYVyilMevh7WcT3FTiiMj6/cc7c7thkf8dvssi/ModLoader64.desktopp
+sudo cat <<EOF > supermodel.desktop
+[Desktop Entry]
+Version=1.0
+Type=Application
+Terminal=true
+Exec=supermodel
+Name=Supermodel
+Comment=A Sega Model 3 Arcade Emulator
+Categories=Game;Emulator;
+EOF
+echo "The emulator can be found at ~/Supermodel3/bin/supermodel - enjoy!"
