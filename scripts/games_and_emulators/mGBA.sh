a70f2879349d228eb9263c0395ce9f3c034d12b1
ZugCodez <118562575+ZugCodez@users.noreply.github.com>
2023-08-19 13:46:19 -0600

    Update mGBA.sh

--- scripts/games_and_emulators/mGBA.sh
+++ scripts/games_and_emulators/mGBA.sh
@@ -1,11 +1,5 @@
 #!/bin/bash
 
-function error {
-  echo -e "\\e[91m$1\\e[39m"
-  sleep 3
-  exit 1
-}
-
 clear -x
 echo "mGBA script successfully started!"
 echo "Credits: https://github.com/mgba-emu/mgba.git"
@@ -14,24 +8,16 @@ sleep 3
 echo "Running updates..."
 sleep 1
 
-case "$__os_codename" in
-bionic)
-  echo "          -------UBUNTU 18.04 DETECTED-------"
-
-  sudo apt install -y cmake gcc-11 g++-11 || error "Could not install dependencies"
-  ;;
-*)
-  package_available qt5-default
-  if [[ $? == "0" ]]; then
-    sudo apt install -y qt5-default qtmultimedia5-dev || error "Failed to install dependencies"
-  else
-    sudo apt install -y qtbase5-dev qtbase5-private-dev qtchooser qtmultimedia5-dev || error "Failed to install dependencies"
-  fi
-esac
+package_available qt5-default
+if [[ $? == "0" ]]; then
+  sudo apt install -y qt5-default qtmultimedia5-dev || error "Failed to install dependencies"
+else
+  sudo apt install -y qtbase5-dev qtbase5-private-dev qtchooser qtmultimedia5-dev || error "Failed to install dependencies"
+fi
 
 echo "Installing dependencies..."
 sleep 1
-sudo apt install zipmerge ziptool git cmake extra-cmake-modules libsdl2-dev libsdl2-2.0-0 libpng-dev zlib1g-dev libedit-dev ffmpeg libavcodec-dev libavcodec-extra libzip-dev sqlite3 libelf-dev lua5.3 libjson-c-dev -y || error "Could not install dependencies"
+sudo apt install zipmerge ziptool git cmake extra-cmake-modules libsdl2-dev libsdl2-2.0-0 libpng-dev zlib1g-dev libedit-dev ffmpeg libavcodec-dev libavcodec-extra libzip-dev sqlite3 libelf-dev lua5.3 libjson-c-dev cmake gcc-11 g++-11 -y || error "Could not install dependencies"
 
 echo "Building mGBA..."
 sleep 1
