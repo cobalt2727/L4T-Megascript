19a011b8b86d05f459f8d9f4fc3e99f7795f78f7
theofficialgman <28281419+theofficialgman@users.noreply.github.com>
2023-08-26 17:20:49 -0400

    steam: skip meson from pip if not needed

--- scripts/games_and_emulators/steam.sh
+++ scripts/games_and_emulators/steam.sh
@@ -44,7 +44,11 @@ sudo sed -i 's:^DISABLE_MESA_EGL="1":DISABLE_MESA_EGL="0":' /etc/systemd/nv.sh
 sudo apt-get dist-upgrade -y || error "Could not upgrade MESA (needed for Steam VirtualGL hardware acceleration)"
 sudo apt install ninja-build python3 python3-pip libdrm-dev libgbm-dev pkg-config -y || error "Could not install VIRGL build dependencies"
 hash -r
-sudo -H python3 -m pip install --upgrade meson || error "Could not install meson VIRGL build dependency"
+if package_is_new_enough meson 0.55.0 ;then
+  sudo apt install meson -y || error "Could not install meson VIRGL build dependency"
+else
+  sudo -H python3 -m pip install --upgrade meson || error "Could not install meson VIRGL build dependency"
+fi
 hash -r
 
 case "$__os_codename" in
