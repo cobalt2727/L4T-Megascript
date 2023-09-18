201b0fbea22c1adf7340a0b430960eb701424c13
cobalt2727 <60624944+cobalt2727@users.noreply.github.com>
2023-09-18 17:19:42 -0500

    MC Bedrock: Fedora support + overhaul install

--- scripts/games_and_emulators/minecraft-bedrock-appimage.sh
+++ scripts/games_and_emulators/minecraft-bedrock-appimage.sh
@@ -3,87 +3,67 @@
 clear -x
 
 echo "Minecraft Bedrock script started!"
-echo "Installing dependencies..."
+echo "CREDITS: https://github.com/minecraft-linux/pkg"
 sleep 1
 
 cd ~
-sudo apt install python3-pip curl zenity kmod -y || error "Could not install dependencies"
-rm -rf minecraft-bedrock
-mkdir minecraft-bedrock
-cd minecraft-bedrock
-case "$dpkg_architecture" in
-"arm64")
-  type="arm64"
-  type2="arm64"
+
+case "$__os_id" in
+Raspbian | Debian | LinuxMint | Linuxmint | Ubuntu | [Nn]eon | Pop | Zorin | [eE]lementary | [jJ]ing[Oo][sS])
+  #the rewrite to use the repo cut off a few rare edge cases such as:
+  # 32-bit architectures
+  # Debian versions lower than 10
+  # 18.04 users on a non-arm64 architecture
+  #the monstrosity below this line catches these edge cases, all of which are still officially supported by the AppImage releases
+  # if [[ [[ [ "$__os_id" == "Debian" ] || if [ "$__os_id" == "Raspbian" ] ]] && [[ "$__os_release" < 11 ]] ]] || if [[ [ "$__os_id" == "Ubuntu" ] && [[ ${"__os_release"%.*} < 18 ]] ]] || if [[ [ "$__os_codename" == "bionic" ] && [ "$dpkg_architecture" != "arm64" ] ]]|| if [[ [ "$dpkg_architecture" != "armhf" ] || if [ "$dpkg_architecture" == "i386" ] ]]; then
+  #   echo "Error: Your OS $__os_id $__os_release is not supported by this script. You'll have to download AppImageLauncher and the AppImage for the game and run it yourself."
+  #   echo "The script will open up the download page for you in about 20 seconds."
+  #   echo "Your architecture is $architecture. Good luck."
+  #   echo ""
+  #   echo "Exiting the script"
+  #   sleep 3
+
+  #   notify-send "Remember, you're looking for the $architecture download."
+  #   # xdg-open https://github.com/TheAssassin/AppImageLauncher/releases/latest
+  #   # xdg-open https://github.com/minecraft-linux/appimage-builder/releases/latest
+  #   # exit 2
+  # fi
+
+  curl -sS https://minecraft-linux.github.io/pkg/deb/pubkey.gpg | sudo tee -a /etc/apt/trusted.gpg.d/minecraft-linux-pkg.asc
+
+  echo "deb [arch=$(dpkg --print-architecture)] https://minecraft-linux.github.io/pkg/deb $__os_codename main" | sudo tee /etc/apt/sources.list.d/minecraft-linux-pkg.list
+
+  ###########the QT6 launcher is currently commented out - it currently just coredumps when trying to log in on 22.04
+  # package_available mcpelauncher-ui-manifest-qt6
+  # if [[ $? == "0" ]]; then
+  #   sudo apt install -y mcpelauncher-manifest-qt6 mcpelauncher-ui-manifest-qt6 || error "Failed to install dependencies"
+  #   #just for good measure
+  #   sudo apt remove -y mcpelauncher-manifest mcpelauncher-ui-manifest
+  # else
+  sudo apt install -y mcpelauncher-manifest mcpelauncher-ui-manifest || error "Failed to install dependencies"
+  # fi
+
   ;;
-"armhf")
-  type="armhf"
-  type2="armhf"
-  ;;
-"i386")
-  type="x86"
-  type2="i386"
-  ;;
-"amd64")
-  type="x86_64"
-  type2="amd64"
+
+Fedora)
+  echo -e "[minecraft-linux-pkg]\nname=minecraft-linux-pkg\nbaseurl=https://minecraft-linux.github.io/pkg/fedora-$__os_release\nenabled=1\ncountme=1\nrepo_gpgcheck=0\ntype=rpm\ngpgcheck=1\nskip_if_unavailable=False\ngpgkey=https://minecraft-linux.github.io/pkg/deb/pubkey.gpg" | sudo tee /etc/yum.repos.d/minecraft-linux-pkg.repo
   ;;
 *)
-  echo "Error: your userspace architecture ($dpkg_architecture) is not supporeted by Minecraft Bedrock Launcher and will fail to run"
+  lsb_release
+  echo "Error: Your OS is not supported by this script. You'll have to download AppImageLauncher and the AppImage for the game and run it yourself."
+  echo "The script will open up the download page for you in about 20 seconds."
+  echo "Your architecture is $architecture. Good luck."
   echo ""
   echo "Exiting the script"
   sleep 3
-  exit 2
+
+  notify-send "Remember, you're looking for the $architecture download."
+  xdg-open https://github.com/TheAssassin/AppImageLauncher/latest
+  xdg-open https://github.com/minecraft-linux/appimage-builder/releases
+  error "Error: Are you on a supported OS?"
   ;;
 esac
 
-case "$__os_codename" in
-bionic)
-  ppa_name="deadsnakes/ppa" && ppa_installer
-  sudo apt install -y python3.8
-  yes | python3.6 -m pip uninstall lastversion
-  python3.8 -m pip install --upgrade --force-reinstall pip filelock lastversion || error "Couldn't install dependencies"
-  ;;
-*)
-  python3 -m pip install --upgrade pip lastversion || error "Couldn't install dependencies"
-  ;;
-esac
-
-#the -v after arch detection fixes x86 devices locating both x86 and x86_64
-lastversion --assets --filter $type-v download https://github.com/minecraft-linux/appimage-builder || error "Couldn't download AppImage!"
-mv *.AppImage MC.AppImage
-chmod +x *.AppImage
-curl https://api.github.com/repos/TheAssassin/AppImageLauncher/releases/latest | grep "browser_download_url.*bionic_$type2" | cut -d : -f 2,3 | tr -d \" | wget -i -
-sudo dpkg -i *bionic_*.deb
-hash -r
-ail-cli integrate MC.AppImage || error "Couldn't integrate AppImage"
-cd ~
-rm -rf minecraft-bedrock
-
-echo "Install Dependencies..."
-cd ~
-
-#NOTE:  a long time ago we used to use ZorinOS PPAs and Debian repos to get newer version of libraries needed to make the launcher work on 18.04.
-#       this was a bad idea, and we've since gotten things working without external software sources. so the below section wipes out those if they're found on an 18.04 setup
-case "$__os_codename" in
-bionic)
-  if $(dpkg --compare-versions $(dpkg-query -f='${Version}' --show libc6) lt 2.28); then
-    echo "Continuing the install"
-  else
-    echo "Force Downgrading libc and related packages"
-    echo "You may need to recompile other programs such as Dolphin and BOX64 if you see this message"
-    sudo rm -rf /etc/apt/sources.list.d/zorinos-ubuntu-stable-bionic.list*
-    sudo rm -rf /etc/apt/preferences.d/zorinos*
-    sudo rm -rf /etc/apt/sources.list.d/debian-stable.list*
-    sudo rm -rf /etc/apt/preferences.d/freetype*
-
-    sudo apt update
-    sudo apt install libc-bin=2.27* libc-dev-bin=2.27* libc6=2.27* libc6-dbg=2.27* libc6-dev=2.27* libfreetype6=2.8* libfreetype6-dev=2.8* locales=2.27* -y --allow-downgrades || error "Could not install dependencies"
-  fi
-  ;;
-esac
-
-echo "Please Reboot before launching!"
 sleep 5
 echo "Going back to the menu..."
 sleep 2
