b9e5597fa7b3bc4871ccfb2abf4733d95804b9bc
cobalt2727 <60624944+cobalt2727@users.noreply.github.com>
2023-09-24 01:37:40 -0500

    Dolphin: add fmt submodule

--- scripts/games_and_emulators/dolphin/install.sh
+++ scripts/games_and_emulators/dolphin/install.sh
@@ -94,6 +94,7 @@ git submodule update --init --recursive Externals/cubeb/ || error_user "Failed t
 git submodule update --init Externals/implot || error_user "Failed to download submodules from GitHub!"
 git submodule update --init Externals/gtest || error_user "Failed to download submodules from GitHub!"
 git submodule update --init Externals/rcheevos || error_user "Failed to download submodules from GitHub!"
+git submodule update --init Externals/fmt || error_user "Failed to download submodules from GitHub!"
 
 mkdir -p build
 cd build
