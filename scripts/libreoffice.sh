81b82f74c2b8caa606d9518f5060e3daa110db4c
theofficialgman <28281419+theofficialgman@users.noreply.github.com>
2023-09-24 21:35:51 -0400

    Libreoffice: correct and update script
    -gtk2 is no longer built at the ppa. only gtk3 based libreoffice is supported

--- scripts/libreoffice.sh
+++ scripts/libreoffice.sh
@@ -3,12 +3,12 @@ echo "LibreOffice script started!"
 case "$__os_id" in
 Ubuntu)
   ppa_name="libreoffice/ppa" && ppa_installer
-  sudo apt install -y --no-install-recommends libreoffice libxrender1 libreoffice-gtk2 libreoffice-gtk3 || error "Failed to install dependencies"
+  sudo apt install -y --no-install-recommends libreoffice libxrender1 libreoffice-gtk3 || error "Failed to install dependencies"
 
   if echo $XDG_CURRENT_DESKTOP | grep -q 'GNOME'; then
     sudo apt install -y --no-install-recommends libreoffice-gnome || error "Failed to install dependencies"
-  elif echo $XDG_CURRENT_DESKTOP | grep -q 'Plasma'; then
-    sudo apt install -y --no-install-recommends libreoffice-kde5 || error "Failed to install dependencies"
+  elif echo $XDG_CURRENT_DESKTOP | grep -q 'KDE'; then
+    sudo apt install -y --no-install-recommends libreoffice-kf5 || error "Failed to install dependencies"
   fi
   ;;
 Fedora)
