942072a58bb8ad7503678a789a9fc6ba00b486f4
cobalt2727 <60624944+cobalt2727@users.noreply.github.com>
2024-01-20 22:13:25 -0600

    SRB2: the big rewrite

--- assets/SRB2-A/SRB2.sh
+++ assets/SRB2-A/SRB2.sh
@@ -1,1 +1,3 @@
-read RES_X RES_Y <<<$(xdpyinfo | awk -F'[ x]+' '/dimensions:/{print $3, $4}') && /usr/share/SRB2/build/bin/lsdlsrb2 use_joystick 1 -width $RES_X -height $RES_Y
+#!/bin/bash
+
+read RES_X RES_Y <<<$(xdpyinfo | awk -F'[ x]+' '/dimensions:/{print $3, $4}') && /usr/local/SRB2/lsdlsrb2 use_joystick 1 -width $RES_X -height $RES_Y
--- scripts/games_and_emulators/SRB2.sh
+++ scripts/games_and_emulators/SRB2.sh
@@ -1,20 +1,20 @@
 #!/bin/bash
-#Fun fact, the original version of this script had an aprox 50 more lines of code
 
-cd
-clear -x
 status "SRB2 script started!"
-status "Downloading the files, removing old files and installing needed dependencies..."
-sleep 2
-sudo rm -r /usr/share/SRB2
-cd ~/.srb2
-rm config.cfg
+
+#nuke pre-rewrite files (save data is unaffected)
+sudo rm -rf /usr/share/SRB2/ ~/SRB2-master/ ~/SRB2-DT/ ~/SRB2-A/ ~/SRB2-Data/
+
+#prep folders
+cd /tmp
+sudo rm -rf srb2* SRB2*
 cd /usr/share/applications
 sudo rm "Sonic Robo Blast 2.desktop"
 cd ~/RetroPie/roms/ports
 rm SRB2_retropie.sh
-cd
+cd ~
 
+status "Installing dependencies..."
 case "$__os_id" in
 Raspbian | Debian | LinuxMint | Linuxmint | Ubuntu | [Nn]eon | Pop | Zorin | [eE]lementary | [jJ]ing[Oo][sS])
   sudo apt install gcc g++ wget libsdl2-dev libsdl2-mixer-dev cmake extra-cmake-modules subversion libupnp-dev libgme-dev libopenmpt-dev curl libcurl4-gnutls-dev libpng-dev freepats libgles2-mesa-dev -y || error "Dependency installs failed"
@@ -23,61 +23,58 @@ Fedora)
   sudo dnf install -y wget cmake unzip git SDL2-devel SDL2_mixer-devel libcurl-devel libopenmpt-devel game-music-emu-devel libpng-devel zlib-devel || error "Dependency installs failed"
   ;;
 *)
-  echo -e "\\e[91mUnknown distro detected - this script should work, but please press Ctrl+C now and install necessary dependencies yourself following https://wiki.dolphin-emu.org/index.php?title=Building_Dolphin_on_Linux if you haven't already...\\e[39m"
+  echo -e "\\e[91mUnknown distro detected - this script should work, but please press Ctrl+C now and install necessary dependencies yourself following https://wiki.srb2.org/wiki/Source_code_compiling/CMake if you haven't already...\\e[39m"
   sleep 5
   ;;
 esac
 
-rm -rf master.zip SRB2 SRB2-master SRB2-Data.zip
-wget https://github.com/STJr/SRB2/archive/master.zip --progress=bar:force:noscroll
-wget $(curl --silent "https://api.github.com/repos/STJr/SRB2/releases/latest" | grep "SRB2" | grep "Full" | cut -c 31- | cut -d '"' -f 2) -O SRB2-Data.zip
-mkdir -p ~/SRB2-A
-cd ~/SRB2-A
+# lastversion download stjr/srb2 --assets --filter Full.zip
+wget $(curl --silent "https://api.github.com/repos/STJr/SRB2/releases/latest" | grep "SRB2" | grep "Full" | cut -c 31- | cut -d '"' -f 2) || error "Failed to download assets!"
+unzip -o SRB2-v*-Full.zip "*.dta" "*.pk3" "*.dat" "models/*" -d SRB2-Assets/
+sudo rm SRB2-v*-Full.zip
+
+#lastversion download stjr/srb2 --source
+#mkdir SRB2-Source-Code
+#tar -xzvf srb2-SRB2_release_*.tar.gz -C SRB2-Source-Code --strip-components=1
+#rm srb2*tar.gz
+# STJR's CMakeLists.txt fails if the source folder isn't a git folder - missing a /HEAD file or something. so instead...
+git clone https://github.com/stjr/srb2 --depth=1 -j$(nproc) SRB2-Source-Code || error "Failed to download assets!"
+mkdir -p SRB2-Source-Code/build/ SRB2-Source-Code/assets/installer
+cd SRB2-Source-Code/build/
+sudo rm -rf *
+
+status "Compiling the game..."
+cmake .. -DCMAKE_INSTALL_PREFIX="/usr/local/SRB2/" -DCMAKE_BUILD_TYPE=Release -DCMAKE_CXX_FLAGS=-mcpu=native -DCMAKE_C_FLAGS=-mcpu=native
+make -j$(nproc) || error "Compilation failed!"
+status_green "Game compiled!"
+sudo make install || error "Installation failed!"
+status_green "Game installed!"
+cd ~
+
+status "Setting up desktop files..."
+#create a blank dedicated server script file so people aren't confused where to put it
+mkdir -p /tmp/SRB2-Megascript-Assets/
+cd /tmp/SRB2-Megascript-Assets/
 wget https://github.com/cobalt2727/L4T-Megascript/raw/master/assets/SRB2-A/SRB2.sh
 wget https://github.com/cobalt2727/L4T-Megascript/raw/master/assets/SRB2-A/SRB2Icon.png
 wget https://github.com/cobalt2727/L4T-Megascript/raw/master/assets/SRB2-A/Sonic%20Robo%20Blast%202.desktop
 wget https://github.com/cobalt2727/L4T-Megascript/raw/master/assets/SRB2-A/config.cfg
-cd
-mkdir -p SRB2
-cd
-unzip master.zip
-mkdir -p SRB2-DT
-mv SRB2-Data.zip -t ~/SRB2-DT
-cd ~/SRB2-DT
-unzip SRB2-Data.zip
-rm SRB2-Data.zip*
-cd ~/SRB2-master/assets
-mkdir -p installer
-cd ..
-mkdir -p build
-cd ~
-mkdir -p .srb2
-cd ~/SRB2-A
-chmod 777 SRB2.sh
-mv config.cfg -t ~/.srb2
-mv SRB2.sh SRB2Icon.png -t ~/SRB2
-sudo mv "Sonic Robo Blast 2.desktop" -t /usr/share/applications
-cd ~
-rm master.zip*
-rm -rf SRB2-A
-cd ~/SRB2-DT
-mv music.dta patch.pk3 patch_music.pk3 player.dta srb2.pk3 zones.pk3 -t ~/SRB2-master/assets/installer
-cd ~/SRB2-master/build
-status "Compiling the game..."
-cmake ..
-make -j$(nproc) || error "Compilation failed"
-status_green "Game compiled!"
+wget https://github.com/cobalt2727/L4T-Megascript/raw/master/assets/SRB2-A/dedicated-server-howto.txt
+
+sudo mv SRB2.sh /usr/local/SRB2/SRB2.sh || error "The game installed, but we couldn't properly set up one or more desktop files!"
+sudo mv SRB2Icon.png /usr/local/SRB2/SRB2Icon.png || error "The game installed, but we couldn't properly set up one or more desktop files!"
+#why would I bother testing if I formatted spaces correctly when I can just wildcard
+sudo mv *.desktop /usr/share/applications/ || error "The game installed, but we couldn't properly set up one or more desktop files!"
+#don't break the user's configs if they already have one in there
+mv -vn config.cfg ~/.srb2/config.cfg || error "The game installed, but we couldn't properly set up one or more desktop files!"
+
+#hardly anyone will use this but I'm putting it in anyway. it's neat.
+xdg-open /usr/local/SRB2/
+sudo mv dedicated-server-howto.txt /usr/local/SRB2/dedicated-server-howto.txt && sudo touch /usr/local/SRB2/adedserv.cfg
+
 status "Erasing temporary build files to save space..."
-cd ~/SRB2-master
-mv assets build -t ~/SRB2
-cd ~
-rm -rf SRB2-DT
-rm -rf SRB2-master
-sudo mv SRB2 -t /usr/share
-status_green "Game installed!"
-warning "[NOTE] Remember NOT to move the SRB2 folder or any file inside it or the game will stop working."
-warning "If the game icon doesn't appear inmediately, restart the system."
-status "This message will close in 10 seconds."
-sleep 10
+sudo mv /tmp/SRB2-Assets/* /usr/local/SRB2/ || error "The game installed, but we couldn't properly set up one or more desktop files!"
+sudo rm -rf /tmp/SRB2* /tmp/srb2*
 echo
+
 status "Sending you back to the main menu..."
