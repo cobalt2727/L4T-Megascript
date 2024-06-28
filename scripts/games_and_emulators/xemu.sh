98f79b1f914aaafe1c81fbc44b9cd00fb6ef1d5b
cobalt2727 <60624944+cobalt2727@users.noreply.github.com>
2024-06-28 14:29:01 -0500

    Xemu: add pip
    REALLY need to rewrite this with pipx. someday.

--- scripts/games_and_emulators/xemu.sh
+++ scripts/games_and_emulators/xemu.sh
@@ -38,7 +38,7 @@ case "$__os_codename" in
 bionic)
   ubuntu_ppa_installer "deadsnakes/ppa" || error "PPA failed to install"
   ubuntu_ppa_installer "ubuntu-toolchain-r/test" || error "PPA failed to install"
-  sudo apt install python3.8 gcc-11 g++-11 -y || error "Could not install dependencies!" #GCC 9 (the 20.04 default) also works, I'm just using 11 to future-proof -cobalt
+  sudo apt install python3.8 python3-pip gcc-11 g++-11 -y || error "Could not install dependencies!" #GCC 9 (the 20.04 default) also works, I'm just using 11 to future-proof -cobalt
   sed -i -e 's/python3 /python3.8 /g' build.sh                                           #this is hacky, yes, but hey, it works
   python3.8 -m pip install --upgrade pip meson PyYAML
   CFLAGS=-mcpu=native CXXFLAGS=-mcpu=native CC=gcc-11 CXX=g++-11 ./build.sh || error "Compilation failed!"
