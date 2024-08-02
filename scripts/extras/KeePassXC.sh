9131a002c12320adf9637e37e5b75351dc0632e5
cobalt2727 <60624944+cobalt2727@users.noreply.github.com>
2024-08-01 23:26:30 -0500

    KeePassXC: oops

--- scripts/extras/KeePassXC.sh
+++ scripts/extras/KeePassXC.sh
@@ -2,9 +2,9 @@
 
 if command -v apt >/dev/null; then
   ubuntu_ppa_installer "phoerious/keepassxc"
-
+  sudo apt install keepassxc -y || error "Failed to install KeePassXC!"
 elif command -v dnf >/dev/null; then
-  sudo dnf install keepassxc -y || error "Failed to install AntiMicroX!"
+  sudo dnf install keepassxc -y || error "Failed to install KeePassXC!"
 else
   error "No available package manager found. Are you using a Ubuntu/Debian or Fedora based system?"
 fi
