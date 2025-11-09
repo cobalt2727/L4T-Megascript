365e9a51855feddcad5197c353656b048ee030a4
The Great Wizard Azkali <10491142+Azkali@users.noreply.github.com>
2025-11-09 14:47:03 +0700

    Remove export PATH command from script

--- scripts/rpgmakermlinux-cicpoffs.sh
+++ scripts/rpgmakermlinux-cicpoffs.sh
@@ -4,7 +4,6 @@ echo "RPGMakerLinux-cicpoffs script started!"
 echo -e "Credits: \e[36mhttps://github.com/bakustarver/rpgmakermlinux-cicpoffs\e[0m"
 
 wget -qO- "https://raw.githubusercontent.com/bakustarver/rpgmakermlinux-cicpoffs/main/installgithub.sh" | bash
-export PATH=$PATH:$HOME/.local/bin
 sleep 1
 
 echo "Done!"
