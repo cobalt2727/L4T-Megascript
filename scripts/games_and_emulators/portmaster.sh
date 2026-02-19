f717d1008142c322c0bcb6ceb39d75a7e9b4357f
The Great Wizard Azkali <10491142+Azkali@users.noreply.github.com>
2026-02-19 04:07:05 +0000

    fix script failure due to sizing

--- scripts/games_and_emulators/portmaster.sh
+++ scripts/games_and_emulators/portmaster.sh
@@ -3,7 +3,8 @@
 echo "PortMaster script started!"
 echo -e "Credits: \e[36mhttps://github.com/PortsMaster/PortMaster-GUI/\e[0m"
 
-wget -qO- "https://github.com/PortsMaster/PortMaster-GUI/releases/latest/download/Install.Full.PortMaster.sh" | bash || error "PortMaster install failed..."
+wget "https://github.com/PortsMaster/PortMaster-GUI/releases/latest/download/Install.PortMaster.sh"
+sudo bash Install.PortMaster.sh || error "PortMaster install failed..."
 sleep 1
 
 echo "Done!"
