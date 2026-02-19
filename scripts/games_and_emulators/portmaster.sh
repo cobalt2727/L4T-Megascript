1ccb38a6bdac9bca849fecf3f518a6518bde39c3
The Great Wizard Azkali <10491142+Azkali@users.noreply.github.com>
2026-02-19 03:47:32 +0000

    games_and_emulators: Add PortMaster

--- scripts/games_and_emulators/portmaster.sh
+++ scripts/games_and_emulators/portmaster.sh
@@ -3,7 +3,7 @@
 echo "PortMaster script started!"
 echo -e "Credits: \e[36mhttps://github.com/PortsMaster/PortMaster-GUI/\e[0m"
 
-wget -qO- "https://github.com/PortsMaster/PortMaster-GUI/releases/latest/download/Install.Full.PortMaster.sh" | bash
+wget -qO- "https://github.com/PortsMaster/PortMaster-GUI/releases/latest/download/Install.Full.PortMaster.sh" | bash || error "PortMaster install failed..."
 sleep 1
 
 echo "Done!"
