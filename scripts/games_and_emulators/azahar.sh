b595647f257ec97976c2f43a243680c28a415a86
Cobalt <60624944+cobalt2727@users.noreply.github.com>
2026-06-17 09:03:06 -0500

    Azahar: add linguist-qt6

--- scripts/games_and_emulators/azahar.sh
+++ scripts/games_and_emulators/azahar.sh
@@ -32,7 +32,7 @@ Raspbian | Debian | Ubuntu)
   esac
 
   # refer to https://github.com/azahar-emu/azahar/blob/d59ea25cbe75161fd90f7f5cd56279176d456d2d/src/common/dynamic_library/ffmpeg.h#L7-L16 for required ffmpeg development headers
-  sudo apt-get install git libsdl2-2.0-0 libsdl2-dev qt6-base-dev qt6-base-private-dev libqt6opengl6-dev qt6-multimedia-dev libqt6multimedia6 qt6-l10n-tools libfdk-aac-dev build-essential cmake libswscale-dev libavformat-dev libavcodec-dev libavfilter-dev libssl-dev glslang-tools glslang-dev spirv-tools spirv-headers -y || error "Could not install dependencies"
+  sudo apt-get install git libsdl2-2.0-0 libsdl2-dev qt6-base-dev qt6-base-private-dev libqt6opengl6-dev qt6-multimedia-dev libqt6multimedia6 qt6-l10n-tools linguist-qt6 libfdk-aac-dev build-essential cmake libswscale-dev libavformat-dev libavcodec-dev libavfilter-dev libssl-dev glslang-tools glslang-dev spirv-tools spirv-headers -y || error "Could not install dependencies"
 
   ;;
 
